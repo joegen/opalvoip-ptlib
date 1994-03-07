0a7aa8b4299789394bdfb749d695d73cf7238045
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-03-07 07:47:00 +0000

    Major upgrade
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@77 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/common/osutils.cxx
+++ src/ptlib/common/osutils.cxx
@@ -1,5 +1,5 @@
 /*
- * $Id: osutils.cxx,v 1.6 1994/01/03 04:42:23 robertj Exp $
+ * $Id: osutils.cxx,v 1.7 1994/03/07 07:47:00 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,7 +8,10 @@
  * Copyright 1993 Equivalence
  *
  * $Log: osutils.cxx,v $
- * Revision 1.6  1994/01/03 04:42:23  robertj
+ * Revision 1.7  1994/03/07 07:47:00  robertj
+ * Major upgrade
+ *
+ * Revision 1.6  1994/01/03  04:42:23  robertj
  * Mass changes to common container classes and interactors etc etc etc.
  *
  * Revision 1.5  1993/12/31  06:53:02  robertj
@@ -28,7 +31,7 @@
  *
  */
 
-#include "pwlib.h"
+#include "ptlib.h"
 
 #include <fcntl.h>
 #include <errno.h>
@@ -129,7 +132,7 @@ istream & PTime::ReadFrom(istream &strm)
 
 #if defined(_PTIMER)
 
-PTimer::PTimer(PApplication * app,
+PTimer::PTimer(PTextApplication * app,
               long milliseconds, int seconds, int minutes, int hours, int days)
   : PTimeInterval(milliseconds, seconds, minutes, hours, days),
     owner(PAssertNULL(app)),
@@ -340,6 +343,228 @@ BOOL PFile::Copy(const PString & oldname, const PString & newname)
     return FALSE;
 
   return newfile.Close();
+}
+
+
+#endif
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PArgList
+
+#if defined(_PARGLIST)
+
+PArgList::PArgList()
+{
+  arg_values = NULL;
+  arg_count  = 0;
+}
+
+
+PArgList::PArgList(int theArgc, char ** theArgv, const char * theArgumentSpec)
+{
+  // get the program name and path
+  SetArgs(theArgc, theArgv);
+
+  if (theArgumentSpec != NULL)
+    // we got an argument spec - so process them
+    Parse(theArgumentSpec);
+  else {
+    // we have no argument spec, delay parsing the arguments until later
+    arg_values = NULL;
+    arg_count  = 0;
+  }
+}
+
+
+PArgList::~PArgList()
+{
+  free(optionCount);
+  free(argumentList);
+}
+
+
+void PArgList::SetArgs(int argc, char ** argv)
+{
+  // save argv and and argc for later
+  arg_values = argv;
+  arg_count = argc;
+  shift = 0;
+}
+
+
+void PArgList::Parse(const char * theArgumentSpec)
+{
+  char  c;
+  char *p;
+  int   l;
+
+  // allocate and initialise storage
+  argumentSpec = strdup (theArgumentSpec);
+  l = strlen (argumentSpec);
+  optionCount   = (PINDEX *) calloc (l, sizeof (PINDEX));
+  argumentList = (char **) calloc (l, sizeof (char *));
+
+  while (arg_count > 0 && arg_values[0][0] == '-') {
+    while ((c = *++arg_values[0]) != 0) {
+      if ((p = strchr (argumentSpec, c)) == NULL)
+        UnknownOption (c);
+      else {
+        optionCount[p-argumentSpec]++;
+        if (p[1] == ':') {
+          if (*++(arg_values[0]))
+            argumentList[p-argumentSpec] = arg_values[0];
+          else {
+            if (arg_count < 2) {
+              optionCount[p-argumentSpec] = 0;
+              MissingArgument (c);
+            }
+            else {
+              --arg_count;
+              argumentList [p-argumentSpec] = *++arg_values;
+            }
+          }
+          break;
+        }
+      }
+    }
+    --arg_count;
+    ++arg_values;
+  }
+}
+
+
+PINDEX PArgList::GetOptionCount(char option) const
+{
+  char * p = strchr(argumentSpec, option);
+  return (p == NULL ? 0 : optionCount[p-argumentSpec]);
+}
+
+
+PINDEX PArgList::GetOptionCount(const char * option) const
+{
+  // Future enhancement to have long option names
+  return GetOptionCount(*option);
+}
+
+
+PString PArgList::GetOptionString(char option, const char * dflt) const
+{
+  char * p = strchr(argumentSpec, option);
+  if (p != NULL)
+    return argumentList[p-argumentSpec];
+
+  if (dflt != NULL)
+    return dflt;
+
+  return PString();
+}
+
+
+PString PArgList::GetOptionString(const char * option, const char * dflt) const
+{
+  // Future enhancement to have long option names
+  return GetOptionString(*option, dflt);
+}
+
+
+PString PArgList::GetParameter(PINDEX num) const
+{
+  if ((num+shift) < arg_count)
+    return arg_values[num+shift];
+
+  IllegalArgumentIndex(num+shift);
+  return PString();
+}
+
+
+void PArgList::Shift(int sh) 
+{
+  if ((sh < 0) && (shift > 0))
+    shift -= sh;
+  else if ((sh > 0) && (shift < arg_count))
+    shift += sh;
+}
+
+
+void PArgList::IllegalArgumentIndex(PINDEX idx) const
+{
+#ifdef _WINDLL
+  idx = 1;
+#else
+  cerr << "attempt to access undefined argument at index "
+       << idx << endl;
+#endif
+}
+ 
+
+void PArgList::UnknownOption(char option) const
+{
+#ifdef _WINDLL
+  option = ' ';
+#else
+  cerr << "unknown option \"" << option << "\"\n";
+#endif
+}
+
+
+void PArgList::MissingArgument(char option) const
+{
+#ifdef _WINDLL
+  option = ' ';
+#else
+  cerr << "option \"" << option << "\" requires argument\n";
+#endif
+}
+
+
+#endif
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PTextApplication
+
+#if defined(_PTEXTAPPLICATION)
+
+PObject::Comparison PTextApplication::Compare(const PObject & obj) const
+{
+  return applicationName.Compare(((const PTextApplication &)obj).applicationName);
+}
+
+
+void PTextApplication::PreInitialise(int argc, char ** argv)
+{
+  terminationValue = 1;
+
+  arguments.SetArgs(argc-1, argv+1);
+
+  applicationFile = PFile(PString(argv[0]));
+  applicationName = applicationFile.GetTitle().ToLower();
+}
+
+
+PString PTextApplication::GetEnvironment(const PString & varName) const
+{
+  char * env = getenv(varName);
+  if (env == NULL)
+    return PString();
+  return env;
+}
+
+
+int PTextApplication::Termination()
+{
+  return terminationValue;
+}
+
+
+void PTextApplication::Terminate()
+{
+#ifdef _WINDLL
+  FatalExit(Termination());
+#else
+  exit(Termination());
+#endif
 }
 
 
