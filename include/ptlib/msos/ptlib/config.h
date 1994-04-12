999f5019deb3dea0c51bbbd618a8de3ea48c7edf
Robert Jongbloed <robertj@voxlucida.com.au>
1994-04-12 08:21:52 +0000

    Initial revision

---
+++ include/ptlib/msos/ptlib/config.h
@@ -0,0 +1,16 @@
+/*
+ * $Id: config.h,v 1.1 1994/04/12 08:21:52 robertj Exp $
+ */
+
+
+#ifndef _PCONFIG
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PConfiguration
+
+#include "../../common/config.h"
+};
+
+
+#endif
---
+++ include/ptlib/msos/ptlib/sound.h
@@ -0,0 +1,26 @@
+/*
+ * $Id: sound.h,v 1.1 1994/04/12 08:21:52 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: sound.h,v $
+ * Revision 1.1  1994/04/12 08:21:52  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PSOUND
+
+///////////////////////////////////////////////////////////////////////////////
+// PSound
+
+#include "../../common/sound.h"
+};
+
+
+#endif
