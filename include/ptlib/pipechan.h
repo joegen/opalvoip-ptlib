459601a124f7381f27ecaa277a39ef972895bb37
Robert Jongbloed <robertj@voxlucida.com.au>
1994-04-20 12:17:44 +0000

    Initial revision

---
+++ include/ptlib/pipechan.h
@@ -0,0 +1,54 @@
+/*
+ * $Id: pipechan.h,v 1.1 1994/04/20 12:17:44 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: pipechan.h,v $
+ * Revision 1.1  1994/04/20 12:17:44  robertj
+ * Initial revision
+ *
+ */
+
+
+#define _PPIPECHANNEL
+
+
+PDECLARE_CLASS(PPipeChannel, PChannel)
+  // A channel that uses a operating system pipe between two processes.
+
+  public:
+    PPipeChannel();
+      // Create a new pipe channel.
+
+
+    // Overrides from class PChannel
+    BOOL Read(void * buf, PINDEX len);
+      // Low level read from the channel. This function will block until the
+      // requested number of characters were read.
+
+    int Read();
+      // Read a single 8 bit byte from the channel. If one was not available
+      // then the function returns immediately with a -1 return value.
+
+    BOOL Write(const void * buf, PINDEX len);
+      // Low level write to the channel. This function will block until the
+      // requested number of characters were written.
+
+    BOOL Write(char c);
+      // Write a single character to the channel. This function does not block
+      // and will return FALSE if it could not write the character.
+
+    PINDEX InputAvailable();
+      // Return the number of characters that may be read from the channel
+      // without causing the Read() function to block.
+
+    PINDEX OutputAvailable();
+      // Return the number of characters that may be written to the channel
+      // without causing the Write() function to block.
+
+
+// Class declaration continued in platform specific header file ///////////////
