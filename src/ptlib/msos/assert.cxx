5c552bb587b96116fb1c0b57fdbd304b76e1f11e
Robert Jongbloed <robertj@voxlucida.com.au>
1994-06-25 12:13:01 +0000

    Synchronisation.

--- src/ptlib/msos/assert.cxx
+++ src/ptlib/msos/assert.cxx
@@ -1,5 +1,5 @@
 /*
- * $Id: assert.cxx,v 1.1 1994/04/01 14:39:35 robertj Exp $
+ * $Id: assert.cxx,v 1.2 1994/06/25 12:13:01 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,24 +8,31 @@
  * Copyright 1993 by Robert Jongbloed and Craig Southeren
  *
  * $Log: assert.cxx,v $
- * Revision 1.1  1994/04/01 14:39:35  robertj
- * Initial revision
+ * Revision 1.2  1994/06/25 12:13:01  robertj
+ * Synchronisation.
  *
+// Revision 1.1  1994/04/01  14:39:35  robertj
+// Initial revision
+//
  */
 
 #include "ptlib.h"
 
-void PAssertFunc(const char * file, int line)
+
+void PAssertFunc(const char * file, int line, const char * msg)
 {
   for (;;) {
-    cerr << "Assertion fail: File " << file << ", Line " << line << endl
-         << "<A>bort, <B>reak, <I>gnore? ";
+    cerr << "Assertion fail: File " << file << ", Line " << line << endl;
+    if (msg != NULL)
+      cerr << msg << endl;
+    cerr << "<A>bort, <B>reak, <I>gnore? ";
     cerr.flush();
     switch (getchar()) {
       case 'A' :
       case 'a' :
+      case EOF :
         cerr << "Aborted\n";
-        exit(100);
+        _exit(100);
         
       case 'B' :
       case 'b' :
