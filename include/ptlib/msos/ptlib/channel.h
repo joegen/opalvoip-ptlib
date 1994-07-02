5ca6400c1539736b39c699b511cafc38d984b285
Robert Jongbloed <robertj@voxlucida.com.au>
1994-07-02 03:18:09 +0000

    Initial revision

---
+++ include/ptlib/msos/ptlib/channel.h
@@ -0,0 +1,26 @@
+/*
+ * $Id: channel.h,v 1.1 1994/07/02 03:18:09 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: channel.h,v $
+ * Revision 1.1  1994/07/02 03:18:09  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PCHANNEL
+
+///////////////////////////////////////////////////////////////////////////////
+// PChannel
+
+#include "../../common/channel.h"
+};
+
+
+#endif
