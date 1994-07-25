81914abdcab5b9eda60d60ef584d0867387921bf
Robert Jongbloed <robertj@voxlucida.com.au>
1994-07-25 03:36:03 +0000

    Initial revision

---
+++ include/ptlib/sockets.h
@@ -0,0 +1,56 @@
+/*
+ * $Id: sockets.h,v 1.1 1994/07/25 03:36:03 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ *
+ * $Log: sockets.h,v $
+ * Revision 1.1  1994/07/25 03:36:03  robertj
+ * Initial revision
+ *
+ * Revision 1.3  1994/07/21  12:17:41  robertj
+ * Sockets.
+ *
+ * Revision 1.2  1994/06/25  12:27:39  robertj
+ * *** empty log message ***
+ *
+ * Revision 1.1  1994/04/01  14:38:42  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _SOCKETS_H
+#define _SOCKETS_H
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PSocket
+
+#include <socket.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PIPSocket
+
+#include <ipsock.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PUDPocket
+
+#include <udpsock.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PTCPocket
+
+#include <tcpsock.h>
+
+
+#endif // _SOCKETS_H
+
+
+// End Of File ///////////////////////////////////////////////////////////////
