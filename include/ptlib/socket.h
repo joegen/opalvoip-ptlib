672fb0f78d6b8855ac1ae539448ffd2cfb5cea6b
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-07-25 03:36:03 +0000

    Added sockets to common, normalising to same comment standard.

--- include/ptlib/socket.h
+++ include/ptlib/socket.h
@@ -1,10 +1,19 @@
-////////////////////////////////////////////////////////
-//
-//  PSocket
-//
-////////////////////////////////////////////////////////
+/*
+ * $Id: socket.h,v 1.2 1994/07/25 03:36:03 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: socket.h,v $
+ * Revision 1.2  1994/07/25 03:36:03  robertj
+ * Added sockets to common, normalising to same comment standard.
+ *
+ */
 
-#define	_PSOCKET
+#define _PSOCKET
 
 #ifndef _PCHANNEL
 #include <channel.h>
@@ -21,5 +30,5 @@ PDECLARE_CLASS(PSocket, PChannel)
     virtual BOOL Accept (const PString & address);
       // wait for another host to establish a connection 
 
-// Platform specific declarations follow
 
+// Class declaration continued in platform specific header file ///////////////
--- include/ptlib/udpsock.h
+++ include/ptlib/udpsock.h
@@ -1,12 +1,20 @@
-////////////////////////////////////////////////////////
-//
-//  PUDPSocket
-//
-////////////////////////////////////////////////////////
+/*
+ * $Id: udpsock.h,v 1.2 1994/07/25 03:36:03 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * UDP Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: udpsock.h,v $
+ * Revision 1.2  1994/07/25 03:36:03  robertj
+ * Added sockets to common, normalising to same comment standard.
+ *
+ */
 
-#define	_PUDPSOCKET
+#define _PUDPSOCKET
 
 PDECLARE_CLASS(PUDPSocket, PIPSocket)
 
-// Platform specific declarations follow
-
+// Class declaration continued in platform specific header file ///////////////
