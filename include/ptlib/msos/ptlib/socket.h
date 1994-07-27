1bc3ccb361efbea6639120a5ae36e501da8833e1
Robert Jongbloed <robertj@voxlucida.com.au>
1994-07-27 06:00:10 +0000

    Initial revision

---
+++ include/ptlib/msos/ptlib/socket.h
@@ -0,0 +1,29 @@
+/*
+ * $Id: socket.h,v 1.1 1994/07/27 06:00:10 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: socket.h,v $
+ * Revision 1.1  1994/07/27 06:00:10  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PSOCKET
+
+
+#include <winsock.h>
+
+
+#include "../../common/socket.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
