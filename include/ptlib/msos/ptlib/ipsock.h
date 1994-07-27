926ed5f2a5c96e83bd0fc883e6e4578bec75d018
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-07-27 06:00:10 +0000

    svnimporter: adding tags and branches to revision 154

---
+++ include/ptlib/msos/ptlib/ipsock.h
@@ -0,0 +1,26 @@
+/*
+ * $Id: ipsock.h,v 1.1 1994/07/27 06:00:10 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Internet Protocol Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: ipsock.h,v $
+ * Revision 1.1  1994/07/27 06:00:10  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PIPSOCKET
+
+
+#include "../../common/ipsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
