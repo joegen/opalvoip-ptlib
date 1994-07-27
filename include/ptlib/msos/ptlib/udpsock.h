370060ac7aa7a4546e250525a7a910397303cce7
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-07-27 06:00:10 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@155 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ include/ptlib/msos/ptlib/udpsock.h
@@ -0,0 +1,26 @@
+/*
+ * $Id: udpsock.h,v 1.1 1994/07/27 06:00:10 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * UDP Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: udpsock.h,v $
+ * Revision 1.1  1994/07/27 06:00:10  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PUDPSOCKET
+
+
+#include "../../common/udpsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
