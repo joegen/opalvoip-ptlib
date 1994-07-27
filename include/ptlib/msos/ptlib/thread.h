df9bfc4087ebd952508cafdb3bc64f73ea1fb701
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-07-27 06:00:10 +0000

    svnimporter: adding tags and branches to revision 152

---
+++ include/ptlib/msos/ptlib/thread.h
@@ -0,0 +1,82 @@
+/*
+ * $Id: thread.h,v 1.4 1994/07/27 06:00:10 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: thread.h,v $
+ * Revision 1.4  1994/07/27 06:00:10  robertj
+ * Backup
+ *
+ * Revision 1.3  1994/07/21  12:35:18  robertj
+ * *** empty log message ***
+ *
+ * Revision 1.2  1994/07/02  03:18:09  robertj
+ * Multi-threading implementation.
+ *
+ * Revision 1.1  1994/06/25  12:13:01  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PTHREAD
+
+#include <malloc.h>
+#include <setjmp.h>
+
+#if defined(_MSC_VER) && !defined(_JMP_BUF_DEFINED)
+
+typedef int jmp_buf[9];
+#define setjmp _setjmp
+extern "C" int  __cdecl _setjmp(jmp_buf);
+extern "C" void __cdecl longjmp(jmp_buf, int);
+
+#endif
+
+
+typedef BOOL (__far *PThreadBlockFunction)(PObject *);
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PThread
+
+#include "../../common/thread.h"
+  public:
+    void Block(PThreadBlockFunction isBlocked, PObject * obj);
+      // Flag the thread as blocked. The scheduler will call the specified
+      // function with the obj parameter to determine if the thread is to be
+      // unblocked.
+
+  protected:
+    BOOL IsOnlyThread() const;
+      // Return TRUE if is only thread in process
+
+
+  private:
+    // Member fields
+    PThreadBlockFunction isBlocked;
+      // Callback function to determine if the thread is blocked on I/O.
+
+    PObject * blocker;
+      // When thread is blocked on I/O this is the object to pass to isBlocked.
+
+#ifdef _WINDOWS
+    unsigned stackUsed;
+      // High water mark for stack allocated for the thread
+#endif
+};
+
+
+#ifndef _WINDLL
+
+inline PThread::PThread()
+  { }   // Is mostly initialised by InitialiseProcessThread().
+
+#endif
+
+
+#endif
