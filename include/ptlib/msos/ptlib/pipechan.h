01f476d4cbab90b0b2fbcb17fe495fdc96cbb929
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-10-23 05:35:36 +0000

    svnimporter: adding tags and branches to revision 217

---
+++ include/ptlib/msos/ptlib/pipechan.h
@@ -0,0 +1,23 @@
+/*
+ * $Id: pipechan.h,v 1.1 1994/10/23 05:35:36 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: pipechan.h,v $
+ * Revision 1.1  1994/10/23 05:35:36  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PPIPECHANNEL
+
+#include "..\..\common\pipechan.h"
+};
+
+
+#endif
