6c865070a636826cab529f79653a89836a7cb48d
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-10-23 05:36:51 +0000

    Sockets implementation.

--- include/ptlib/msos/ptlib/socket.h
+++ include/ptlib/msos/ptlib/socket.h
@@ -1,5 +1,5 @@
 /*
- * $Id: socket.h,v 1.1 1994/07/27 06:00:10 robertj Exp $
+ * $Id: socket.h,v 1.2 1994/10/23 05:36:51 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,7 +8,13 @@
  * Copyright 1993 Equivalence
  *
  * $Log: socket.h,v $
- * Revision 1.1  1994/07/27 06:00:10  robertj
+ * Revision 1.2  1994/10/23 05:36:51  robertj
+ * Sockets implementation.
+ *
+ * Revision 1.1  1994/08/22  00:18:02  robertj
+ * Initial revision
+ *
+ * Revision 1.1  1994/07/27  06:00:10  robertj
  * Initial revision
  *
  */
@@ -17,9 +23,36 @@
 
 
 #include <winsock.h>
+#define P_HAS_BERKELEY_SOCKETS
 
 
 #include "../../common/socket.h"
+  public:
+    virtual BOOL Read(void * buf, PINDEX len);
+      // Low level read from the channel. This function may block until the
+      // requested number of characters were read or the read timeout was
+      // reached. The return value indicates that at least one character was
+      // read from the channel.
+
+    virtual BOOL Write(const void * buf, PINDEX len);
+      // Low level write to the channel. This function will block until the
+      // requested number of characters are written or the write timeout is
+      // reached. The return value is TRUE if at least len bytes were written
+      // to the channel.
+
+    virtual BOOL Close();
+      // Close the socket.
+
+  protected:
+      BOOL ConvertOSError(int error);
+      // Convert an operating system error into platform independent error.
+      // This will set the lastError and osError member variables for access
+      // by GetErrorCode() and GetErrorNumber(). Returns TRUE if there was
+      // no error.
+
+
+  private:
+    static BOOL WinSockStarted;
 };
 
 
