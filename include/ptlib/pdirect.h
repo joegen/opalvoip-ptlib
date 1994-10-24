4dabecf00708ae431c9b0c98b32148d3cc31e075
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-10-24 00:07:03 +0000

    svnimporter: adding tags and branches to revision 231

---
+++ include/ptlib/pdirect.h
@@ -0,0 +1,183 @@
+/*
+ * $Id: pdirect.h,v 1.16 1994/10/24 00:07:03 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: pdirect.h,v $
+ * Revision 1.16  1994/10/24 00:07:03  robertj
+ * Changed PFilePath and PDirectory so descends from either PString or
+ *     PCaselessString depending on the platform.
+ *
+ * Revision 1.15  1994/10/23  04:49:25  robertj
+ * Chnaged PDirectory to descend of PString.
+ * Added PDirectory Exists() function.
+ *
+ * Revision 1.14  1994/08/23  11:32:52  robertj
+ * Oops
+ *
+ * Revision 1.13  1994/08/22  00:46:48  robertj
+ * Added pragma fro GNU C++ compiler.
+ *
+ * Revision 1.12  1994/06/25  11:55:15  robertj
+ * Unix version synchronisation.
+ *
+ * Revision 1.11  1994/04/20  12:17:44  robertj
+ * Split name into PFilePath
+ *
+ * Revision 1.10  1994/04/11  14:16:27  robertj
+ * Added function for determining if character is a valid directory separator.
+ *
+ * Revision 1.9  1994/04/01  14:14:57  robertj
+ * Put platform independent file permissions and type codes back.
+ *
+ * Revision 1.7  1994/01/13  03:17:55  robertj
+ * Added functions to get the name of the volume the directory is contained in
+ *    and a function to determine if the directory is a root directory of the
+ *    volume (ie is does not have a parent directory).
+ *
+ * Revision 1.6  1994/01/03  04:42:23  robertj
+ * Mass changes to common container classes and interactors etc etc etc.
+ *
+ * Revision 1.5  1993/12/31  06:45:38  robertj
+ * Made inlines optional for debugging purposes.
+ *
+ * Revision 1.4  1993/08/21  01:50:33  robertj
+ * Made Clone() function optional, default will assert if called.
+ *
+ * Revision 1.3  1993/07/14  12:49:16  robertj
+ * Fixed RCS keywords.
+ *
+ */
+
+
+#define _PDIRECTORY
+
+#ifdef __GNUC__
+#pragma interface
+#endif
+
+
+///////////////////////////////////////////////////////////////////////////////
+// File System
+
+enum PFileTypes {
+  PRegularFile = 1,
+  PSymbolicLink = 2,
+  PSubDirectory = 4,
+  PCharDevice = 8,
+  PBlockDevice = 16,
+  PFifo = 32,
+  PSocketDevice = 64,
+  PUnknownFileType = 256,
+  PAllFiles = PRegularFile|PSymbolicLink|PSubDirectory|
+                 PCharDevice|PBlockDevice|PFifo|PSocketDevice|PUnknownFileType
+};
+
+
+enum PPermissions {
+  PWorldExecute = 1,
+  PWorldWrite = 2,
+  PWorldRead = 4,
+  PGroupExecute = 8,
+  PGroupWrite = 16,
+  PGroupRead = 32,
+  PUserExecute = 64,
+  PUserWrite = 128,
+  PUserRead = 256,
+  PAllPermissions = PUserRead|PUserWrite|PUserExecute|PGroupRead|
+                PGroupWrite|PGroupExecute|PWorldRead|PWorldWrite|PWorldExecute,
+  PDefaultPerms = PUserRead|PUserWrite|PGroupRead|PWorldRead
+};
+
+
+struct PFileInfo {
+  PFileTypes type;
+  PTime      created;
+  PTime      modified;
+  PTime      accessed;
+  DWORD      size;
+  int        permissions;
+  BOOL       hidden;
+};
+
+
+PDECLARE_CONTAINER(PDirectory, PFILE_PATH_STRING)
+  // Class to represent a directory in the operating system file system.
+
+  public:
+    PDirectory();
+      // Create a directory object of the current directory
+      
+    PDirectory(const PString & pathname);
+      // Create a directory object of the specified directory
+      
+
+    // New member functions
+    PString GetVolume() const;
+      // Return the volume name that the directory is in.
+
+    BOOL IsRoot() const;
+      // Return TRUE if the directory is the root directory of a volume.
+
+    PINLINE static BOOL IsSeparator(char c);
+      // Return TRUE if the character is a directory path separator.
+
+
+    BOOL Exists() const;
+    static BOOL Exists(const PString & p);
+      // Change to the specified directory.
+      
+    BOOL Change() const;
+    static BOOL Change(const PString & p);
+      // Change to the specified directory.
+      
+    BOOL Create(int perm = PDefaultPerms) const;
+    PINLINE static BOOL Create(const PString & p, int perm = PDefaultPerms);
+      // Create a new directory with the specified permissions
+
+    BOOL Remove();
+    PINLINE static BOOL Remove(const PString & p);
+      // Delete the specified directory.
+      
+
+    BOOL Open(int scanMask = PAllFiles);
+      // Open the directory for scanning its list of files.
+      
+    BOOL Restart(int scanMask = PAllFiles);
+      // Restart file list scan from the beginning of directory.
+      
+    BOOL Next();
+      // Move to the next file in the directory scan.
+      
+    void Close();
+      // Close the directory after a file list scan.
+      
+    PString GetEntryName() const;
+      // Return the name (without the volume or directory path) of the
+      // current entry in the directory scan. This may be the name of a file
+      // or a subdirectory or even a link or device for operating systems that
+      // support them.
+
+    BOOL IsSubDir() const;
+      // Return TRUE if the directory entry currently being scanned is itself
+      // another directory entry.
+      
+    BOOL GetInfo(PFileInfo & info) const;
+      // Get information on the current directory entry
+
+
+  protected:
+    // New functions for class
+    void Construct();
+      // Common constructor code
+
+
+    // Member variables
+    int     scanMask;
+
+
+// Class declaration continued in platform specific header file ///////////////
