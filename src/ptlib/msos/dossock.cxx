3f93e7363385b79196965d8c7f2dec7a73feaab9
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1994-10-30 12:06:54 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@253 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ src/ptlib/msos/dossock.cxx
@@ -0,0 +1,88 @@
+/*
+ * $Id: dossock.cxx,v 1.1 1994/10/30 12:06:54 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Implementation
+ *
+ * Copyright 1994 Equivalence
+ *
+ * $Log: dossock.cxx,v $
+ * Revision 1.1  1994/10/30 12:06:54  robertj
+ * Initial revision
+ *
+// Revision 1.1  1994/10/23  05:42:49  robertj
+// Initial revision
+//
+// Revision 1.1  1994/08/22  00:18:02  robertj
+// Initial revision
+//
+ */
+
+#include <ptlib.h>
+#include <sockets.h>
+
+
+//////////////////////////////////////////////////////////////////////////////
+// PSocket
+
+PSocket::PSocket()
+{
+}
+
+
+BOOL PSocket::Read(void * buf, PINDEX len)
+{
+  return TRUE;
+}
+
+
+BOOL PSocket::Write(const void * buf, PINDEX len)
+{
+  return TRUE;
+}
+
+
+BOOL PSocket::Close()
+{
+  if (IsOpen())
+    return FALSE;
+//  return ConvertOSError(closesocket(os_handle));
+  return FALSE;
+}
+
+
+BOOL PSocket::ConvertOSError(int error)
+{
+  if (error >= 0) {
+    lastError = NoError;
+    osError = 0;
+    return TRUE;
+  }
+
+//  osError = WSAGetLastError();
+  switch (osError) {
+    case 0 :
+      lastError = NoError;
+      return TRUE;
+    default :
+      lastError = Miscellaneous;
+      osError |= 0x20000000;
+  }
+  return FALSE;
+}
+
+
+BOOL PTCPSocket::ReadOutOfBand(void *, PINDEX)
+{
+  return FALSE;
+}
+
+
+BOOL PTCPSocket::WriteOutOfBand(const void *, PINDEX)
+{
+  return FALSE;
+}
+
+
+// End Of File ///////////////////////////////////////////////////////////////
