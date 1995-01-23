81b7422bc262b9817a584241fa8cfc6a30412639
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-01-23 18:43:27 +0000

    svnimporter: adding tags and branches to revision 413

---
+++ include/ptlib/unix/ptlib/sfile.h
@@ -0,0 +1,30 @@
+/*
+ * $Id: sfile.h,v 1.1 1995/01/23 18:43:27 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: sfile.h,v $
+ * Revision 1.1  1995/01/23 18:43:27  craigs
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PSTRUCTUREDFILE
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PStructuredFile
+
+#include "../../common/sfile.h"
+};
+
+
+#endif
---
+++ include/ptlib/unix/ptlib/textfile.h
@@ -0,0 +1,30 @@
+/*
+ * $Id: textfile.h,v 1.1 1995/01/23 18:43:27 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: textfile.h,v $
+ * Revision 1.1  1995/01/23 18:43:27  craigs
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PTEXTFILE
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PTextFile
+
+#include "../../common/textfile.h"
+};
+
+
+#endif
