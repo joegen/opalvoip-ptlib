c179c08ddff5b2bd4615af2fec88eb4bb6403e9d
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-01-23 22:59:51 +0000

    Changes for HPUX and Sun 4

--- include/ptlib/unix/ptlib/socket.h
+++ include/ptlib/unix/ptlib/socket.h
@@ -1,5 +1,5 @@
 /*
- * $Id: socket.h,v 1.1 1995/01/23 18:43:27 craigs Exp $
+ * $Id: socket.h,v 1.2 1995/01/23 22:59:51 craigs Exp $
  *
  * Portable Windows Library
  *
@@ -8,8 +8,8 @@
  * Copyright 1993 Equivalence
  *
  * $Log: socket.h,v $
- * Revision 1.1  1995/01/23 18:43:27  craigs
- * Initial revision
+ * Revision 1.2  1995/01/23 22:59:51  craigs
+ * Changes for HPUX and Sun 4
  *
  * Revision 1.1  1994/04/12  08:31:05  robertj
  * Initial revision
@@ -22,12 +22,21 @@
 
 #pragma interface
 
-#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <termio.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <signal.h>
 #include <netdb.h>
+
+#if defined (P_SUN4)
+extern "C" int socket(int, int, int);
+extern "C" int connect(int s, struct sockaddr *name, int namelen);
+extern "C" int ioctl(int, int, void *);
+extern "C" int send(int s, const void *buf, int len, int flags);
+extern "C" int recv(int s, void *buf, int len, int flags);
+#endif
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // PSocket
