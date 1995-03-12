cb74eeb72ec7033fd996137e388b8afda6dd1841
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-03-12 04:45:40 +0000

    Added more functionality.

--- include/ptlib/socket.h
+++ include/ptlib/socket.h
@@ -1,5 +1,5 @@
 /*
- * $Id: socket.h,v 1.7 1995/01/03 09:36:19 robertj Exp $
+ * $Id: socket.h,v 1.8 1995/03/12 04:45:40 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,7 +8,10 @@
  * Copyright 1993 Equivalence
  *
  * $Log: socket.h,v $
- * Revision 1.7  1995/01/03 09:36:19  robertj
+ * Revision 1.8  1995/03/12 04:45:40  robertj
+ * Added more functionality.
+ *
+ * Revision 1.7  1995/01/03  09:36:19  robertj
  * Documentation.
  *
  * Revision 1.6  1995/01/02  12:16:17  robertj
@@ -53,12 +56,34 @@ PDECLARE_CLASS(PSocket, PChannel)
       const PString & hostname,   // Remote host address.
       WORD portnum                // Remote port number.
     ) = 0;
-    // Open a connection to another host using the port number.
+    virtual BOOL Open(
+      WORD port = 0             // Port number to use for the connection.
+    ) = 0;
+    virtual BOOL Open(
+      PSocket & socket          // Listening socket making the connection.
+    ) = 0;
+    /* Open a socket to a remote host on the specified port number.
+    
+       The first form creates a "connecting" socket which is typically the
+       client or initiator of a communications channel. This connects to a
+       "listening" socket at the other end of the communications channel.
 
-    virtual BOOL Accept(
-      const PString & hostname
-    );
-    // Wait for another host to establish a connection 
+       The second form creates a "listening" socket which may be used for
+       server based applications. A "connecting" socket begins a connection by
+       initiating a connection to this socket. An active socket of this type
+       is then used to generate other "accepting" sockets which establish a
+       two way communications channel with the "connecting" socket.
+
+       The third form opens an "accepting" socket. When a "listening" socket
+       has a pending connection to make, this will accept a connection made
+       by the "connecting" socket created to establish a link.
+
+       If the $B$port$B$ parameter is zero then the port number as defined by
+       the object instance construction or the $B$SetPort()$B$ function is
+       used.
+
+       Returns: TRUE if the channel was successfully opened.
+     */
 
 
 // Class declaration continued in platform specific header file ///////////////
