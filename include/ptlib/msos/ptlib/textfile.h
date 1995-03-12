0812b5979526a537969f4034867022b16a372436
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-03-12 05:00:01 +0000

    svnimporter: adding tags and branches to revision 474

---
+++ include/ptlib/msos/ptlib/textfile.h
@@ -0,0 +1,38 @@
+/*
+ * $Id: textfile.h,v 1.2 1995/03/12 05:00:01 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: textfile.h,v $
+ * Revision 1.2  1995/03/12 05:00:01  robertj
+ * Re-organisation of DOS/WIN16 and WIN32 platforms to maximise common code.
+ * Used built-in equate for WIN32 API (_WIN32).
+ *
+ * Revision 1.1  1994/06/25  12:13:01  robertj
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PTEXTFILE
+
+///////////////////////////////////////////////////////////////////////////////
+// PTextFile
+
+#include "../../common/textfile.h"
+  protected:
+    virtual BOOL IsTextFile() const;
+      // Return TRUE if text file translation is required
+
+  private:
+    char characterAfterCarriageReturn;
+};
+
+
+#endif
