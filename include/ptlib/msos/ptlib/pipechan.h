95c0458c859d81ff3de9a5e8baf5602fb00fde5a
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-03-12 04:59:56 +0000

    Re-organisation of DOS/WIN16 and WIN32 platforms to maximise common code. Used built-in equate for WIN32 API (_WIN32).

--- include/ptlib/msos/ptlib/pipechan.h
+++ include/ptlib/msos/ptlib/pipechan.h
@@ -1,5 +1,5 @@
 /*
- * $Id: pipechan.h,v 1.1 1994/10/23 05:35:36 robertj Exp $
+ * $Id: pipechan.h,v 1.2 1995/03/12 04:59:56 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,7 +8,11 @@
  * Copyright 1993 Equivalence
  *
  * $Log: pipechan.h,v $
- * Revision 1.1  1994/10/23 05:35:36  robertj
+ * Revision 1.2  1995/03/12 04:59:56  robertj
+ * Re-organisation of DOS/WIN16 and WIN32 platforms to maximise common code.
+ * Used built-in equate for WIN32 API (_WIN32).
+ *
+ * Revision 1.1  1994/10/23  05:35:36  robertj
  * Initial revision
  *
  */
@@ -17,6 +21,14 @@
 #ifndef _PPIPECHANNEL
 
 #include "..\..\common\pipechan.h"
+#if defined(_WIN32)
+  protected:
+    PROCESS_INFORMATION info;
+    HANDLE hToChild, hFromChild;
+#else
+    int hToChild, hFromChild;
+    BOOL hasRun;
+#endif
 };
 
 
