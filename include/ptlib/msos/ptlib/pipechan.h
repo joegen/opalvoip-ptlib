ab3e32ac7821d4804510e59379363b459461c759
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-03-14 13:31:35 +0000

    svnimporter: adding tags and branches to revision 519

---
+++ include/ptlib/msos/ptlib/pipechan.h
@@ -0,0 +1,38 @@
+/*
+ * $Id: pipechan.h,v 1.3 1995/03/14 13:31:35 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: pipechan.h,v $
+ * Revision 1.3  1995/03/14 13:31:35  robertj
+ * Implemented DOS pipe channel.
+ *
+ * Revision 1.2  1995/03/12  04:59:56  robertj
+ * Re-organisation of DOS/WIN16 and WIN32 platforms to maximise common code.
+ * Used built-in equate for WIN32 API (_WIN32).
+ *
+ * Revision 1.1  1994/10/23  05:35:36  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PPIPECHANNEL
+
+#include "..\..\common\pipechan.h"
+  protected:
+#if defined(_WIN32)
+    PROCESS_INFORMATION info;
+    HANDLE hToChild, hFromChild;
+#else
+    BOOL hasRun;
+    PFilePath toChild, fromChild;
+#endif
+};
+
+
+#endif
