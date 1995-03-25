0eb4ea4473ddd50bfd411b6b9e5714757a8a85d0
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-03-25 20:57:13 +0000

    Initial revision

---
+++ include/ptlib/unix/ptlib/dynalink.h
@@ -0,0 +1,16 @@
+/*
+ * $Id: dynalink.h,v 1.1 1995/03/25 20:57:13 craigs Exp $
+ */
+
+#ifndef _PDYNALINK
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PDynaLink
+
+#include "../../common/dynalink.h"
+};
+
+
+#endif
