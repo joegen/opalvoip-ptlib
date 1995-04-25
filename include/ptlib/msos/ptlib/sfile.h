a7be0b509eec383ac871182f5eb4764a027d014a
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-04-25 11:31:47 +0000

    svnimporter: adding tags and branches to revision 565

---
+++ include/ptlib/msos/ptlib/sfile.h
@@ -0,0 +1,39 @@
+/*
+ * $Id: sfile.h,v 1.2 1995/04/25 11:31:47 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: sfile.h,v $
+ * Revision 1.2  1995/04/25 11:31:47  robertj
+ * Fixed Borland compiler warnings.
+ *
+ * Revision 1.1  1994/06/25 12:13:01  robertj
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PSTRUCTUREDFILE
+
+///////////////////////////////////////////////////////////////////////////////
+// PStructuredFile
+
+#ifdef __BORLANDC__
+#pragma warn -hid
+#endif
+
+#include "../../common/sfile.h"
+};
+
+#ifdef __BORLANDC__
+#pragma warn .hid
+#endif
+
+#endif
