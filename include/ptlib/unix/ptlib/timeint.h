050c06e77cc152319bad172a3cab120729e31054
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-07-09 00:35:00 +0000

    svnimporter: adding tags and branches to revision 652

---
+++ include/ptlib/unix/ptlib/timeint.h
@@ -0,0 +1,42 @@
+/*
+ * $Id: timeint.h,v 1.3 1995/07/09 00:35:00 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: timeint.h,v $
+ * Revision 1.3  1995/07/09 00:35:00  craigs
+ * Latest and greatest omnibus change
+ *
+ * Revision 1.2  1995/03/25 20:58:24  craigs
+ * Removed unnecessary prefix on declaration of PTimerInterval::operator =
+ *
+ * Revision 1.1  1995/01/23  18:43:27  craigs
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PTIMEINTERVAL
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PTimeInterval
+
+#include <time.h>
+
+#include "../../common/timeint.h"
+  public:
+    PTimeInterval & operator =(const PTimeInterval & timeInterval);
+    PTimeInterval(const PTimeInterval & copy);
+};
+
+#define PMaxTimeInterval PTimeInterval((long)0x7fffffff)
+
+#endif
