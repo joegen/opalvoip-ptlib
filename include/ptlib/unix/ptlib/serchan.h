f6749a597ad80682b9e045378ab00b83c5bbb159
Craig Southeren <craigs@postincrement.com>
1995-12-08 13:14:52 +0000

    Added new function

--- include/ptlib/unix/ptlib/serchan.h
+++ include/ptlib/unix/ptlib/serchan.h
@@ -1,5 +1,5 @@
 /*
- * $Id: serchan.h,v 1.1 1995/01/23 18:43:27 craigs Exp $
+ * $Id: serchan.h,v 1.2 1995/12/08 13:14:52 craigs Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: serchan.h,v $
+ * Revision 1.2  1995/12/08 13:14:52  craigs
+ * Added new function
+ *
  * Revision 1.1  1995/01/23 18:43:27  craigs
  * Initial revision
  *
@@ -17,12 +20,16 @@
 
 #pragma interface
 
+#include <termio.h>
+
 #include "../../common/serchan.h"
   public:
     BOOL Close();
 
   private:
-    struct termio * Termio;
+    struct termio oldTermio;
+    struct termio Termio;
+
     DWORD  baudRate;
     BYTE   dataBits;
     Parity parityBits;
