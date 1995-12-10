2e8e6ba9fa29ace6b59f29a36281fc11eed6f1f0
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1995-12-10 11:47:14 +0000

    svnimporter: adding tags and branches to revision 747

---
+++ include/ptlib/msos/ptlib/config.h
@@ -0,0 +1,19 @@
+/*
+ * $Id: config.h,v 1.3 1995/12/10 11:47:14 robertj Exp $
+ */
+
+
+#ifndef _PCONFIG
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PConfiguration
+
+#include "../../common/config.h"
+  protected:
+    Source  source;
+    PString location;
+};
+
+
+#endif
