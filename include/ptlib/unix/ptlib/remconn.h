4d72312054d5beab1470b2167b207b5c0838c978
Craig Southeren <craigs@postincrement.com>
1996-01-26 11:06:31 +0000

    Initial revision

---
+++ include/ptlib/unix/ptlib/remconn.h
@@ -0,0 +1,23 @@
+/*
+ * $Id: remconn.h,v 1.1 1996/01/26 11:06:31 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: remconn.h,v $
+ * Revision 1.1  1996/01/26 11:06:31  craigs
+ * Initial revision
+ *
+ */
+
+#ifndef _PREMOTECONNECTION
+
+#pragma interface
+
+#include "../../common/remconn.h"
+};
+
+#endif
---
+++ include/ptlib/unix/ptlib/semaphor.h
@@ -0,0 +1,23 @@
+/*
+ * $Id: semaphor.h,v 1.1 1996/01/26 11:06:31 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: semaphor.h,v $
+ * Revision 1.1  1996/01/26 11:06:31  craigs
+ * Initial revision
+ *
+ */
+
+#ifndef _PSEMAPHORE
+
+#pragma interface
+
+#include "../../common/semaphor.h"
+};
+
+#endif
