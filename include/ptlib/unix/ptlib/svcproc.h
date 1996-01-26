e9828897432eb3543d0f3cf67495dc38e401224d
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-01-26 11:06:31 +0000

    Initial revision

---
+++ include/ptlib/unix/ptlib/svcproc.h
@@ -0,0 +1,24 @@
+/*
+ * $Id: svcproc.h,v 1.1 1996/01/26 11:06:31 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: svcproc.h,v $
+ * Revision 1.1  1996/01/26 11:06:31  craigs
+ * Initial revision
+ *
+ */
+
+#ifndef _PSERVICEPROCESS
+
+#pragma interface
+
+#include "../../common/svcproc.h"
+};
+
+
+#endif
