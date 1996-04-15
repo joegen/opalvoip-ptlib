f273cbb59c695a979be1f8ee3b525ba22972bc4a
Craig Southeren <craigs@postincrement.com>
1996-04-15 10:50:48 +0000

    Last revision prior to release of MibMaster

--- include/ptlib/unix/ptlib/pipechan.h
+++ include/ptlib/unix/ptlib/pipechan.h
@@ -1,5 +1,5 @@
 /*
- * $Id: pipechan.h,v 1.1 1995/01/23 18:43:27 craigs Exp $
+ * $Id: pipechan.h,v 1.2 1996/04/15 10:50:48 craigs Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: pipechan.h,v $
+ * Revision 1.2  1996/04/15 10:50:48  craigs
+ * Last revision prior to release of MibMaster
+ *
  * Revision 1.1  1995/01/23 18:43:27  craigs
  * Initial revision
  *
@@ -17,11 +20,22 @@
 
 #pragma interface
 
+#include <signal.h>
+
 #include "../../common/pipechan.h"
+  public:
+    void PXWaitForTerminate();
+    void PXKill(int killType = SIGTERM);
+    
   protected:
     int toChildPipe[2];
     int fromChildPipe[2];
     int childPid;
+    int retVal;
 };
+
+#ifdef P_USE_INLINES
+#include "../../common/pipechan.inl"
+#endif
 
 #endif
