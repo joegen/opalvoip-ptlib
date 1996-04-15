0ee9b7fb650daba3d92982b45c48d5ae59b29b97
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-04-15 10:49:11 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@1219 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ src/ptlib/unix/switch.cxx
@@ -0,0 +1,22 @@
+#include <ptlib.h>
+
+void PThread::SwitchContext(PThread * from)
+{
+  if (setjmp(from->context) != 0) // Are being reactivated from previous yield
+    return;
+
+  if (status == Starting) {
+    if (setjmp(context) != 0) {
+      status = Running;
+      Main();
+      Terminate(); // Never returns from here
+    }
+#if defined(P_LINUX)
+    context[0].__sp = (__ptr_t)stackTop-16;  // Change the stack pointer in jmp_buf
+#else
+#warning No lightweight thread context switch mechanism defined
+#endif
+  }
+  longjmp(context, TRUE);
+  PAssertAlways("longjmp failed"); // Should never get here
+}
