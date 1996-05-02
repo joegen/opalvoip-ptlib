19fe9003187ce900d5ef45f397d4ab1dfeaea559
Craig Southeren <craigs@postincrement.com>
1996-05-02 13:34:22 +0000

    More Sun4 fixes

--- include/ptlib/unix/ptlib/socket.h
+++ include/ptlib/unix/ptlib/socket.h
@@ -1,5 +1,5 @@
 /*
- * $Id: socket.h,v 1.5 1996/05/02 12:28:03 craigs Exp $
+ * $Id: socket.h,v 1.6 1996/05/02 13:34:22 craigs Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: socket.h,v $
+ * Revision 1.6  1996/05/02 13:34:22  craigs
+ * More Sun4 fixes
+ *
  * Revision 1.5  1996/05/02 12:28:03  craigs
  * More Sun4 fixes
  *
@@ -31,13 +34,20 @@
 
 #pragma interface
 
+#include <netinet/in.h>
+
+#if 0
 #include <fcntl.h>
 #include <termio.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <signal.h>
 #include <netdb.h>
-#include <netinet/in.h>
+#endif
+
+#if defined(P_SUN4)
+#include <errno.h>
+#endif
 
 ///////////////////////////////////////////////////////////////////////////////
 // PSocket
