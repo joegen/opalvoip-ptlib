7cc6aec28d3c3f25112f6802b32e782fea47a225
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-05-02 11:55:01 +0000

    Fixed problem with compiling on Sun4
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@1280 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/unix/ptlib/serchan.h
+++ include/ptlib/unix/ptlib/serchan.h
@@ -1,5 +1,5 @@
 /*
- * $Id: serchan.h,v 1.2 1995/12/08 13:14:52 craigs Exp $
+ * $Id: serchan.h,v 1.3 1996/05/02 11:55:01 craigs Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: serchan.h,v $
+ * Revision 1.3  1996/05/02 11:55:01  craigs
+ * Fixed problem with compiling on Sun4
+ *
  * Revision 1.2  1995/12/08 13:14:52  craigs
  * Added new function
  *
@@ -20,15 +23,39 @@
 
 #pragma interface
 
-#include <termio.h>
+#include <sys/ioctl.h>
+
+#ifdef P_SUN4
+#undef NL0
+#undef NL1
+#undef CR0
+#undef CR1
+#undef CR2
+#undef CR3
+#undef TAB0
+#undef TAB1
+#undef TAB2
+#undef XTABS
+#undef BS0
+#undef BS1
+#undef FF0
+#undef FF1
+#undef ECHO
+#undef NOFLSH
+#undef TOSTOP
+#undef FLUSHO
+#undef PENDIN
+#endif
+
+#include <sys/termios.h>
 
 #include "../../common/serchan.h"
   public:
     BOOL Close();
 
   private:
-    struct termio oldTermio;
-    struct termio Termio;
+    struct termios oldTermio;
+    struct termios Termio;
 
     DWORD  baudRate;
     BYTE   dataBits;
