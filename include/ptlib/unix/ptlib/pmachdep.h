abf94a225ae23200c58a6ea901a937a0934b6a15
Craig Southeren <craigs@postincrement.com>
1996-05-03 13:12:07 +0000

    Initial revision

---
+++ include/ptlib/unix/ptlib/pmachdep.h
@@ -0,0 +1,77 @@
+#ifndef PMACHDEP_H
+#define PMACHDEP_H
+
+
+#if ! defined (P_SUN4)
+
+#include <sys/termios.h>
+
+#define PSETPGRP()  setpgrp()
+
+#else
+
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+
+extern "C" {
+char *mktemp(char *);
+int accept(int, struct sockaddr *, int *);
+int connect(int, struct sockaddr *, int);
+int ioctl(int, int, void *);
+int recv(int, void *, int, int);
+int recvfrom(int, void *, int, int, struct sockaddr *, int *);
+int select(int, fd_set *, fd_set *, fd_set *, struct timeval *);
+int sendto(int, const void *, int, int, const struct sockaddr *, int);
+int send(int, const void *, int, int);
+int shutdown(int, int);
+int socket(int, int, int);
+int vfork();
+void bzero(void *, int);
+void closelog();
+void gettimeofday(struct timeval * tv, struct timezone * tz);
+void openlog(const char *, int, int);
+void syslog(int, char *, ...);
+int setpgrp(int, int);
+pid_t wait3(int *status, int options, struct rusage *rusage);
+int bind(int, struct sockaddr *, int);
+int listen(int, int);
+int getsockopt(int, int, int, char *, int *);
+int setsockopt(int, int, int, char *, int);
+int getpeername(int, struct sockaddr *, int *);
+int gethostname(char *, int);
+int getsockname(int, struct sockaddr *, int *);
+char * inet_ntoa(struct in_addr);
+
+struct hostent * gethostbyname(const char *);
+struct hostent * gethostbyaddr(const char *, int, int);
+
+#define PSETPGRP()  setpgrp(0, 0)
+
+#include <sys/termios.h>
+#undef NL0
+#undef NL1
+#undef CR0
+#undef CR1
+#undef CR2
+#undef CR3
+#undef TAB0
+#undef TAB1
+#undef TAB2
+#undef XTABS
+#undef BS0
+#undef BS1
+#undef FF0
+#undef FF1
+#undef ECHO
+#undef NOFLSH
+#undef TOSTOP
+#undef FLUSHO
+#undef PENDIN
+#endif
+
+};
+
+#endif
+
