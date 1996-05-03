e60a031a25d8cb299812f9710d6ac5ed4737f76f
Craig Southeren <craigs@postincrement.com>
1996-05-03 13:21:08 +0000

    Fix for Sun4

--- src/ptlib/unix/switch.cxx
+++ src/ptlib/unix/switch.cxx
@@ -15,10 +15,10 @@ void PThread::SwitchContext(PThread * from)
 #if defined(P_LINUX)
     context[0].__sp = (__ptr_t)stackTop-16;  // Change the stack pointer in jmp_buf
 #elif defined (P_SUN4)
-    context[2] = (__ptr_t)stackTop-1024;  // Change the stack pointer in jmp_buf
+    context[2] = (int)(stackTop-1024);  // Change the stack pointer in jmp_buf
     context[2] &= 7;
 #elif defined (P_SOLARIS)
-    context[1] = (__ptr_t)stackTop-1024;  // Change the stack pointer in jmp_buf
+    context[1] = (int)(stackTop-1024);  // Change the stack pointer in jmp_buf
     context[1] &= 7;
 #else
 #warning No lightweight thread context switch mechanism defined
