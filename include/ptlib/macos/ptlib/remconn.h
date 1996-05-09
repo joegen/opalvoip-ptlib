99dfa4fd51c923e48cc51e92712332eaca4ee04c
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-05-09 12:23:04 +0000

    Further implementation.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@1331 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/macos/ptlib/remconn.h
+++ include/ptlib/macos/ptlib/remconn.h
@@ -1,11 +1,14 @@
 /*
- * $Id: remconn.h,v 1.1 1996/01/02 13:10:31 robertj Exp $
+ * $Id: remconn.h,v 1.2 1996/05/09 12:23:04 robertj Exp $
  *
  * Portable Windows Library
  *
  * Copyright 1993 Equivalence
  *
  * $Log: remconn.h,v $
+ * Revision 1.2  1996/05/09 12:23:04  robertj
+ * Further implementation.
+ *
  * Revision 1.1  1996/01/02 13:10:31  robertj
  * Initial revision
  *
@@ -14,7 +17,7 @@
 #ifndef _PREMOTECONNECTION
 
 
-#include "../../common/remconn.h"
+#include "::common:remconn.h"
 };
 
 
