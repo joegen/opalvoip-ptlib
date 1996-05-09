068090358765ed65db6b07a67908558e0e622fb8
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-05-09 10:55:16 +0000

    More SunOS fixes
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@1305 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/unix/switch.cxx
+++ src/ptlib/unix/switch.cxx
@@ -1,29 +1,69 @@
 #include <ptlib.h>
 
+static volatile PThread * localThis;
+
 void PThread::SwitchContext(PThread * from)
 {
+  /////////////////////////////////////////////////
+  //
+  //  no need to switch to ourselves
+  //
+  /////////////////////////////////////////////////
+  if (this == from)
+    return;
+
+  /////////////////////////////////////////////////
+  //
+  //  save context for old thread
+  //
+  /////////////////////////////////////////////////
+//#if defined (P_SUN4)
+//  __asm__ ("ta 3");
+//#endif
   if (setjmp(from->context) != 0) // Are being reactivated from previous yield
     return;
 
+  /////////////////////////////////////////////////
+  //
+  //  if starting the current thread, create a context, give it a new stack
+  //  and then switch to it.
+  //  if we have just switched into a new thread, change status to Running
+  //  and execute the Main and Terminate functions
+  //
+  /////////////////////////////////////////////////
   if (status == Starting) {
+    localThis = this;
+    if (setjmp(context) != 0) 
+      localThis->BeginThread();
 
-    if (setjmp(context) != 0) {
-      status = Running;
-      Main();
-      Terminate(); // Never returns from here
-    }
+  /////////////////////////////////////////////////
+  //
+  // Change the stack pointer in the jmp_buf to point
+  // to the new stack
+  //
+  /////////////////////////////////////////////////
+
+
 #if defined(P_LINUX)
-    context[0].__sp = (__ptr_t)stackTop-16;  // Change the stack pointer in jmp_buf
+    context[0].__sp = (__ptr_t)stackTop-16;
 #elif defined (P_SUN4)
-    context[2] = (int)(stackTop-1024);  // Change the stack pointer in jmp_buf
-    context[2] &= 7;
+    context[2] = (int)stackTop-1024;
+    context[2] &= ~7;
 #elif defined (P_SOLARIS)
-    context[1] = (int)(stackTop-1024);  // Change the stack pointer in jmp_buf
-    context[1] &= 7;
+    context[1] = (int)stackTop-1024;
+    context[1] &= ~7;
+#elif defined (P_ULTRIX)
+    context[JB_SP] = (int)(stackTop-16);
 #else
 #warning No lightweight thread context switch mechanism defined
 #endif
   }
+
+  /////////////////////////////////////////////////
+  //
+  //  switch to the new thread
+  //
+  /////////////////////////////////////////////////
   longjmp(context, TRUE);
   PAssertAlways("longjmp failed"); // Should never get here
 }
