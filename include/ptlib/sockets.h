57ef3e0694831ee6ef7de623d319b1e75f3bb8cb
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-05-15 10:13:15 +0000

    svnimporter: adding tags and branches to revision 1345

---
+++ include/ptlib/sockets.h
@@ -0,0 +1,99 @@
+/*
+ * $Id: sockets.h,v 1.6 1996/05/15 10:13:15 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ *
+ * $Log: sockets.h,v $
+ * Revision 1.6  1996/05/15 10:13:15  robertj
+ * Added ICMP protocol socket, getting common ancestor to UDP.
+ *
+ * Revision 1.5  1995/06/04 12:36:55  robertj
+ * Added application layer protocol sockets.
+ *
+ * Revision 1.4  1994/08/23 11:32:52  robertj
+ * Oops
+ *
+ * Revision 1.3  1994/08/22  00:46:48  robertj
+ * Added pragma fro GNU C++ compiler.
+ *
+ * Revision 1.2  1994/08/21  23:43:02  robertj
+ * Added telnet.
+ *
+ * Revision 1.1  1994/07/25  03:36:03  robertj
+ * Initial revision
+ *
+ * Revision 1.3  1994/07/21  12:17:41  robertj
+ * Sockets.
+ *
+ * Revision 1.2  1994/06/25  12:27:39  robertj
+ * *** empty log message ***
+ *
+ * Revision 1.1  1994/04/01  14:38:42  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _SOCKETS_H
+#define _SOCKETS_H
+
+#ifdef __GNUC__
+#pragma interface
+#endif
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PSocket
+
+#include <socket.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PIPSocket
+
+#include <ipsock.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PIPDatagramSocket
+
+#include <ipdsock.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PUDPSocket
+
+#include <udpsock.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PICMPSocket
+
+#include <icmpsock.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PTCPSocket
+
+#include <tcpsock.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PTelnetSocket
+
+#include <telnet.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PApplicationSocket
+
+#include <appsock.h>
+
+
+#endif // _SOCKETS_H
+
+
+// End Of File ///////////////////////////////////////////////////////////////
