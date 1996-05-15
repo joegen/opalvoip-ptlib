9ebc2d47de3e893d8f1b14d0324438bb0bb5efb5
Robert Jongbloed <robertj@voxlucida.com.au>
1996-05-15 10:19:15 +0000

    Added ICMP protocol socket, getting common ancestor to UDP.

--- include/ptlib/udpsock.h
+++ include/ptlib/udpsock.h
@@ -1,5 +1,5 @@
 /*
- * $Id: udpsock.h,v 1.9 1996/03/03 07:38:00 robertj Exp $
+ * $Id: udpsock.h,v 1.10 1996/05/15 10:19:15 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: udpsock.h,v $
+ * Revision 1.10  1996/05/15 10:19:15  robertj
+ * Added ICMP protocol socket, getting common ancestor to UDP.
+ *
  * Revision 1.9  1996/03/03 07:38:00  robertj
  * Added Reusability clause to the Listen() function on sockets.
  *
@@ -40,9 +43,8 @@
 #pragma interface
 #endif
 
-
-PDECLARE_CLASS(PUDPSocket, PIPSocket)
-/* Create a socket checnnel that uses the UDP transport on the Internal
+PDECLARE_CLASS(PUDPSocket, PIPDatagramSocket)
+/* Create a socket channel that uses the UDP transport on the Internal
    Protocol.
  */
 
@@ -65,57 +67,6 @@ PDECLARE_CLASS(PUDPSocket, PIPSocket)
        a "listening" socket is specified then the channel is also opened.
      */
 
-
-  // Overrides from class PSocket.
-    virtual BOOL Connect(
-      const PString & address   // Address of remote machine to connect to.
-    );
-    /* Connect a socket to a remote host on the specified port number. This is
-       typically used by the client or initiator of a communications channel.
-       This connects to a "listening" socket at the other end of the
-       communications channel.
-
-       The port number as defined by the object instance construction or the
-       <A>PIPSocket::SetPort()</A> function.
-
-       <H2>Returns:</H2>
-       TRUE if the channel was successfully connected to the remote host.
-     */
-
-
-    virtual BOOL Listen(
-      unsigned queueSize = 5,  // Number of pending accepts that may be queued.
-      WORD port = 0,           // Port number to use for the connection.
-      Reusability reuse = AddressIsExclusive // Can/Cant listen more than once.
-    );
-    /* Listen on a socket for a remote host on the specified port number. This
-       may be used for server based applications. A "connecting" socket begins
-       a connection by initiating a connection to this socket. An active socket
-       of this type is then used to generate other "accepting" sockets which
-       establish a two way communications channel with the "connecting" socket.
-
-       If the <CODE>port</CODE> parameter is zero then the port number as
-       defined by the object instance construction or the
-       <A>PIPSocket::SetPort()</A> function.
-
-       For the UDP protocol, the <CODE>queueSize</CODE> parameter is ignored.
-
-       <H2>Returns:</H2>
-       TRUE if the channel was successfully opened.
-     */
-
-    virtual BOOL Accept(
-      PSocket & socket          // Listening socket making the connection.
-    );
-    /* Open a socket to a remote host on the specified port number.
-
-       You cannot accept on a UDP socket, so this function asserts.
-
-       <H2>Returns:</H2>
-       TRUE if the channel was successfully opened.
-     */
-
-
   // Overrides from class PIPSocket.
     virtual WORD GetPortByService(
       const PString & service   // Name of service to get port number for.
@@ -134,33 +85,5 @@ PDECLARE_CLASS(PUDPSocket, PIPSocket)
        <H2>Returns:</H2>
        Service name for port number.
      */
-
-
-  // New functions for class
-    virtual BOOL ReadFrom(
-      void * buf,     // Data to be written as URGENT TCP data.
-      PINDEX len,     // Number of bytes pointed to by <CODE>buf</CODE>.
-      Address & addr, // Address from which the datagram was received.
-      WORD & port     // Port from which the datagram was received.
-    );
-    /* Read a datagram from a remote computer.
-       
-       <H2>Returns:</H2>
-       TRUE if all the bytes were sucessfully written.
-     */
-
-
-    virtual BOOL WriteTo(
-      const void * buf,   // Data to be written as URGENT TCP data.
-      PINDEX len,         // Number of bytes pointed to by <CODE>buf</CODE>.
-      const Address & addr, // Address to which the datagram is sent.
-      WORD port           // Port to which the datagram is sent.
-    );
-    /* Write a datagram to a remote computer.
-
-       <H2>Returns:</H2>
-       TRUE if all the bytes were sucessfully written.
-     */
-
 
 // Class declaration continued in platform specific header file ///////////////
