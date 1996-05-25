0ad672520862374ce7288c13188f0c59eba4eaae
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-05-25 06:07:23 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@1399 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ include/ptlib/unix/ptlib/ipdsock.h
@@ -0,0 +1,26 @@
+/*
+ * $Id: ipdsock.h,v 1.1 1996/05/25 06:07:23 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: ipdsock.h,v $
+ * Revision 1.1  1996/05/25 06:07:23  craigs
+ * Initial revision
+ *
+ */
+
+#ifndef _PIPDATAGRAMSOCKET
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PIPDatagramSocket
+
+#include "../../common/ipdsock.h"
+};
+
+#endif
