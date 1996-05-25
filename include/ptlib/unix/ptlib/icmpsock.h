11dac597cfbab6a029fc13619fd118b7434140a6
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-05-25 06:07:23 +0000

    svnimporter: adding tags and branches to revision 1398

---
+++ include/ptlib/unix/ptlib/icmpsock.h
@@ -0,0 +1,27 @@
+
+/*
+ * $Id: icmpsock.h,v 1.1 1996/05/25 06:07:23 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: icmpsock.h,v $
+ * Revision 1.1  1996/05/25 06:07:23  craigs
+ * Initial revision
+ *
+ */
+
+#ifndef _PICMPSOCKET
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PICMPSocket
+
+#include "../../common/icmpsock.h"
+};
+
+#endif
