586ff52b52da451d44dfa7bc601203eca3476373
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-06-03 10:03:10 +0000

    svnimporter: adding tags and branches to revision 1443

---
+++ src/ptlib/msos/icmp.cxx
@@ -0,0 +1,184 @@
+/*
+ * $Id: icmp.cxx,v 1.3 1996/06/03 10:03:10 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Socket Classes Implementation
+ *
+ * Copyright 1996 Equivalence
+ *
+ * $Log: icmp.cxx,v $
+ * Revision 1.3  1996/06/03 10:03:10  robertj
+ * Changed ping to return more parameters.
+ *
+ * Revision 1.2  1996/05/30 10:08:51  robertj
+ * Fixed bug in ping (checksum incorrect).
+ *
+ * Revision 1.1  1996/05/15 21:11:35  robertj
+ * Initial revision
+ *
+ */
+
+#include <ptlib.h>
+#include <sockets.h>
+
+
+#define	MAX_IP_LEN	60
+#define	MAX_ICMP_LEN	76
+#define	ICMP_DATA_LEN	(64-8)
+#define	RX_BUFFER_SIZE	(MAX_IP_LEN+MAX_ICMP_LEN+ICMP_DATA_LEN)
+
+#define	ICMP_ECHO	8
+
+
+typedef struct {
+  BYTE   type;
+  BYTE   code;
+  WORD   checksum;
+
+  WORD   id;
+  WORD   sequence;
+
+  PInt64 sendtime;
+  BYTE   data[ICMP_DATA_LEN-sizeof(PInt64)];
+} ICMPPacket;
+
+
+typedef struct {
+  BYTE verIhl;
+  BYTE typeOfService;
+  BYTE totalLength;
+  WORD identification;
+  WORD fragOff;
+  BYTE timeToLive;
+  BYTE protocol;
+  WORD checksum;
+  BYTE sourceAddr[4];
+  BYTE destAddr[4];
+} IPHdr;
+
+
+static WORD CalcChecksum(void * p, PINDEX len)
+{
+  WORD * ptr = (WORD *)p;
+  DWORD sum = 0;
+  while (len > 1) {
+    sum += *ptr++;
+    len-=2;
+  }
+
+  if (len > 0) {
+    WORD t = *(BYTE *)ptr;
+    sum += t;
+  }
+
+  sum = (sum >> 16) + (sum & 0xffff);
+  sum += (sum >> 16);
+  return (WORD)~sum;
+}
+
+
+PICMPSocket::PICMPSocket()
+  : PIPDatagramSocket(SOCK_RAW, "icmp")
+{
+  _Socket();
+}
+
+
+BOOL PICMPSocket::Ping(const PString & host)
+{
+  PingInfo info;
+  return Ping(host, info);
+}
+
+
+BOOL PICMPSocket::Ping(const PString & host, PingInfo & info)
+{
+  if (!WritePing(host, info))
+    return FALSE;
+
+  return ReadPing(info);
+}
+
+
+BOOL PICMPSocket::WritePing(const PString & host, PingInfo & info)
+{
+  // find address of the host
+  PIPSocket::Address addr;
+  if (!GetHostAddress(host, addr)) {
+    lastError = BadParameter;
+    return FALSE;
+  }
+
+  // create the ICMP packet
+  ICMPPacket packet;
+
+  // clear the packet including data area
+  memset(&packet, 0, sizeof(packet));
+
+  packet.type     = ICMP_ECHO;
+  packet.sequence = info.sequenceNum;
+  packet.id       = info.identifier;
+
+  // set the send time
+  packet.sendtime = PTimer::Tick().GetMilliSeconds();
+
+  // calculate the checksum
+  packet.checksum = CalcChecksum(&packet, sizeof(packet));
+
+  // send the packet
+  return WriteTo(&packet, sizeof(packet), addr, 0);
+}
+
+
+BOOL PICMPSocket::ReadPing(PingInfo & info)
+{
+  // receive a packet
+  BYTE packet[RX_BUFFER_SIZE];
+  IPHdr      * ipHdr;
+  ICMPPacket * icmpPacket;
+  WORD port;
+  PInt64 now;
+
+  for (;;) {
+    if (!ReadFrom(packet, sizeof(packet), info.remoteAddr, port))
+      return FALSE;
+    now  = PTimer::Tick().GetMilliSeconds();
+    ipHdr      = (IPHdr *)packet;
+    icmpPacket = (ICMPPacket *)(packet + ((ipHdr->verIhl & 0xf) << 2));
+    if (info.identifier == (WORD)icmpPacket->id)
+      break;
+  }
+
+  info.remoteAddr = Address(ipHdr->sourceAddr[0], ipHdr->sourceAddr[1],
+                            ipHdr->sourceAddr[2], ipHdr->sourceAddr[3]);
+  info.localAddr  = Address(ipHdr->destAddr[0], ipHdr->destAddr[1],
+                            ipHdr->destAddr[2], ipHdr->destAddr[3]);
+
+  // calc round trip time. Be careful, as unaligned "long long" ints
+  // can cause problems on some platforms
+#ifdef P_SUN4
+  PInt64 then;
+  memcpy(&then, &icmpPacket->sendtime, sizeof(PInt64));
+  info.delay.SetInterval(now - then);
+#else
+  info.delay.SetInterval(now - icmpPacket->sendtime);
+#endif
+
+  info.sequenceNum = icmpPacket->sequence;
+  return TRUE;
+}
+
+
+WORD PICMPSocket::GetPortByService(const PString &) const
+{
+  PAssertAlways(PUnsupportedFeature);
+  return 0;
+}
+
+
+PString PICMPSocket::GetServiceByPort(WORD) const
+{
+  PAssertAlways(PUnsupportedFeature);
+  return PString();
+}
