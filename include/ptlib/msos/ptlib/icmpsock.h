8b89d6a4dc6179b3f5884b793450a004a22473db
Robert Jongbloed <robertj@voxlucida.com.au>
1996-06-29 04:05:03 +0000

    Initial revision

---
+++ include/ptlib/msos/ptlib/icmpsock.h
@@ -0,0 +1,26 @@
+/*
+ * $Id: icmpsock.h,v 1.1 1996/06/29 04:05:03 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * IP Datagram Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: icmpsock.h,v $
+ * Revision 1.1  1996/06/29 04:05:03  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PICMPSOCKET
+
+
+#include "../../common/icmpsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
---
+++ include/ptlib/msos/ptlib/ipdsock.h
@@ -0,0 +1,26 @@
+/*
+ * $Id: ipdsock.h,v 1.1 1996/06/29 04:05:03 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * IP Datagram Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: ipdsock.h,v $
+ * Revision 1.1  1996/06/29 04:05:03  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PIPDATAGRAMSOCKET
+
+
+#include "../../common/ipdsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
