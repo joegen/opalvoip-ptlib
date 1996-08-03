0373e4298f63fac627bbd210abdb1983d8a30eb9
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-03 12:09:51 +0000

    svnimporter: adding tags and branches to revision 1593

---
+++ include/ptlib/unix/ptlib/textfile.h
@@ -0,0 +1,33 @@
+/*
+ * $Id: textfile.h,v 1.2 1996/08/03 12:09:51 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: textfile.h,v $
+ * Revision 1.2  1996/08/03 12:09:51  craigs
+ * Changed for new common directories
+ *
+ * Revision 1.1  1995/01/23 18:43:27  craigs
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PTEXTFILE
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PTextFile
+
+#include "../../common/ptlib/textfile.h"
+};
+
+
+#endif
