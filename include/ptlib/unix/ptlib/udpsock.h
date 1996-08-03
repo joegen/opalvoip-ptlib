c4fb9e721ee009e5c848ad58b7755cd999db030e
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-03 12:10:23 +0000

    svnimporter: adding tags and branches to revision 1595

---
+++ include/ptlib/unix/ptlib/udpsock.h
@@ -0,0 +1,33 @@
+
+/*
+ * $Id: udpsock.h,v 1.2 1996/08/03 12:10:23 craigs Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: udpsock.h,v $
+ * Revision 1.2  1996/08/03 12:10:23  craigs
+ * Changed for new common directories
+ *
+ * Revision 1.1  1995/01/23 18:43:27  craigs
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PUDPSOCKET
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PUDPSocket
+
+#include "../../common/ptlib/udpsock.h"
+};
+
+#endif
