9d6c2cf42d094aef3e7a40e2d9fcdb5abd340aa9
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:09:21 +0000

    svnimporter: adding tags and branches to revision 1663

---
+++ include/ptlib/msos/ptlib/timer.h
@@ -0,0 +1,32 @@
+/*
+ * $Id: timer.h,v 1.2 1996/08/08 10:09:21 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: timer.h,v $
+ * Revision 1.2  1996/08/08 10:09:21  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1994/06/25 12:13:01  robertj
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PTIMER
+
+///////////////////////////////////////////////////////////////////////////////
+// PTimer
+
+#include "../../common/ptlib/timer.h"
+};
+
+
+#endif
