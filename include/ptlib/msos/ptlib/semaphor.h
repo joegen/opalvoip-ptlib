8bc66bbd0b11d8d5289181ab44790d775993c0f2
Robert Jongbloed <robertj@voxlucida.com.au>
1996-08-08 10:09:11 +0000

    Directory structure changes for common files.

--- include/ptlib/msos/ptlib/semaphor.h
+++ include/ptlib/msos/ptlib/semaphor.h
@@ -1,5 +1,5 @@
 /*
- * $Id: semaphor.h,v 1.2 1996/06/13 13:32:10 robertj Exp $
+ * $Id: semaphor.h,v 1.3 1996/08/08 10:09:11 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: semaphor.h,v $
+ * Revision 1.3  1996/08/08 10:09:11  robertj
+ * Directory structure changes for common files.
+ *
  * Revision 1.2  1996/06/13 13:32:10  robertj
  * Rewrite of auto-delete threads, fixes Windows95 total crash.
  *
@@ -23,7 +26,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // PSemaphore
 
-#include "../../common/semaphor.h"
+#include "../../common/ptlib/semaphor.h"
 #if defined(P_PLATFORM_HAS_THREADS)
   public:
     HANDLE GetHandle() const { return hSemaphore; }
