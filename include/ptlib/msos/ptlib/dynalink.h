f87f1585e114e3a3dfb295ddff958f29e94ed333
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:08:59 +0000

    Directory structure changes for common files.

--- include/ptlib/msos/ptlib/dynalink.h
+++ include/ptlib/msos/ptlib/dynalink.h
@@ -1,5 +1,5 @@
 /*
- * $Id: dynalink.h,v 1.1 1995/03/14 12:44:49 robertj Exp $
+ * $Id: dynalink.h,v 1.2 1996/08/08 10:08:59 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: dynalink.h,v $
+ * Revision 1.2  1996/08/08 10:08:59  robertj
+ * Directory structure changes for common files.
+ *
  * Revision 1.1  1995/03/14 12:44:49  robertj
  * Initial revision
  *
@@ -16,7 +19,7 @@
 #ifndef _PDYNALINK
 
 
-#include "../../common/dynalink.h"
+#include "../../common/ptlib/dynalink.h"
   protected:
 #if defined(_WINDOWS) || defined(_WIN32)
     HINSTANCE _hDLL;
