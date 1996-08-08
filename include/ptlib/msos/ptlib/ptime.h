7e8350b14540cb1443c350f4c4266f422f1d6aa5
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:09:09 +0000

    svnimporter: adding tags and branches to revision 1643

---
+++ include/ptlib/msos/ptlib/ptime.h
@@ -0,0 +1,31 @@
+/*
+ * $Id: ptime.h,v 1.2 1996/08/08 10:09:09 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: ptime.h,v $
+ * Revision 1.2  1996/08/08 10:09:09  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1994/06/25 12:13:01  robertj
+ * Initial revision
+ *
+ * Revision 1.1  1994/04/12  08:31:05  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PTIME
+
+///////////////////////////////////////////////////////////////////////////////
+// PTime
+
+#include "../../common/ptlib/ptime.h"
+};
+
+
+#endif
