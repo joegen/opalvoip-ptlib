3560f92accab6e4ecb03bdcde1881263823745ff
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:10:45 +0000

    svnimporter: adding tags and branches to revision 1668

---
+++ include/ptlib/msos/ptlib/sound.h
@@ -0,0 +1,29 @@
+/*
+ * $Id: sound.h,v 1.2 1996/08/08 10:10:45 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: sound.h,v $
+ * Revision 1.2  1996/08/08 10:10:45  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1994/04/12 08:21:52  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PSOUND
+
+///////////////////////////////////////////////////////////////////////////////
+// PSound
+
+#include "../../common/ptlib/sound.h"
+};
+
+
+#endif
