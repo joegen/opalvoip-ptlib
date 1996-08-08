cc881febd587e2c2b174f8145ce1255cb5ff44e7
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:09:22 +0000

    svnimporter: adding tags and branches to revision 1665

---
+++ include/ptlib/msos/ptlib/udpsock.h
@@ -0,0 +1,29 @@
+/*
+ * $Id: udpsock.h,v 1.2 1996/08/08 10:09:22 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * UDP Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: udpsock.h,v $
+ * Revision 1.2  1996/08/08 10:09:22  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1994/07/27 06:00:10  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PUDPSOCKET
+
+
+#include "../../common/ptlib/udpsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
