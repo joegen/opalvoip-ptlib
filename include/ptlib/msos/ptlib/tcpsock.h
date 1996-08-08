b5066749ace53295ca023288e4d070a2728874f1
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:09:17 +0000

    svnimporter: adding tags and branches to revision 1656

---
+++ include/ptlib/msos/ptlib/tcpsock.h
@@ -0,0 +1,29 @@
+/*
+ * $Id: tcpsock.h,v 1.2 1996/08/08 10:09:17 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * TCP Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: tcpsock.h,v $
+ * Revision 1.2  1996/08/08 10:09:17  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1994/07/27 06:00:10  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PTCPSOCKET
+
+
+#include "../../common/ptlib/tcpsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
