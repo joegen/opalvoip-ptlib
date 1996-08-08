fb327a629415fda1382fe34ceaf99c5ec3933204
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:09:02 +0000

    svnimporter: adding tags and branches to revision 1630

---
+++ include/ptlib/msos/ptlib/icmpsock.h
@@ -0,0 +1,29 @@
+/*
+ * $Id: icmpsock.h,v 1.2 1996/08/08 10:09:02 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * IP Datagram Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: icmpsock.h,v $
+ * Revision 1.2  1996/08/08 10:09:02  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1996/06/29 04:05:03  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PICMPSOCKET
+
+
+#include "../../common/ptlib/icmpsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
