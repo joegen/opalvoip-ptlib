658fcc8553a3c2f689be14d30a126fd5828dcd76
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:08:56 +0000

    svnimporter: adding tags and branches to revision 1619

---
+++ include/ptlib/msos/ptlib/channel.h
@@ -0,0 +1,29 @@
+/*
+ * $Id: channel.h,v 1.2 1996/08/08 10:08:56 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Operating System Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: channel.h,v $
+ * Revision 1.2  1996/08/08 10:08:56  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1994/07/02 03:18:09  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PCHANNEL
+
+///////////////////////////////////////////////////////////////////////////////
+// PChannel
+
+#include "../../common/ptlib/channel.h"
+};
+
+
+#endif
