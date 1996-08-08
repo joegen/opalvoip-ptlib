00656e4b1ed93497693716c1e46220d5a10ee1ca
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-08 10:09:22 +0000

    Directory structure changes for common files.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@1666 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/msos/ptlib/udpsock.h
+++ include/ptlib/msos/ptlib/udpsock.h
@@ -1,5 +1,5 @@
 /*
- * $Id: udpsock.h,v 1.1 1994/07/27 06:00:10 robertj Exp $
+ * $Id: udpsock.h,v 1.2 1996/08/08 10:09:22 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: udpsock.h,v $
+ * Revision 1.2  1996/08/08 10:09:22  robertj
+ * Directory structure changes for common files.
+ *
  * Revision 1.1  1994/07/27 06:00:10  robertj
  * Initial revision
  *
@@ -16,7 +19,7 @@
 #ifndef _PUDPSOCKET
 
 
-#include "../../common/udpsock.h"
+#include "../../common/ptlib/udpsock.h"
 };
 
 
