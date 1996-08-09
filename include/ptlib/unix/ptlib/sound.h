695b45772a40402cc1deca49dceae89eda76fae9
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-08-09 12:16:09 +0000

    svnimporter: adding tags and branches to revision 1676

---
+++ include/ptlib/unix/ptlib/sound.h
@@ -0,0 +1,16 @@
+/*
+ * $Id: sound.h,v 1.1 1996/08/09 12:16:09 craigs Exp $
+ */
+
+#ifndef _PSOUND
+
+#pragma interface
+
+///////////////////////////////////////////////////////////////////////////////
+// PSound
+
+#include "../../common/ptlib/sound.h"
+};
+
+
+#endif
