7e069204cc8dae041ab02cd70ec74b573a40a0ce
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-09-14 13:19:35 +0000

    svnimporter: adding tags and branches to revision 1817

---
+++ include/ptlib/msos/ptlib/ipxsock.h
@@ -0,0 +1,28 @@
+/*
+ * $Id: ipxsock.h,v 1.1 1996/09/14 13:19:35 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Internet Protocol Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: ipxsock.h,v $
+ * Revision 1.1  1996/09/14 13:19:35  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PIPXSOCKET
+
+#include <wsipx.h>
+#include <wsnwlink.h>
+
+#include "../../common/ptlib/ipxsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
