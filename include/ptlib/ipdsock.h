5115d88c70417f4965201fa6408dc9ab54c7a6d8
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-09-14 13:09:20 +0000

    Major upgrade:   rearranged sockets to help support IPX.   added indirect channel class and moved all protocols to descend from it,   separating the protocol from the low level byte transport.

--- include/ptlib/ipdsock.h
+++ include/ptlib/ipdsock.h
@@ -1,5 +1,5 @@
 /*
- * $Id: ipdsock.h,v 1.1 1996/05/15 21:11:16 robertj Exp $
+ * $Id: ipdsock.h,v 1.2 1996/09/14 13:09:20 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,12 @@
  * Copyright 1993 Equivalence
  *
  * $Log: ipdsock.h,v $
+ * Revision 1.2  1996/09/14 13:09:20  robertj
+ * Major upgrade:
+ *   rearranged sockets to help support IPX.
+ *   added indirect channel class and moved all protocols to descend from it,
+ *   separating the protocol from the low level byte transport.
+ *
  * Revision 1.1  1996/05/15 21:11:16  robertj
  * Initial revision
  *
@@ -20,74 +26,14 @@
 #endif
 
 PDECLARE_CLASS(PIPDatagramSocket, PIPSocket)
-  public:
-    PIPDatagramSocket(
-      WORD type,
-      WORD port
-    );
-    PIPDatagramSocket(
-      WORD type,
-      const char * protocol,
-      const PString & service
-    );
-    PIPDatagramSocket(
-      WORD type,
-      const char * protocolName
-    );
+  protected:
+    PIPDatagramSocket();
     /* Create a TCP/IP protocol socket channel. If a remote machine address or
        a "listening" socket is specified then the channel is also opened.
      */
 
-    // Overrides from class PIPSocket.
-    virtual BOOL Connect(
-      const PString & address   // Address of remote machine to connect to.
-    );
-    /* Connect a socket to a remote host on the specified port number. This is
-       typically used by the client or initiator of a communications channel.
-       This connects to a "listening" socket at the other end of the
-       communications channel.
 
-       The port number as defined by the object instance construction or the
-       <A>PIPSocket::SetPort()</A> function.
-
-       <H2>Returns:</H2>
-       TRUE if the channel was successfully connected to the remote host.
-     */
-
-
-    virtual BOOL Listen(
-      unsigned queueSize = 5,  // Number of pending accepts that may be queued.
-      WORD port = 0,           // Port number to use for the connection.
-      Reusability reuse = AddressIsExclusive // Can/Cant listen more than once.
-    );
-    /* Listen on a socket for a remote host on the specified port number. This
-       may be used for server based applications. A "connecting" socket begins
-       a connection by initiating a connection to this socket. An active socket
-       of this type is then used to generate other "accepting" sockets which
-       establish a two way communications channel with the "connecting" socket.
-
-       If the <CODE>port</CODE> parameter is zero then the port number as
-       defined by the object instance construction or the
-       <A>PIPSocket::SetPort()</A> function.
-
-       For the UDP protocol, the <CODE>queueSize</CODE> parameter is ignored.
-
-       <H2>Returns:</H2>
-       TRUE if the channel was successfully opened.
-     */
-
-    virtual BOOL Accept(
-      PSocket & socket          // Listening socket making the connection.
-    );
-    /* Open a socket to a remote host on the specified port number.
-
-       You cannot accept on a UDP socket, so this function asserts.
-
-       <H2>Returns:</H2>
-       TRUE if the channel was successfully opened.
-     */
-
-
+  public:
   // New functions for class
     virtual BOOL ReadFrom(
       void * buf,     // Data to be written as URGENT TCP data.
@@ -112,15 +58,6 @@ PDECLARE_CLASS(PIPDatagramSocket, PIPSocket)
        <H2>Returns:</H2>
        TRUE if all the bytes were sucessfully written.
      */
-
-
-  protected:
-    BOOL _Socket();
-    /* Create a socket with correct protocol and type
-     */
-
-    WORD sockType;
-    WORD protocol;
 
 
 // Class declaration continued in platform specific header file ///////////////
