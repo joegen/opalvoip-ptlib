4ff14ea8e1619d58140699fb3a39f8ef0702ec0b
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1996-09-14 13:19:35 +0000

    Initial revision

---
+++ include/ptlib/msos/ptlib/spxsock.h
@@ -0,0 +1,25 @@
+/*
+ * $Id: spxsock.h,v 1.1 1996/09/14 13:19:35 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * Internet Protocol Socket Class Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: spxsock.h,v $
+ * Revision 1.1  1996/09/14 13:19:35  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PSPXSOCKET
+
+#include "../../common/ptlib/spxsock.h"
+};
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
