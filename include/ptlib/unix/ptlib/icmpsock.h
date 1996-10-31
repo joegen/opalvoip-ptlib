99537edf9d866274ac215178770e224e7c104219
Craig Southeren <craigs@postincrement.com>
1996-10-31 10:27:42 +0000

    New platform dependent socket implementation

--- include/ptlib/unix/ptlib/icmpsock.h
+++ include/ptlib/unix/ptlib/icmpsock.h
@@ -1,6 +1,6 @@
 
 /*
- * $Id: icmpsock.h,v 1.2 1996/08/03 12:08:19 craigs Exp $
+ * $Id: icmpsock.h,v 1.3 1996/10/31 10:27:42 craigs Exp $
  *
  * Portable Windows Library
  *
@@ -9,6 +9,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: icmpsock.h,v $
+ * Revision 1.3  1996/10/31 10:27:42  craigs
+ * New platform dependent socket implementation
+ *
  * Revision 1.2  1996/08/03 12:08:19  craigs
  * Changed for new common directories
  *
@@ -25,6 +28,25 @@
 // PICMPSocket
 
 #include "../../common/ptlib/icmpsock.h"
+  protected:
+    BOOL WritePing(
+      const PString & host,   // Host to send ping.
+      PingInfo & info         // Information on the ping and reply.
+    );
+    /* Send an ECHO_REPLY message to the specified host.
+
+       <H2>Returns:</H2>
+       FALSE if host not found or no response.
+     */
+
+    BOOL ReadPing(
+      PingInfo & info         // Information on the ping and reply.
+    );
+    /* Receive an ECHO_REPLY message from the host.
+
+       <H2>Returns:</H2>
+       FALSE if an error occurred.
+     */
 };
 
 #endif
