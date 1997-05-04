eca78767046e389fdceacc1ebde183c2f3595a9f
craigs <craigs@023b2edf-31b2-4de3-b41e-bca80c47788f>
1997-05-04 02:49:52 +0000

    checked in with -k by robertj at 1997/09/01 04:53:35

--- include/ptclib/pssl.h
+++ include/ptclib/pssl.h
@@ -1,5 +1,5 @@
 /*
- * $Id: pssl.h,v 1.1 1997/01/12 13:23:35 robertj Exp $
+ * $Id: pssl.h,v 1.3 1997/05/04 02:49:52 craigs Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1996 Equivalence
  *
  * $Log: pssl.h,v $
+ * Revision 1.3  1997/05/04 02:49:52  craigs
+ * Added support for client and server certificates
+ *
  * Revision 1.1  1997/01/12 13:23:35  robertj
  * Initial revision
  *
@@ -28,14 +31,36 @@
 
 extern "C" {
 #include <ssl.h>
+#include <crypto.h>
 };
 
 PDECLARE_CLASS(PSSLChannel, PIndirectChannel)
   public:
+    enum {
+      CertificateOK,
+      UnknownCertificate,
+      UnknownPrivateKey,
+      PrivateKeyMismatch,
+    };
+
+    enum {
+      VerifyNone,
+      VerifyPeer,
+      VerifyPeerMandatory,
+    };
+
     PSSLChannel();
     ~PSSLChannel();
 
-    int Accept(PChannel & channel);
+    static BOOL SetCAPath(const PDirectory & caPath);
+    static BOOL SetCAFile(const PFilePath & caFile);
+    static BOOL SetCAPathAndFile(const PDirectory & caPath, const PFilePath & caFile);
+
+    void SetVerifyMode(int mode);
+
+    int SetClientCertificate(const PString & certFile);
+    int SetClientCertificate(const PString & certFile, const PString & keyFile);
+
 
     // Normal socket read & write functions
     BOOL   Read(void * buf, PINDEX len);
@@ -46,15 +71,22 @@ PDECLARE_CLASS(PSSLChannel, PIndirectChannel)
     BOOL   RawWrite(const void * buf, PINDEX len);
     PINDEX RawGetLastWriteCount() const;
 
+    BOOL Accept(PChannel & channel);
+    BOOL Connect(PChannel & channel);
     BOOL Shutdown(ShutdownValue) { return TRUE; }
 
   protected:
-    int Set_SSL_Fd();
+    int  Set_SSL_Fd();
+    int  SetClientCertificate(const char * certFile, const char * keyFile);
 
-    static SSL_CTX * sslContext;
-    static int       sslContextRefCount;
+    static BOOL SetCAPathAndFile(const char * caPath, const char * caFile);
+    static void Cleanup();
+
 
     SSL * ssl;
+    static SSL_CTX * context;
+    static PSemaphore semaphores[CRYPTO_NUM_LOCKS];
+    static PSemaphore initFlag;
 };
 
 #endif
