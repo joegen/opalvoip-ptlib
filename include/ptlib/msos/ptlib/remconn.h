efcbac351d24435069080b0297b0da362f0390fc
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-01-26 00:53:13 +0000

    Moved to common.

--- include/ptlib/msos/ptlib/remconn.h
+++ include/ptlib/msos/ptlib/remconn.h
@@ -1,11 +1,14 @@
 /*
- * $Id: remconn.h,v 1.2 1996/08/08 10:09:10 robertj Exp $
+ * $Id: remconn.h,v 1.3 1998/01/26 00:53:13 robertj Exp $
  *
  * Portable Windows Library
  *
  * Copyright 1993 Equivalence
  *
  * $Log: remconn.h,v $
+ * Revision 1.3  1998/01/26 00:53:13  robertj
+ * Moved to common.
+ *
  * Revision 1.2  1996/08/08 10:09:10  robertj
  * Directory structure changes for common files.
  *
@@ -24,7 +27,6 @@
   private:
     // Win32 specific stuff
     HRASCONN rasConnection;
-    DWORD    rasError;
 };
 
 
