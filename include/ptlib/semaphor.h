7d231b3d6453a363c863050ec403ec72fa2f0b5e
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-03-20 03:16:11 +0000

    Added special classes for specific sepahores, PMutex and PSyncPoint.

--- include/ptlib/semaphor.h
+++ include/ptlib/semaphor.h
@@ -1,5 +1,5 @@
 /*
- * $Id: semaphor.h,v 1.3 1995/12/10 11:34:50 robertj Exp $
+ * $Id: semaphor.h,v 1.4 1998/03/20 03:16:11 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: semaphor.h,v $
+ * Revision 1.4  1998/03/20 03:16:11  robertj
+ * Added special classes for specific sepahores, PMutex and PSyncPoint.
+ *
  * Revision 1.3  1995/12/10 11:34:50  robertj
  * Fixed incorrect order of parameters in semaphore constructor.
  *
@@ -66,8 +69,8 @@ PDECLARE_CLASS(PSemaphore, PObject)
 
   public:
     PSemaphore(
-      unsigned initial = UINT_MAX, // Initial value for semaphore count.
-      unsigned maximum = 1         // Maximum value for semaphore count.
+      unsigned initial, // Initial value for semaphore count.
+      unsigned maximum  // Maximum value for semaphore count.
     );
     /* Create a new semaphore with maximum count and initial value specified.
        If the initial value is larger than the maximum value then is is set to
@@ -81,8 +84,8 @@ PDECLARE_CLASS(PSemaphore, PObject)
 
 
   // New functions for class.
-    void Wait();
-    BOOL Wait(
+    virtual void Wait();
+    virtual BOOL Wait(
       const PTimeInterval & timeout // Amount of time to wait for semaphore.
     );
     /* If the semaphore count is > 0, decrement the semaphore and return. If
@@ -92,13 +95,13 @@ PDECLARE_CLASS(PSemaphore, PObject)
        TRUE if semaphore was signalled, FALSE if timed out.
      */
 
-    void Signal();
+    virtual void Signal();
     /* If there are waiting (blocked) threads then unblock the first one that
        was blocked. If no waiting threads and the count is less than the
        maximum then increment the semaphore.
      */
 
-    BOOL WillBlock() const;
+    virtual BOOL WillBlock() const;
     /* Determine if the semaphore would block if the <A>Wait()</A> function
        were called.
      */
