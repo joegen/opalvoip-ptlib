c146c5910062f0198e4629c8abb60f0bce92e54d
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-03-20 03:19:49 +0000

    svnimporter: adding tags and branches to revision 2576

---
+++ include/ptlib/msos/ptlib/semaphor.h
@@ -0,0 +1,43 @@
+/*
+ * $Id: semaphor.h,v 1.4 1998/03/20 03:19:49 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: semaphor.h,v $
+ * Revision 1.4  1998/03/20 03:19:49  robertj
+ * Added special classes for specific sepahores, PMutex and PSyncPoint.
+ *
+ * Revision 1.3  1996/08/08 10:09:11  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.2  1996/06/13 13:32:10  robertj
+ * Rewrite of auto-delete threads, fixes Windows95 total crash.
+ *
+ * Revision 1.1  1995/08/01 21:42:22  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PSEMAPHORE
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PSemaphore
+
+#include "../../common/ptlib/semaphor.h"
+#if defined(P_PLATFORM_HAS_THREADS)
+  public:
+    HANDLE GetHandle() const { return handle; }
+  protected:
+    PSemaphore(HANDLE h);
+    HANDLE handle;
+#endif
+};
+
+
+#endif
