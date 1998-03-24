62ac231e592aec0887a05fb64fbc340cdc943de1
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-03-24 07:31:04 +0000

    Initial revision

---
+++ include/ptlib/unix/ptlib/mutex.h
@@ -0,0 +1,30 @@
+/*
+ * $Id: mutex.h,v 1.1 1998/03/24 07:31:04 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: mutex.h,v $
+ * Revision 1.1  1998/03/24 07:31:04  robertj
+ * Initial revision
+ *
+ * Revision 1.1  1998/03/23 02:42:02  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PMUTEX
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PMutex
+
+#include "../../common/ptlib/mutex.h"
+};
+
+
+#endif
---
+++ include/ptlib/unix/ptlib/syncpoint.h
@@ -0,0 +1,30 @@
+/*
+ * $Id: syncpoint.h,v 1.1 1998/03/24 07:31:04 robertj Exp $
+ *
+ * Portable Windows Library
+ *
+ * User Interface Classes Interface Declarations
+ *
+ * Copyright 1993 Equivalence
+ *
+ * $Log: syncpoint.h,v $
+ * Revision 1.1  1998/03/24 07:31:04  robertj
+ * Initial revision
+ *
+ * Revision 1.1  1998/03/23 02:42:03  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PSYNCPOINT
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PSyncPoint
+
+#include "../../common/ptlib/syncpoint.h"
+};
+
+
+#endif
