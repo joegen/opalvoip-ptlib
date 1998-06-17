7c67099d6fe63fb10f496ab4621791389d96b6d7
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-06-17 14:47:47 +0000

    Fixed continuous display of assert if input is from /dev/null

--- src/ptlib/unix/assert.cxx
+++ src/ptlib/unix/assert.cxx
@@ -8,6 +8,9 @@
  * Copyright 1993 by Robert Jongbloed and Craig Southeren
  *
  * $Log: assert.cxx,v $
+ * Revision 1.2  1998/06/17 14:47:47  robertj
+ * Fixed continuous display of assert if input is from /dev/null
+ *
  * Revision 1.1  1996/01/26 11:09:15  craigs
  * Initial revision
  *
@@ -21,20 +24,27 @@
 void PAssertFunc (const char * file, int line, const char * msg)
 
 {
+  PError << "Assertion fail: File " << file << ", Line " << line << endl;
+  if (msg != NULL)
+    PError << msg << endl;
   for(;;) {
-    PError << "Assertion fail: File " << file << ", Line " << line << endl;
-    if (msg != NULL)
-      PError << msg << endl;
-    PError << "\n<A>bort, <C>ore dump, <I>gnore? ";
-    PError.flush();
-    switch (tolower(getchar())) {
+    PError << "\n<A>bort, <C>ore dump, <I>gnore? " << flush;
+    int c = getchar();
+    switch (c) {
       case 'a' :
+      case 'A' :
+        PError << "\nAborting.\n";
         exit (1);
 
       case 'c' :
+      case 'C' :
+        PError << "\nDumping core.\n";
         kill(getpid(), SIGABRT);
 
       case 'i' :
+      case 'I' :
+      case EOF :
+        PError << "\nIgnoring.\n";
         return;
     }
   }
