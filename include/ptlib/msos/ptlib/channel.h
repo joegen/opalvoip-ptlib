9afe413e31491cfd3970236bce948fe579935dce
Robert Jongbloed <robertj@voxlucida.com.au>
1998-08-20 06:03:44 +0000

    Allowed Win32 class to be used in other compilation modules

--- include/ptlib/msos/ptlib/channel.h
+++ include/ptlib/msos/ptlib/channel.h
@@ -1,5 +1,5 @@
 /*
- * $Id: channel.h,v 1.2 1996/08/08 10:08:56 robertj Exp $
+ * $Id: channel.h,v 1.3 1998/08/20 06:03:44 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: channel.h,v $
+ * Revision 1.3  1998/08/20 06:03:44  robertj
+ * Allowed Win32 class to be used in other compilation modules
+ *
  * Revision 1.2  1996/08/08 10:08:56  robertj
  * Directory structure changes for common files.
  *
@@ -24,6 +27,17 @@
 
 #include "../../common/ptlib/channel.h"
 };
+
+
+class PWin32Overlapped : public OVERLAPPED
+{
+  // Support class for overlapped I/O in Win32.
+  public:
+    PWin32Overlapped();
+    ~PWin32Overlapped();
+    void Reset();
+};
+
 
 
 #endif
