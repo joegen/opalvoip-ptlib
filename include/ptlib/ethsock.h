8cb81ea6a5835ad487bc8a51bb7409985c525c50
Robert Jongbloed <robertj@voxlucida.com.au>
1998-08-21 05:26:34 +0000

    Fine tuning of interface.

--- include/ptlib/ethsock.h
+++ include/ptlib/ethsock.h
@@ -1,5 +1,5 @@
 /*
- * $Id: ethsock.h,v 1.1 1998/08/20 05:46:45 robertj Exp $
+ * $Id: ethsock.h,v 1.2 1998/08/21 05:26:34 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1993 Equivalence
  *
  * $Log: ethsock.h,v $
+ * Revision 1.2  1998/08/21 05:26:34  robertj
+ * Fine tuning of interface.
+ *
  * Revision 1.1  1998/08/20 05:46:45  robertj
  * Initial revision
  *
@@ -38,6 +41,9 @@ PDECLARE_CLASS(PEthSocket, PSocket)
        buffers to be allocated to avoid losing frequent packets.
      */
 
+    ~PEthSocket();
+      // Close the socket
+
 
   public:
 #ifdef _MSC_VER
@@ -50,7 +56,7 @@ PDECLARE_CLASS(PEthSocket, PSocket)
       struct {
         DWORD l;
         WORD  s;
-      };
+      } ls;
 
       Address();
       Address(const BYTE * addr);
@@ -59,24 +65,77 @@ PDECLARE_CLASS(PEthSocket, PSocket)
       Address & operator=(const Address & addr);
       Address & operator=(const PString & str);
       operator PString() const;
-      BOOL operator==(const Address & eth) { return l == eth.l && s == eth.s; }
-      BOOL operator!=(const Address & eth) { return l != eth.l || s != eth.s; }
+      BOOL operator==(const BYTE * eth) const    { return memcmp(b, eth, sizeof(b)) == 0; }
+      BOOL operator!=(const BYTE * eth) const    { return memcmp(b, eth, sizeof(b)) != 0; }
+      BOOL operator==(const Address & eth) const { return ls.l == eth.ls.l && ls.s == eth.ls.s; }
+      BOOL operator!=(const Address & eth) const { return ls.l != eth.ls.l || ls.s != eth.ls.s; }
       friend ostream & operator<<(ostream & s, Address & a)
         { return s << (PString)a; }
     };
+
+  struct Frame {
+    BYTE dst_addr[6];
+    BYTE src_addr[6];
+    union {
+      struct {
+        WORD type;
+        BYTE payload[1500];
+      } ether;
+      struct {
+        WORD length;
+        BYTE dsap;
+        BYTE ssap;
+        BYTE ctrl;
+        BYTE oui[3];
+        WORD type;
+        BYTE payload[1492];
+      } snap;
+    };
+  };
 #ifdef _MSC_VER
 #pragma warning(default:4201)
 #pragma pack()
 #endif
 
   // Overrides from class PChannel
-    virtual PString GetName() const;
-    /* Get the platform and I/O channel type name of the channel. For an
-       ethernet socket this returns the interface name, which is platform
-       dependent.
+    virtual BOOL Close();
+    /* Close the channel, shutting down the link to the data source.
 
        <H2>Returns:</H2>
-       the name of the channel.
+       TRUE if the channel successfully closed.
+     */
+
+    virtual BOOL Read(
+      void * buf,   // Pointer to a block of memory to receive the read bytes.
+      PINDEX len    // Maximum number of bytes to read into the buffer.
+    );
+    /* Low level read from the channel. This function may block until the
+       requested number of characters were read or the read timeout was
+       reached. The GetLastReadCount() function returns the actual number
+       of bytes read.
+
+       The GetErrorCode() function should be consulted after Read() returns
+       FALSE to determine what caused the failure.
+
+       <H2>Returns:</H2>
+       TRUE indicates that at least one character was read from the channel.
+       FALSE means no bytes were read due to timeout or some other I/O error.
+     */
+
+    virtual BOOL Write(
+      const void * buf, // Pointer to a block of memory to write.
+      PINDEX len        // Number of bytes to write.
+    );
+    /* Low level write to the channel. This function will block until the
+       requested number of characters are written or the write timeout is
+       reached. The GetLastWriteCount() function returns the actual number
+       of bytes written.
+
+       The GetErrorCode() function should be consulted after Write() returns
+       FALSE to determine what caused the failure.
+
+       <H2>Returns:</H2>
+       TRUE if at least len bytes were written to the channel.
      */
 
 
@@ -174,6 +233,16 @@ PDECLARE_CLASS(PEthSocket, PSocket)
        addresses bound to the interface.
      */
 
+    enum EthTypes {
+      TypeAll = 3,          // All frames
+      TypeIP  = 0x800,      // Internet Protocol
+      TypeX25 = 0x805,      // X.25
+      TypeARP = 0x806,      // Address Resolution Protocol
+      TypeAtalk = 0x809B,   // Appletalk DDP
+      TypeAARP = 0x80F3,    // Appletalk AARP
+      TypeIPX = 0x8137,     // IPX
+      TypeIPv6 = 0x86DD     // Bluebook IPv6
+    };
     enum FilterMask {
       FilterDirected     = 0x01,    // Packets directed at the interface.
       FilterMulticast    = 0x02,    // Multicast packets directed at the interface.
@@ -181,21 +250,34 @@ PDECLARE_CLASS(PEthSocket, PSocket)
       FilterBroadcast    = 0x08,    // Packets with a broadcast address.
       FilterPromiscuous  = 0x10     // All packets.
     };
-    unsigned GetFilter();
-    /* Get the current filtering criteria for receiving packets. A bit-wise OR
-       of the FilterMask values will filter packets so that they do not appear
-       in the Read() function at all.
+    BOOL GetFilter(
+      unsigned & mask,  // Bits for filtering on address
+      WORD & type       // Code for filtering on type.
+    );
+    /* Get the current filtering criteria for receiving packets.
+
+       A bit-wise OR of the FilterMask values will filter packets so that
+       they do not appear in the Read() function at all.
+
+       The type is be the specific frame type to accept. A value of TypeAll
+       may be used to match all frame types.
 
        <H2>Returns:</H2>
        A bit mask is returned, a value of 0 indicates an error.
      */
 
-    BOOL SetFilter(unsigned filter);
+    BOOL SetFilter(
+      unsigned mask,       // Bits for filtering on address
+      WORD type = TypeAll  // Code for filtering on type.
+    );
     /* Set the current filtering criteria for receiving packets. A bit-wise OR
        of the FilterMask values will filter packets so that they do not appear
        in the Read() function at all.
 
-       A value of zero for the filter is useless and will assert.
+       The type is be the specific frame type to accept. A value of TypeAll
+       may be used to match all frame types.
+
+       A value of zero for the filter mask is useless and will assert.
 
        <H2>Returns:</H2>
        TRUE if the address is returned, FALSE on error.
@@ -203,6 +285,7 @@ PDECLARE_CLASS(PEthSocket, PSocket)
 
 
     enum MediumTypes {
+      MediumLoop,     // A Loopback Network
       Medium802_3,    // An ethernet Network Interface Card (10base2, 10baseT etc)
       MediumWan,      // A Wide Area Network (modem etc)
       MediumUnknown,  // Something else
@@ -219,10 +302,28 @@ PDECLARE_CLASS(PEthSocket, PSocket)
     /* Reset the interface.
      */
 
+    BOOL ReadPacket(
+      PBYTEArray & buffer,  // Buffer to receive the raw packet
+      Address & dest,       // Destination address of packet
+      Address & src,        // Source address of packet
+      WORD & type,          // Packet frame type ID
+      PINDEX & len,         // Length of payload
+      BYTE * & payload      // Pointer into <CODE>buffer</CODE> of payload.
+    );
+    /* Read a packet from the interface and parse out the information
+       specified by the parameters. This will automatically adjust for 802.2
+       and 802.3 ethernet frames.
+
+       <H2>Returns:</H2>
+       TRUE if the packet read, FALSE on error.
+     */
 
   protected:
     virtual BOOL OpenSocket();
     virtual const char * GetProtocolName() const;
+
+
+    WORD filterType;  // Remember the set filter frame type
 
 
 // Class declaration continued in platform specific header file ///////////////
