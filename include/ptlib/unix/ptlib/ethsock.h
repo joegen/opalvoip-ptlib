70a9407cefeb8401d012cf58e433861a6feeb7a9
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-08 09:53:56 +0000

    Fixed ppp and ippp compatibility.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2774 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/unix/ptlib/ethsock.h
+++ include/ptlib/unix/ptlib/ethsock.h
@@ -1,5 +1,5 @@
 /*
- * $Id: ethsock.h,v 1.1 1998/08/21 05:30:13 robertj Exp $
+ * $Id: ethsock.h,v 1.2 1998/09/08 09:53:56 robertj Exp $
  *
  * Portable Windows Library
  *
@@ -8,6 +8,9 @@
  * Copyright 1996 Equivalence Pty. Ltd.
  *
  * $Log: ethsock.h,v $
+ * Revision 1.2  1998/09/08 09:53:56  robertj
+ * Fixed ppp and ippp compatibility.
+ *
  * Revision 1.1  1998/08/21 05:30:13  robertj
  * Initial revision
  *
@@ -26,8 +29,8 @@
     Address     macAddress;
     MediumTypes medium;
     unsigned    filterMask;
-    size_t      skipBytes;
-    size_t      padBytes;
+    BOOL        fakeMacHeader;
+    BOOL        ipppInterface;
 };
 
 #endif
