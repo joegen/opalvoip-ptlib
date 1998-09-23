6fc9752a9cddd2885f5c31c3367ead3f303458fd
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:19:54 +0000

    Added open source copyright license.

--- include/pwlib.h
+++ include/pwlib.h
@@ -1,13 +1,35 @@
 /*
- * $Id: pwlib.h,v 1.43 1998/09/21 13:30:41 robertj Exp $
+ * pwlib.h
+ *
+ * Umbrella include file for all GUI classes.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pwlib.h,v $
+ * Revision 1.44  1998/09/23 06:19:54  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.43  1998/09/21 13:30:41  robertj
  * Changes to support new PListView class. Different call back method.
  *
