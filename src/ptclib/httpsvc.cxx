2e0fead704d2064103ef6273c59f4152b0211ade
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:22:15 +0000

    Added open source copyright license.

--- src/ptclib/httpsvc.cxx
+++ src/ptclib/httpsvc.cxx
@@ -1,11 +1,35 @@
 /*
- * $Id: httpsvc.cxx,v 1.38 1998/09/18 01:47:23 robertj Exp $
+ * httpsvc.cxx
  *
- * Common classes for service applications using HTTP as the user interface.
+ * Classes for service applications using HTTP as the user interface.
  *
- * Copyright 1995-1996 Equivalence
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: httpsvc.cxx,v $
+ * Revision 1.39  1998/09/23 06:22:15  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.38  1998/09/18 01:47:23  robertj
  * Fixed bug that made files with signature on first line fail on unix systems.
  *
