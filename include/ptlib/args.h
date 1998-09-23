d85da1348624ea0b98fdb15f06dd83472b5f9c5c
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:20:14 +0000

    Added open source copyright license.

--- include/ptlib/args.h
+++ include/ptlib/args.h
@@ -1,13 +1,35 @@
 /*
- * $Id: args.h,v 1.10 1995/12/10 11:26:38 robertj Exp $
+ * args.h
+ *
+ * Program Argument Parsing class
  *
  * Portable Windows Library
  *
- * Argument Parsing Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: args.h,v $
+ * Revision 1.11  1998/09/23 06:20:14  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.10  1995/12/10 11:26:38  robertj
  * Fixed signed/unsigned bug in shift count.
  *
