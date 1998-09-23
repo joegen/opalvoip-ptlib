8de84f714cfe57c9c56d185856b3678705c73ef2
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:22:33 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2881 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptclib/psnmp.cxx
+++ src/ptclib/psnmp.cxx
@@ -1,11 +1,35 @@
 /*
- * SNMP Library
+ * psnmp.cxx
  *
- * $Id: psnmp.cxx,v 1.5 1998/01/26 02:50:17 robertj Exp $
+ * SNMP base and support classes.
  *
- * Copyright 1996 by Equivalence
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: psnmp.cxx,v $
+ * Revision 1.6  1998/09/23 06:22:33  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.5  1998/01/26 02:50:17  robertj
  * GNU Support
  *
