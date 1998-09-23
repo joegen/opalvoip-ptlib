009d8b1427aa8208b09a36eb648418ab86a53bcf
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:22:38 +0000

    Added open source copyright license.

--- src/ptclib/modem.cxx
+++ src/ptclib/modem.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: modem.cxx,v 1.2 1996/04/15 10:57:59 robertj Exp $
+ * serial.cxx
+ *
+ * Asynchronous Serial I/O channel class.
  *
  * Portable Windows Library
  *
- * Operating System Classes Inline Function Definitions
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: modem.cxx,v $
+ * Revision 1.3  1998/09/23 06:22:38  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.2  1996/04/15 10:57:59  robertj
  * Moved some functions from INL to serial.cxx so unix linker can make smaller executables.
  *
