cdd683414eb8b807c1c408f70542f2d1885093ba
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:20:53 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2836 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptclib/modem.h
+++ include/ptclib/modem.h
@@ -1,13 +1,35 @@
 /*
- * $Id: modem.h,v 1.8 1995/06/17 11:12:46 robertj Exp $
+ * modem.h
+ *
+ * AT command set modem on asynchonous port class.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: modem.h,v $
+ * Revision 1.9  1998/09/23 06:20:53  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.8  1995/06/17 11:12:46  robertj
  * Documentation update.
  *
