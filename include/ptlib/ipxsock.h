b30d0b5b798614f49036e814ade3ef3f8cfd91da
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:20:47 +0000

    Added open source copyright license.

--- include/ptlib/ipxsock.h
+++ include/ptlib/ipxsock.h
@@ -1,13 +1,35 @@
 /*
- * $Id: ipxsock.h,v 1.3 1996/10/08 13:21:04 robertj Exp $
+ * ipxsock.h
+ *
+ * IPX protocol socket I/O channel class.
  *
  * Portable Windows Library
  *
- * Internet Protocol Socket Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: ipxsock.h,v $
+ * Revision 1.4  1998/09/23 06:20:47  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.3  1996/10/08 13:21:04  robertj
  * More IPX implementation.
  *
