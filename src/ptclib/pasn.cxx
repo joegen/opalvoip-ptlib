d95b9c1fbeaccfb65a4dfc7438ee49e731a5da5f
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:22:27 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2878 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptclib/pasn.cxx
+++ src/ptclib/pasn.cxx
@@ -1,11 +1,35 @@
 /*
- * ASN Library
+ * pasn.cxx
  *
- * $Id: pasn.cxx,v 1.6 1998/02/16 06:57:05 robertj Exp $
+ * ASN classes in support of the SNMP code.
  *
- * Copyright 1996 by Equivalence
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pasn.cxx,v $
+ * Revision 1.7  1998/09/23 06:22:27  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.6  1998/02/16 06:57:05  robertj
  * Moved pragma implemenetation in here so do not need upasn.cxx file.
  *
