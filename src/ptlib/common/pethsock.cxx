ad8d09534a8565d85fc9d21884427a5281630a8c
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:22:29 +0000

    Added open source copyright license.

--- src/ptlib/common/pethsock.cxx
+++ src/ptlib/common/pethsock.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: pethsock.cxx,v 1.2 1998/09/14 12:37:51 robertj Exp $
+ * pethsock.cxx
+ *
+ * Direct Ethernet socket I/O channel class.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1994 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pethsock.cxx,v $
+ * Revision 1.3  1998/09/23 06:22:29  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.2  1998/09/14 12:37:51  robertj
  * Added function to parse type and payload address out of ethernet/802.2 packet.
  *
