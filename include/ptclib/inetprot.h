ffff9ceb14e489e2d2c086020bbec323fd2d7145
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:19:40 +0000

    Added open source copyright license.

--- include/ptclib/inetprot.h
+++ include/ptclib/inetprot.h
@@ -1,13 +1,35 @@
 /*
- * $Id: inetprot.h,v 1.11 1996/09/14 13:09:13 robertj Exp $
+ * inetprot.h
+ *
+ * Internet Protocol ancestor channel class
  *
  * Portable Windows Library
  *
- * Internet Protocol Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1995 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: inetprot.h,v $
+ * Revision 1.12  1998/09/23 06:19:40  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.11  1996/09/14 13:09:13  robertj
  * Major upgrade:
  *   rearranged sockets to help support IPX.
