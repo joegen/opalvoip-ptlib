3976e6a0d611f102e47d81f145e4cf3f7f1090ae
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:22:36 +0000

    Added open source copyright license.

--- src/ptlib/common/ptime.cxx
+++ src/ptlib/common/ptime.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: ptime.cxx,v 1.21 1998/03/05 12:49:53 robertj Exp $
+ * ptime.cxx
+ *
+ * Time and date classes.
  *
  * Portable Windows Library
  *
- * Time classes
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: ptime.cxx,v $
+ * Revision 1.22  1998/09/23 06:22:36  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.21  1998/03/05 12:49:53  robertj
  * MemCheck fixes.
  *
