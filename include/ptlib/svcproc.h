673d6579d6d840e0d292c9c92a6a6526e7bfd0e4
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:21:31 +0000

    Added open source copyright license.

--- include/ptlib/svcproc.h
+++ include/ptlib/svcproc.h
@@ -1,13 +1,35 @@
 /*
- * $Id: svcproc.h,v 1.12 1998/04/07 13:33:21 robertj Exp $
+ * svcproc.h
+ *
+ * Service Process (daemon) class.
  *
  * Portable Windows Library
  *
- * Service Process for Windows NT platforms
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1995 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: svcproc.h,v $
+ * Revision 1.13  1998/09/23 06:21:31  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.12  1998/04/07 13:33:21  robertj
  * Changed startup code to support PApplication class.
  *
