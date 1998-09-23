8025eda8d618051b328af5d20ad4703a6b079f9a
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:22:31 +0000

    Added open source copyright license.

--- src/ptlib/common/pipechan.cxx
+++ src/ptlib/common/pipechan.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: pipechan.cxx,v 1.2 1996/05/09 12:18:41 robertj Exp $
+ * pipechan.cxx
+ *
+ * Sub-process communicating with pipe I/O channel class
  *
  * Portable Windows Library
  *
- * Operating System Classes Inline Function Definitions
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pipechan.cxx,v $
+ * Revision 1.3  1998/09/23 06:22:31  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.2  1996/05/09 12:18:41  robertj
  * Fixed syntax error found by Mac platform.
  *
