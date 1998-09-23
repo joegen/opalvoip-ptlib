ea4738b1c7ac382b76e40437a03e7768102244ff
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:27:38 +0000

    Added open source copyright license.

--- include/ptclib/psnmp.h
+++ include/ptclib/psnmp.h
@@ -1,11 +1,35 @@
 /*
- * $Id: psnmp.h,v 1.3 1996/11/04 03:56:16 robertj Exp $
+ * psnmp.h
  *
- * SNMP Interface
+ * Simple Network Management Protocol classes.
  *
- * Copyright 1996 Equivalence
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: psnmp.h,v $
+ * Revision 1.4  1998/09/23 06:27:38  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.3  1996/11/04 03:56:16  robertj
  * Added selectable read buffer size.
  *
