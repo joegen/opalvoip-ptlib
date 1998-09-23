35f66711f75056f57d26a2f63d90193ead8a047b
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:21:17 +0000

    Added open source copyright license.

--- include/ptlib/remconn.h
+++ include/ptlib/remconn.h
@@ -1,13 +1,35 @@
 /*
- * $Id: remconn.h,v 1.10 1998/07/24 06:58:27 robertj Exp $
+ * remconn.h
+ *
+ * Remote networking connection class.
  *
  * Portable Windows Library
  *
- * Internet Protocol Socket Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: remconn.h,v $
+ * Revision 1.11  1998/09/23 06:21:17  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.10  1998/07/24 06:58:27  robertj
  * Added ability to get IP number of RAS connection.
  *
