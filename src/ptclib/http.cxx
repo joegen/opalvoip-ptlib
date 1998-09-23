e348a08bd91dc6d75de12d4972c98155cdb66b7b
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:22:07 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2869 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptclib/http.cxx
+++ src/ptclib/http.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: http.cxx,v 1.50 1998/02/03 10:02:34 robertj Exp $
+ * http.cxx
+ *
+ * HTTP ancestor class and common classes.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1994 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: http.cxx,v $
+ * Revision 1.51  1998/09/23 06:22:07  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.50  1998/02/03 10:02:34  robertj
  * Added ability to get scheme, host and port from URL as a string.
  *
