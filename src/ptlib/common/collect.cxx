7d7fb093dcde96322eb837048511a49da050d738
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:21:52 +0000

    Added open source copyright license.

--- src/ptlib/common/collect.cxx
+++ src/ptlib/common/collect.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: collect.cxx,v 1.40 1998/09/14 12:32:45 robertj Exp $
+ * collect.cxx
+ *
+ * Container Classes
  *
  * Portable Windows Library
  *
- * PContainer Class Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: collect.cxx,v $
+ * Revision 1.41  1998/09/23 06:21:52  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.40  1998/09/14 12:32:45  robertj
  * Fixed bug in ordinal dictionary GetAt() and SetAt() for scalar integer types.
  *
