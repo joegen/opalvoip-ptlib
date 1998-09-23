2eb4c5c506b653dc86f54e67adf15f94fa9871e4
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:21:56 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2864 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptclib/cypher.cxx
+++ src/ptclib/cypher.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: cypher.cxx,v 1.24 1998/07/24 06:58:13 robertj Exp $
+ * cypher.cxx
+ *
+ * Encryption support classes.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: cypher.cxx,v $
+ * Revision 1.25  1998/09/23 06:21:56  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.24  1998/07/24 06:58:13  robertj
  * Improved robustness of encrypted data decoding, error on illegal tail block size.
  *
