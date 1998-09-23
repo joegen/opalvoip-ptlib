67334a879abc5163d180dcc86099b356e6165de4
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:20:35 +0000

    Added open source copyright license.

--- include/ptlib/file.h
+++ include/ptlib/file.h
@@ -1,13 +1,35 @@
 /*
- * $Id: file.h,v 1.27 1995/07/31 12:15:43 robertj Exp $
+ * file.h
+ *
+ * Operating System file I/O channel class.
  *
  * Portable Windows Library
  *
- * Operating System Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: file.h,v $
+ * Revision 1.28  1998/09/23 06:20:35  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.27  1995/07/31 12:15:43  robertj
  * Removed PContainer from PChannel ancestor.
  *
