53dc3e7dab04abab28dde0727941a691bbbadc2e
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:20:57 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2838 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/object.h
+++ include/ptlib/object.h
@@ -1,13 +1,35 @@
 /*
- * $Id: object.h,v 1.28 1998/09/14 12:29:11 robertj Exp $
+ * object.h
+ *
+ * Mother of all ancestor classes.
  *
  * Portable Windows Library
  *
- * PContainer Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: object.h,v $
+ * Revision 1.29  1998/09/23 06:20:57  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.28  1998/09/14 12:29:11  robertj
  * Fixed memory leak dump under windows to not include static globals.
  * Fixed problem with notifier declaration not allowing implementation inline after macro.
