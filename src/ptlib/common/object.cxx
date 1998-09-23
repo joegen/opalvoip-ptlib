7c8ffdd440e4169944f0b7025d93077410bb0be0
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:22:22 +0000

    Added open source copyright license.

--- src/ptlib/common/object.cxx
+++ src/ptlib/common/object.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: object.cxx,v 1.26 1998/05/30 13:27:02 robertj Exp $
+ * object.cxx
+ *
+ * Global object support.
  *
  * Portable Windows Library
  *
- * PContainer Class Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: object.cxx,v $
+ * Revision 1.27  1998/09/23 06:22:22  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.26  1998/05/30 13:27:02  robertj
  * Changed memory check code so global statics are not included in leak check.
  *
