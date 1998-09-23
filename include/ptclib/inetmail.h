d1d6c6755cab777a88dd75a7feb86c0113ac5fbe
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:19:36 +0000

    Added open source copyright license.

--- include/ptclib/inetmail.h
+++ include/ptclib/inetmail.h
@@ -1,13 +1,36 @@
 /*
- * $Id: inetmail.h,v 1.7 1996/12/21 01:24:15 robertj Exp $
+ * inetmail.h
+ *
+ * Internet Mail channel classes
+ * Simple Mail Transport Protocol & Post Office Protocol v3
  *
  * Portable Windows Library
  *
- * Application Socket Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1995 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: inetmail.h,v $
+ * Revision 1.8  1998/09/23 06:19:36  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.7  1996/12/21 01:24:15  robertj
  * Added missing open message to pop server.
  *
