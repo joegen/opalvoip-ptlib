256cd073a6b4596f0640d7d2c33de43a3867c410
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:22:47 +0000

    Added open source copyright license.

--- src/ptclib/telnet.cxx
+++ src/ptclib/telnet.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: telnet.cxx,v 1.5 1998/01/26 02:49:23 robertj Exp $
+ * telnet.cxx
+ *
+ * TELNET socket I/O channel class.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1994 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: telnet.cxx,v $
+ * Revision 1.6  1998/09/23 06:22:47  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.5  1998/01/26 02:49:23  robertj
  * GNU support.
  *
