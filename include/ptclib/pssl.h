8276c24058908743f80e6050147612f8e122c762
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:19:50 +0000

    Added open source copyright license.

--- include/ptclib/pssl.h
+++ include/ptclib/pssl.h
@@ -1,18 +1,37 @@
 /*
- * $Id: pssl.h,v 1.3 1997/05/04 02:49:52 craigs Exp $
+ * pssl.h
+ *
+ * Secure Sockets Layer channel interface class.
  *
  * Portable Windows Library
  *
- * Application Socket Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1996 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pssl.h,v $
+ * Revision 1.4  1998/09/23 06:19:50  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.3  1997/05/04 02:49:52  craigs
  * Added support for client and server certificates
- *
- * Revision 1.1  1997/01/12 13:23:35  robertj
- * Initial revision
  *
  * Revision 1.1  1996/11/15 07:37:48  craigs
  * Initial revision
