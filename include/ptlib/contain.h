35623cc3b8a17896d6ac122889fa4d418f05a318
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:20:23 +0000

    Added open source copyright license.

--- include/ptlib/contain.h
+++ include/ptlib/contain.h
@@ -1,13 +1,35 @@
 /*
- * $Id: contain.h,v 1.46 1997/07/08 13:15:04 robertj Exp $
+ * contain.h
+ *
+ * Umbrella include for Container Classes.
  *
  * Portable Windows Library
  *
- * Container Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: contain.h,v $
+ * Revision 1.47  1998/09/23 06:20:23  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.46  1997/07/08 13:15:04  robertj
  * DLL support.
  *
