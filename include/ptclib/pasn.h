20ae49ba1b6590880afa777dc3a18d80c1ede43c
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:19:44 +0000

    Added open source copyright license.

--- include/ptclib/pasn.h
+++ include/ptclib/pasn.h
@@ -1,11 +1,35 @@
 /*
- * $Id: pasn.h,v 1.6 1997/08/20 08:48:56 craigs Exp $
+ * pasn.h
  *
- * ASN Classes
+ * Abstract Syntax Notation 1 classes for support of SNMP only.
  *
- * Copyright 1996 Equivalence
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pasn.h,v $
+ * Revision 1.7  1998/09/23 06:19:44  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.6  1997/08/20 08:48:56  craigs
  * Added GetString() to PASNNull
  *
