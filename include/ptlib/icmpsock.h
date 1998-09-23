925a8a7e5e255bf82ea1d465963b1f94ad61f16a
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:20:39 +0000

    Added open source copyright license.

--- include/ptlib/icmpsock.h
+++ include/ptlib/icmpsock.h
@@ -1,13 +1,35 @@
 /*
- * $Id: icmpsock.h,v 1.6 1998/01/26 00:30:41 robertj Exp $
+ * icmpsock.h
+ *
+ * Internet Control Message Protocol socket I/O channel class.
  *
  * Portable Windows Library
  *
- * ICMP Socket Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: icmpsock.h,v $
+ * Revision 1.7  1998/09/23 06:20:39  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.6  1998/01/26 00:30:41  robertj
  * Added error codes, TTL and data buffer to Ping.
  *
