d70bfaa814f177c2e402e0e532e4937121a479ed
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:21:59 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2865 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptclib/ftp.cxx
+++ src/ptclib/ftp.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: ftp.cxx,v 1.11 1998/01/26 05:20:30 robertj Exp $
+ * ftp.cxx
+ *
+ * FTP ancestor class.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1994 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: ftp.cxx,v $
+ * Revision 1.12  1998/09/23 06:21:59  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.11  1998/01/26 05:20:30  robertj
  * GNU Support.
  *
