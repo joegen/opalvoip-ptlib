e84bb592583d224657d9980851dba6db5d887979
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-23 06:21:08 +0000

    Added open source copyright license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2841 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/pipechan.h
+++ include/ptlib/pipechan.h
@@ -1,13 +1,35 @@
 /*
- * $Id: pipechan.h,v 1.11 1997/01/03 05:25:05 robertj Exp $
+ * pipechan.h
+ *
+ * Sub-process with communications using pipe I/O channel class.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pipechan.h,v $
+ * Revision 1.12  1998/09/23 06:21:08  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.11  1997/01/03 05:25:05  robertj
  * Added Wait and Kill functions.
  *
