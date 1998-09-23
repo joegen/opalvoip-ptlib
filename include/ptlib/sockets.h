ca95dfdaa7f044bf78cc1a28c4955f962c9b511a
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-23 06:19:58 +0000

    Added open source copyright license.

--- include/ptlib/sockets.h
+++ include/ptlib/sockets.h
@@ -1,13 +1,35 @@
 /*
- * $Id: sockets.h,v 1.11 1998/08/21 05:25:21 robertj Exp $
+ * sockets.h
+ *
+ * Berkley Sockets classes.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: sockets.h,v $
+ * Revision 1.12  1998/09/23 06:19:58  robertj
+ * Added open source copyright license.
+ *
  * Revision 1.11  1998/08/21 05:25:21  robertj
  * Added ethernet socket in right place this time.
  *
