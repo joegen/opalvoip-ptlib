1bd75bfa499b1e7d78687daf9104fa8b3a180ec1
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-24 03:30:14 +0000

    Added open software license.

--- include/ptlib/msos/ptlib/pdirect.h
+++ include/ptlib/msos/ptlib/pdirect.h
@@ -1,13 +1,35 @@
 /*
- * $Id: pdirect.h,v 1.4 1996/08/08 10:09:06 robertj Exp $
+ * pdirect.h
+ *
+ * File system directory class.
  *
  * Portable Windows Library
  *
- * Operating System Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pdirect.h,v $
+ * Revision 1.5  1998/09/24 03:30:14  robertj
+ * Added open software license.
+ *
  * Revision 1.4  1996/08/08 10:09:06  robertj
  * Directory structure changes for common files.
  *
