9f0e62fa715c56075a3e98d84c0e3bdd6c442a73
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 04:12:15 +0000

    Added open software license.

--- src/ptlib/unix/remconn.cxx
+++ src/ptlib/unix/remconn.cxx
@@ -1,9 +1,35 @@
 /*
- * RemConn.CXX
+ * remconn.CXX
  *
- * Simple proxy service for internet access under Unix
+ * Remote network connection (ppp) class implementation
  *
- * Copyright 1995 Equivalence
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: remconn.cxx,v $
+ * Revision 1.16  1998/09/24 04:12:15  robertj
+ * Added open software license.
+ *
  */
 
 #pragma implementation "remconn.h"
