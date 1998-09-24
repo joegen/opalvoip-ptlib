fc61383d978e020db9a53cf47e1f0dbd44a3b7ca
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-24 04:11:31 +0000

    Added open software license.

--- include/ptlib/unix/ptlib/contain.h
+++ include/ptlib/unix/ptlib/contain.h
@@ -1,13 +1,35 @@
 /*
- * $Id
+ * contain.h
+ *
+ * Low level object and container definitions.
  *
  * Portable Windows Library
  *
- * Machine dependent declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
  *
- * $Log
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: contain.h,v $
+ * Revision 1.9  1998/09/24 04:11:31  robertj
+ * Added open software license.
+ *
  */
 
 #ifndef _PCONTAIN
