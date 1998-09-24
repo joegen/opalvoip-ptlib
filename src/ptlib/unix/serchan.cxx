9099180d678445dec994bb2414a8643e84445056
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 04:12:17 +0000

    Added open software license.

--- src/ptlib/unix/serchan.cxx
+++ src/ptlib/unix/serchan.cxx
@@ -1,4 +1,36 @@
-
+/*
+ * serchan.cxx
+ *
+ * Asynchronous serial I/O channel class implementation.
+ *
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: serchan.cxx,v $
+ * Revision 1.7  1998/09/24 04:12:17  robertj
+ * Added open software license.
+ *
+ */
 
 #pragma implementation "serchan.h"
 #pragma implementation "modem.h"
