4ab0cad8b6aa8cdf1664927a2fb4acd0460946fb
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 04:11:50 +0000

    Added open software license.

--- include/ptlib/unix/ptlib/remconn.h
+++ include/ptlib/unix/ptlib/remconn.h
@@ -1,13 +1,35 @@
 /*
- * $Id: remconn.h,v 1.4 1996/09/21 05:42:12 craigs Exp $
+ * remconn.h
+ *
+ * Remote network connection (ppp) class.
  *
  * Portable Windows Library
  *
- * Operating System Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: remconn.h,v $
+ * Revision 1.5  1998/09/24 04:11:50  robertj
+ * Added open software license.
+ *
  * Revision 1.4  1996/09/21 05:42:12  craigs
  * Changes for new common files, PConfig changes and signal handling
  *
