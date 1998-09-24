aa2a67180ebd389d81813a9ed81f8aeb309cf8e0
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 04:12:29 +0000

    Added open software license.

--- src/ptlib/unix/uicmp.cxx
+++ src/ptlib/unix/uicmp.cxx
@@ -1,14 +1,35 @@
-
 /*
- * $Id: uicmp.cxx,v 1.6 1998/08/26 01:45:56 craigs Exp $
+ * uicmp.cxx
+ *
+ * ICMP socket class implementation.
  *
  * Portable Windows Library
  *
- * Socket Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1996 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: uicmp.cxx,v $
+ * Revision 1.7  1998/09/24 04:12:29  robertj
+ * Added open software license.
+ *
  * Revision 1.6  1998/08/26 01:45:56  craigs
  * Fixed error in IPHdr
  *
