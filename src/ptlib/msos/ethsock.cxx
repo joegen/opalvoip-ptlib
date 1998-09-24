64340885c9cf8c229a7bfb9b18afc9ca1629c963
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:45 +0000

    Added open software license.

--- src/ptlib/msos/ethsock.cxx
+++ src/ptlib/msos/ethsock.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: ethsock.cxx,v 1.5 1998/09/15 08:25:36 robertj Exp $
+ * ethsock.cxx
+ *
+ * Direct Ethernet socket implementation.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1994 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: ethsock.cxx,v $
+ * Revision 1.6  1998/09/24 03:30:45  robertj
+ * Added open software license.
+ *
  * Revision 1.5  1998/09/15 08:25:36  robertj
  * Fixed a number of warnings at maximum optimisation.
  *
