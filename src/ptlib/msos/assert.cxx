2ab505680bada16e01b7ebf6063dc200ed857c0a
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:39 +0000

    Added open software license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2923 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/msos/assert.cxx
+++ src/ptlib/msos/assert.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: assert.cxx,v 1.19 1997/03/18 21:22:31 robertj Exp $
+ * assert.cxx
+ *
+ * Function to implement assert clauses.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: assert.cxx,v $
+ * Revision 1.20  1998/09/24 03:30:39  robertj
+ * Added open software license.
+ *
  * Revision 1.19  1997/03/18 21:22:31  robertj
  * Display error message if assert stack dump fails
  *
