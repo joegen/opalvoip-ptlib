159f78f8f486dbeca4a465cf5ef7e56b084f00a9
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:42 +0000

    Added open software license.

--- src/ptlib/msos/dossock.cxx
+++ src/ptlib/msos/dossock.cxx
@@ -1,25 +1,38 @@
 /*
- * $Id: dossock.cxx,v 1.2 1995/02/05 00:53:15 robertj Exp $
+ * dossock.cxx
+ *
+ * MS-DOS imeplmentation of sockets.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1994 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: dossock.cxx,v $
+ * Revision 1.3  1998/09/24 03:30:42  robertj
+ * Added open software license.
+ *
  * Revision 1.2  1995/02/05 00:53:15  robertj
  * Commonised out of band stuff.
  *
-// Revision 1.1  1994/10/30  12:06:54  robertj
-// Initial revision
-//
-// Revision 1.1  1994/10/23  05:42:49  robertj
-// Initial revision
-//
-// Revision 1.1  1994/08/22  00:18:02  robertj
-// Initial revision
-//
  */
 
 #include <ptlib.h>
