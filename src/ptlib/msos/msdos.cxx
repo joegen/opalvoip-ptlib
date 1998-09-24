62ede612eefe8af7a69daa220ac4e1e2b8100e80
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:50 +0000

    Added open software license.

--- src/ptlib/msos/msdos.cxx
+++ src/ptlib/msos/msdos.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: msdos.cxx,v 1.12 1996/01/28 02:55:32 robertj Exp $
+ * msdos.cxx
+ *
+ * General class implementation for MS-DOS
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: msdos.cxx,v $
+ * Revision 1.13  1998/09/24 03:30:50  robertj
+ * Added open software license.
+ *
  * Revision 1.12  1996/01/28 02:55:32  robertj
  * WIN16 support.
  *
