7d47bc869fce8f76e29c8f2531eb75d04d142105
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-24 03:30:12 +0000

    Added open software license.

--- include/ptlib/msos/ptlib/mutex.h
+++ include/ptlib/msos/ptlib/mutex.h
@@ -1,13 +1,35 @@
 /*
- * $Id: mutex.h,v 1.1 1998/03/23 02:42:02 robertj Exp $
+ * mutex.h
+ *
+ * Mutual exclusion thread synchronisation class.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: mutex.h,v $
+ * Revision 1.2  1998/09/24 03:30:12  robertj
+ * Added open software license.
+ *
  * Revision 1.1  1998/03/23 02:42:02  robertj
  * Initial revision
  *
