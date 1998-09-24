08715127c2f30830dccbb28c88968d976e79e050
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:51 +0000

    Added open software license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2933 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/msos/mswin.cxx
+++ src/ptlib/msos/mswin.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: mswin.cxx,v 1.16 1996/02/15 14:55:02 robertj Exp $
+ * mswin.cxx
+ *
+ * General class implementations for 16 bit Windows.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 by Robert Jongbloed and Craig Southeren
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: mswin.cxx,v $
+ * Revision 1.17  1998/09/24 03:30:51  robertj
+ * Added open software license.
+ *
  * Revision 1.16  1996/02/15 14:55:02  robertj
  * Win16 compatibility
  *
