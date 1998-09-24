720b1ab564c73f800b6064bcd1d7c0f59f35e182
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:25 +0000

    Added open software license.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@2912 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/msos/ptlib/socket.h
+++ include/ptlib/msos/ptlib/socket.h
@@ -1,13 +1,35 @@
 /*
- * $Id: socket.h,v 1.13 1998/08/27 02:06:42 robertj Exp $
+ * socket.h
+ *
+ * Berkley sockets ancestor class.
  *
  * Portable Windows Library
  *
- * Socket Class Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: socket.h,v $
+ * Revision 1.14  1998/09/24 03:30:25  robertj
+ * Added open software license.
+ *
  * Revision 1.13  1998/08/27 02:06:42  robertj
  * GNU C library v6 compatibility
  *
