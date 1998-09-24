c71759b7763def19f2a88cef0635065e793eb8f6
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-24 03:30:59 +0000

    Added open software license.

--- src/ptlib/msos/win32.cxx
+++ src/ptlib/msos/win32.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: win32.cxx,v 1.65 1998/09/18 13:56:20 robertj Exp $
+ * win32.cxx
+ *
+ * Miscellaneous implementation of classes for Win32
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: win32.cxx,v $
+ * Revision 1.66  1998/09/24 03:30:59  robertj
+ * Added open software license.
+ *
  * Revision 1.65  1998/09/18 13:56:20  robertj
  * Added support of REG_BINARY registry types in PConfig class.
  *
