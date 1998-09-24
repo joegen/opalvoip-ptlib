68cb438810b9f6d77f3310b9e6dd7f2033c043f0
Robert Jongbloed <robertj@voxlucida.com.au>
1998-09-24 03:31:02 +0000

    Added open software license.

--- src/ptlib/msos/winsock.cxx
+++ src/ptlib/msos/winsock.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: winsock.cxx,v 1.38 1998/08/28 14:09:45 robertj Exp $
+ * winsock.cxx
+ *
+ * WINSOCK implementation of Berkley sockets.
  *
  * Portable Windows Library
  *
- * Operating System Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1994 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: winsock.cxx,v $
+ * Revision 1.39  1998/09/24 03:31:02  robertj
+ * Added open software license.
+ *
  * Revision 1.38  1998/08/28 14:09:45  robertj
  * Fixed bug in Write() that caused endlesss loops, introduced in previous version.
  *
