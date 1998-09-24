5116c3180670df42ea01f5666c8ae4b6818d9918
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:46 +0000

    Added open software license.

--- src/ptlib/msos/icmp.cxx
+++ src/ptlib/msos/icmp.cxx
@@ -1,13 +1,35 @@
 /*
- * $Id: icmp.cxx,v 1.9 1998/01/26 00:53:33 robertj Exp $
+ * icmp.cxx
+ *
+ * ICMP class implementation for Win32.
  *
  * Portable Windows Library
  *
- * Socket Classes Implementation
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1996 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: icmp.cxx,v $
+ * Revision 1.10  1998/09/24 03:30:46  robertj
+ * Added open software license.
+ *
  * Revision 1.9  1998/01/26 00:53:33  robertj
  * Added error codes, TTL and data buffer to Ping.
  *
