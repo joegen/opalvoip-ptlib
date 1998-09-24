8dda608449ba4dd9d1d968b209f88a664b4ffb5b
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-09-24 03:30:16 +0000

    Added open software license.

--- include/ptlib/msos/ptlib/pprocess.h
+++ include/ptlib/msos/ptlib/pprocess.h
@@ -1,13 +1,35 @@
 /*
- * $Id: pprocess.h,v 1.21 1998/04/07 13:32:58 robertj Exp $
+ * pprocess.h
+ *
+ * Operating system process (running program) class.
  *
  * Portable Windows Library
  *
- * User Interface Classes Interface Declarations
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
  *
- * Copyright 1993 Equivalence
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
  *
  * $Log: pprocess.h,v $
+ * Revision 1.22  1998/09/24 03:30:16  robertj
+ * Added open software license.
+ *
  * Revision 1.21  1998/04/07 13:32:58  robertj
  * Changed startup code to support PApplication class.
  *
