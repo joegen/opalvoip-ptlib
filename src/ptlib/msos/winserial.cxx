aa23518bc1b970d070655e2f0c650f147473a02d
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1998-11-30 12:32:47 +0000

    Added missing copyright header.

--- src/ptlib/msos/winserial.cxx
+++ src/ptlib/msos/winserial.cxx
@@ -1,3 +1,37 @@
+/*
+ * winserial.cxx
+ *
+ * Miscellaneous implementation of classes for Win32
+ *
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: winserial.cxx,v $
+ * Revision 1.2  1998/11/30 12:32:47  robertj
+ * Added missing copyright header.
+ *
+ */
+
 #include <ptlib.h>
 #include <ptlib/serchan.h>
 
