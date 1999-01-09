32e2623f134a958d089820d3ae5ba03fb266a755
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1999-01-09 03:35:09 +0000

    svnimporter: adding tags and branches to revision 3518

---
+++ include/ptlib/unix/ptlib/mutex.h
@@ -0,0 +1,62 @@
+/*
+ * mutex.h
+ *
+ * Mutual exclusion thread synchronisation class.
+ *
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, eitF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: mutex.h,v $
+ * Revision 1.4  1999/01/09 03:35:09  robertj
+ * Improved efficiency of mutex to use pthread functions directly.
+ *
+ * Revision 1.3  1998/11/30 22:06:51  robertj
+ * New directory structure.
+ *
+ * Revision 1.2  1998/09/24 04:11:41  robertj
+ * Added open software license.
+ *
+ * Revision 1.1  1998/03/24 07:31:04  robertj
+ * Initial revision
+ *
+ */
+
+
+#ifndef _PMUTEX
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PMutex
+
+#include "../../mutex.h"
+#ifdef P_PTHREADS
+  public:
+    virtual void Wait();
+    virtual BOOL Wait(const PTimeInterval & timeout);
+    virtual void Signal();
+    virtual BOOL WillBlock() const;
+#endif
+};
+
+
+#endif
