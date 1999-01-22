931a3fc22520fb44157d0c5ab1f2f821cc372bb0
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1999-01-22 01:08:30 +0000

    Distrubution Zip file creator.

---
+++ make_zip.bat
@@ -0,0 +1,15 @@
+cd ..
+wzzip -P pwlib_min.zip pwlib/Readme.txt pwlib/mpl-1.0.htm pwlib/*.dsw pwlib/include/ptlib.h
+wzzip -rP -x@pwlib/exclusions.txt pwlib_min.zip pwlib/include/ptclib
+wzzip -rP -x@pwlib/exclusions.txt pwlib_min.zip pwlib/include/ptlib
+wzzip -rP -x@pwlib/exclusions.txt pwlib_min.zip pwlib/src/ptclib
+wzzip -rP -x@pwlib/exclusions.txt pwlib_min.zip pwlib/src/ptlib
+wzzip -P pwlib_min.zip pwlib/tools/MergeSym/*.ds? pwlib/tools/MergeSym/main.cxx
+wzzip -P pwlib_min.zip pwlib/tools/asnparser/*.ds? pwlib/tools/asnparser/main.* pwlib/tools/asnparser/*.y pwlib/tools/asnparser/*.l
+
+wzzip -P pwlib_full.zip pwlib/Readme.txt pwlib/mpl-1.0.htm pwlib/*.dsw
+wzzip -rP -x@pwlib/exclusions.txt pwlib_full.zip pwlib/include
+wzzip -rP -x@pwlib/exclusions.txt pwlib_full.zip pwlib/src
+wzzip -P pwlib_full.zip pwlib/tools/MergeSym/*.ds? pwlib/tools/MergeSym/main.cxx
+wzzip -P pwlib_full.zip pwlib/tools/asnparser/*.ds? pwlib/tools/asnparser/main.* pwlib/tools/asnparser/*.y pwlib/tools/asnparser/*.l
+wzzip -P pwlib_full.zip pwlib/tools/pwrc/*.ds? pwlib/tools/pwrc/pwrc.h pwlib/tools/pwrc/main.cxx pwlib/tools/pwrc/codegen.cxx pwlib/tools/pwrc/windows.cxx pwlib/tools/pwrc/dwindows.cxx pwlib/tools/pwrc/*.y pwlib/tools/pwrc/*.l
