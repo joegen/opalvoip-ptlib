c1278ad09bdc2c9c4ae8c1093ed3da6b0c15200d
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1999-03-09 08:01:47 +0000

    Changed comments for doc++ support (more to come).

--- include/ptclib/socks.h
+++ include/ptclib/socks.h
@@ -1,11 +1,14 @@
 /*
- * main.h
+ * socks.h
  *
  * PWLib application header file for GetCyberPatrol
  *
  * Copyright 98 Equivalence
  *
  * $Log: socks.h,v $
+ * Revision 1.3  1999/03/09 08:01:47  robertj
+ * Changed comments for doc++ support (more to come).
+ *
  * Revision 1.2  1998/12/23 00:33:05  robertj
  * UDP support
  *
@@ -19,6 +22,11 @@
 
 #include <ptlib/sockets.h>
 
+
+/**
+Base class for implementing the SOCKS protocol for use with PSocksSocket and
+PSocksUDPSocket.
+*/
 
 class PSocksProtocol
 {
@@ -38,14 +46,14 @@ class PSocksProtocol
       WORD port
     );
 
+    /** Set the username and password for the SOCKS server authentication. This
+       is for the cleartext authentication only, GSSAPI, Kerberos etc is not
+       yet supported.
+     */
     void SetAuthentication(
       const PString & username,
       const PString & password
     );
-    /* Set the username and password for the SOCKS server authentication. This
-       is for the cleartext authentication only, GSSAPI, Kerberos etc is not
-       yet supported.
-     */
 
   protected:
     BOOL ConnectSocksServer(PTCPSocket & thisSocket);
@@ -72,10 +80,10 @@ class PSocksProtocol
 };
 
 
+/** This is an ancestor class allowing access to a SOCKS servers (version 4 and 5).
+ */
 class PSocksSocket : public PTCPSocket, public PSocksProtocol
 {
-/* This is an ancestor class allowing access to a SOCKS servers (version 4 and 5).
- */
   PCLASSINFO(PSocksSocket, PTCPSocket)
 
   public:
@@ -84,13 +92,7 @@ class PSocksSocket : public PTCPSocket, public PSocksProtocol
     );
 
   // Overrides from class PSocket.
-    virtual BOOL Connect(
-      const PString & address   // Address of remote machine to connect to.
-    );
-    virtual BOOL Connect(
-      const Address & addr      // Address of remote machine to connect to.
-    );
-    /* Connect a socket to a remote host on the specified port number. This is
+    /** Connect a socket to a remote host on the specified port number. This is
        typically used by the client or initiator of a communications channel.
        This connects to a "listening" socket at the other end of the
        communications channel.
@@ -98,16 +100,17 @@ class PSocksSocket : public PTCPSocket, public PSocksProtocol
        The port number as defined by the object instance construction or the
        <A>PIPSocket::SetPort()</A> function.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the channel was successfully connected to the remote host.
      */
-
-    virtual BOOL Listen(
-      unsigned queueSize = 5,  // Number of pending accepts that may be queued.
-      WORD port = 0,           // Port number to use for the connection.
-      Reusability reuse = AddressIsExclusive // Can/Cant listen more than once.
+    virtual BOOL Connect(
+      const PString & address   // Address of remote machine to connect to.
     );
-    /* Listen on a socket for a remote host on the specified port number. This
+    virtual BOOL Connect(
+      const Address & addr      // Address of remote machine to connect to.
+    );
+
+    /** Listen on a socket for a remote host on the specified port number. This
        may be used for server based applications. A "connecting" socket begins
        a connection by initiating a connection to this socket. An active socket
        of this type is then used to generate other "accepting" sockets which
@@ -119,15 +122,16 @@ class PSocksSocket : public PTCPSocket, public PSocksProtocol
 
        For the UDP protocol, the <CODE>queueSize</CODE> parameter is ignored.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the channel was successfully opened.
      */
-
-    BOOL Accept();
-    virtual BOOL Accept(
-      PSocket & socket          // Listening socket making the connection.
+    virtual BOOL Listen(
+      unsigned queueSize = 5,  // Number of pending accepts that may be queued.
+      WORD port = 0,           // Port number to use for the connection.
+      Reusability reuse = AddressIsExclusive // Can/Cant listen more than once.
     );
-    /* Open a socket to a remote host on the specified port number. This is an
+
+    /** Open a socket to a remote host on the specified port number. This is an
        "accepting" socket. When a "listening" socket has a pending connection
        to make, this will accept a connection made by the "connecting" socket
        created to establish a link.
@@ -143,38 +147,42 @@ class PSocksSocket : public PTCPSocket, public PSocksProtocol
        operating system handle transferred to the current object. This occurs
        regardless of the return value of the function.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the channel was successfully opened.
      */
+    BOOL Accept();
+    virtual BOOL Accept(
+      PSocket & socket          // Listening socket making the connection.
+    );
 
 
   // Overrides from class PIPSocket.
-    virtual BOOL GetLocalAddress(
-      Address & addr    // Variable to receive hosts IP address
-    );
-    virtual BOOL GetLocalAddress(
-      Address & addr,    // Variable to receive peer hosts IP address
-      WORD & port        // Variable to receive peer hosts port number
-    );
-    /* Get the Internet Protocol address for the local host.
+    /** Get the Internet Protocol address for the local host.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the IP number was returned.
      */
-
-    virtual BOOL GetPeerAddress(
+    virtual BOOL GetLocalAddress(
       Address & addr    // Variable to receive hosts IP address
     );
-    virtual BOOL GetPeerAddress(
+    virtual BOOL GetLocalAddress(
       Address & addr,    // Variable to receive peer hosts IP address
       WORD & port        // Variable to receive peer hosts port number
     );
-    /* Get the Internet Protocol address for the peer host the socket is
+
+    /** Get the Internet Protocol address for the peer host the socket is
        connected to.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the IP number was returned.
      */
+    virtual BOOL GetPeerAddress(
+      Address & addr    // Variable to receive hosts IP address
+    );
+    virtual BOOL GetPeerAddress(
+      Address & addr,    // Variable to receive peer hosts IP address
+      WORD & port        // Variable to receive peer hosts port number
+    );
 
 
   protected:
@@ -186,10 +194,10 @@ class PSocksSocket : public PTCPSocket, public PSocksProtocol
 };
 
 
+/** This class allows access to RFC1928 compliant SOCKS server.
+ */
 class PSocks4Socket : public PSocksSocket
 {
-/* This class allows access to RFC1928 compliant SOCKS server.
- */
   PCLASSINFO(PSocks4Socket, PSocksSocket)
 
   public:
@@ -202,8 +210,7 @@ class PSocks4Socket : public PSocksSocket
     );
 
   // Overrides from class PObject
-    virtual PObject * Clone() const;
-    /* Create a copy of the class on the heap. The exact semantics of the
+    /** Create a copy of the class on the heap. The exact semantics of the
        descendent class determine what is required to make a duplicate of the
        instance. Not all classes can even <EM>do</EM> a clone operation.
        
@@ -212,9 +219,10 @@ class PSocks4Socket : public PSocksSocket
 
        The default behaviour is for this function to assert.
 
-       <H2>Returns:</H2>
+       @return
        pointer to new copy of the class instance.
      */
+    virtual PObject * Clone() const;
 
 
   protected:
@@ -228,10 +236,10 @@ class PSocks4Socket : public PSocksSocket
 };
 
 
+/** This class allows access to RFC1928 compliant SOCKS server.
+ */
 class PSocks5Socket : public PSocksSocket
 {
-/* This class allows access to RFC1928 compliant SOCKS server.
- */
   PCLASSINFO(PSocks5Socket, PSocksSocket)
 
   public:
@@ -244,8 +252,7 @@ class PSocks5Socket : public PSocksSocket
     );
 
   // Overrides from class PObject
-    virtual PObject * Clone() const;
-    /* Create a copy of the class on the heap. The exact semantics of the
+    /** Create a copy of the class on the heap. The exact semantics of the
        descendent class determine what is required to make a duplicate of the
        instance. Not all classes can even <EM>do</EM> a clone operation.
        
@@ -254,16 +261,17 @@ class PSocks5Socket : public PSocksSocket
 
        The default behaviour is for this function to assert.
 
-       <H2>Returns:</H2>
+       @return
        pointer to new copy of the class instance.
      */
+    virtual PObject * Clone() const;
 };
 
 
+/** This class allows access to RFC1928 compliant SOCKS server.
+ */
 class PSocksUDPSocket : public PUDPSocket, public PSocksProtocol
 {
-/* This class allows access to RFC1928 compliant SOCKS server.
- */
   PCLASSINFO(PSocksUDPSocket, PUDPSocket)
 
   public:
@@ -277,8 +285,7 @@ class PSocksUDPSocket : public PUDPSocket, public PSocksProtocol
 
 
   // Overrides from class PObject
-    virtual PObject * Clone() const;
-    /* Create a copy of the class on the heap. The exact semantics of the
+    /** Create a copy of the class on the heap. The exact semantics of the
        descendent class determine what is required to make a duplicate of the
        instance. Not all classes can even <EM>do</EM> a clone operation.
        
@@ -287,19 +294,14 @@ class PSocksUDPSocket : public PUDPSocket, public PSocksProtocol
 
        The default behaviour is for this function to assert.
 
-       <H2>Returns:</H2>
+       @return
        pointer to new copy of the class instance.
      */
+    virtual PObject * Clone() const;
 
 
   // Overrides from class PSocket.
-    virtual BOOL Connect(
-      const PString & address   // Address of remote machine to connect to.
-    );
-    virtual BOOL Connect(
-      const Address & addr      // Address of remote machine to connect to.
-    );
-    /* Connect a socket to a remote host on the specified port number. This is
+    /** Connect a socket to a remote host on the specified port number. This is
        typically used by the client or initiator of a communications channel.
        This connects to a "listening" socket at the other end of the
        communications channel.
@@ -307,16 +309,17 @@ class PSocksUDPSocket : public PUDPSocket, public PSocksProtocol
        The port number as defined by the object instance construction or the
        <A>PIPSocket::SetPort()</A> function.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the channel was successfully connected to the remote host.
      */
-
-    virtual BOOL Listen(
-      unsigned queueSize = 5,  // Number of pending accepts that may be queued.
-      WORD port = 0,           // Port number to use for the connection.
-      Reusability reuse = AddressIsExclusive // Can/Cant listen more than once.
+    virtual BOOL Connect(
+      const PString & address   // Address of remote machine to connect to.
     );
-    /* Listen on a socket for a remote host on the specified port number. This
+    virtual BOOL Connect(
+      const Address & addr      // Address of remote machine to connect to.
+    );
+
+    /** Listen on a socket for a remote host on the specified port number. This
        may be used for server based applications. A "connecting" socket begins
        a connection by initiating a connection to this socket. An active socket
        of this type is then used to generate other "accepting" sockets which
@@ -328,63 +331,68 @@ class PSocksUDPSocket : public PUDPSocket, public PSocksProtocol
 
        For the UDP protocol, the <CODE>queueSize</CODE> parameter is ignored.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the channel was successfully opened.
      */
+    virtual BOOL Listen(
+      unsigned queueSize = 5,  // Number of pending accepts that may be queued.
+      WORD port = 0,           // Port number to use for the connection.
+      Reusability reuse = AddressIsExclusive // Can/Cant listen more than once.
+    );
 
   // Overrides from class PIPSocket.
-    virtual BOOL GetLocalAddress(
-      Address & addr    // Variable to receive hosts IP address
-    );
-    virtual BOOL GetLocalAddress(
-      Address & addr,    // Variable to receive peer hosts IP address
-      WORD & port        // Variable to receive peer hosts port number
-    );
-    /* Get the Internet Protocol address for the local host.
+    /** Get the Internet Protocol address for the local host.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the IP number was returned.
      */
-
-    virtual BOOL GetPeerAddress(
+    virtual BOOL GetLocalAddress(
       Address & addr    // Variable to receive hosts IP address
     );
-    virtual BOOL GetPeerAddress(
+    virtual BOOL GetLocalAddress(
       Address & addr,    // Variable to receive peer hosts IP address
       WORD & port        // Variable to receive peer hosts port number
     );
-    /* Get the Internet Protocol address for the peer host the socket is
+
+    /** Get the Internet Protocol address for the peer host the socket is
        connected to.
 
-       <H2>Returns:</H2>
+       @return
        TRUE if the IP number was returned.
      */
+    virtual BOOL GetPeerAddress(
+      Address & addr    // Variable to receive hosts IP address
+    );
+    virtual BOOL GetPeerAddress(
+      Address & addr,    // Variable to receive peer hosts IP address
+      WORD & port        // Variable to receive peer hosts port number
+    );
 
 
   // Overrides from class PIPDatagramSocket.
+    /** Read a datagram from a remote computer.
+       
+       @return
+       TRUE if any bytes were sucessfully read.
+     */
     virtual BOOL ReadFrom(
       void * buf,     // Data to be written as URGENT TCP data.
       PINDEX len,     // Number of bytes pointed to by <CODE>buf</CODE>.
       Address & addr, // Address from which the datagram was received.
       WORD & port     // Port from which the datagram was received.
     );
-    /* Read a datagram from a remote computer.
-       
-       <H2>Returns:</H2>
-       TRUE if any bytes were sucessfully read.
-     */
 
+    /** Write a datagram to a remote computer.
+
+       @return
+       TRUE if all the bytes were sucessfully written.
+     */
     virtual BOOL WriteTo(
       const void * buf,   // Data to be written as URGENT TCP data.
       PINDEX len,         // Number of bytes pointed to by <CODE>buf</CODE>.
       const Address & addr, // Address to which the datagram is sent.
       WORD port           // Port to which the datagram is sent.
     );
-    /* Write a datagram to a remote computer.
-
-       <H2>Returns:</H2>
-       TRUE if all the bytes were sucessfully written.
-     */
 
 
   protected:
