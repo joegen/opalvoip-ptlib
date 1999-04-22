934ae36dcb6aba21cf4f1cab39a4bdde9eacaceb
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1999-04-22 02:39:48 +0000

    Added  history

--- make_zip.bat
+++ make_zip.bat
@@ -1,5 +1,5 @@
 cd ..
-wzzip -P -jhrs pwlib_min.zip pwlib/Readme.txt pwlib/mpl-1.0.htm pwlib/*.dsw pwlib/include/ptlib.h
+wzzip -P -jhrs pwlib_min.zip pwlib/Readme.txt pwlib/History.txt pwlib/mpl-1.0.htm pwlib/*.dsw pwlib/include/ptlib.h
 wzzip -P -jhrs -r -x@pwlib/exclusions.txt pwlib_min.zip pwlib/include/ptclib
 wzzip -P -jhrs -r -x@pwlib/exclusions.txt pwlib_min.zip pwlib/include/ptlib
 wzzip -P -jhrs -r -x@pwlib/exclusions.txt pwlib_min.zip pwlib/src/ptclib
@@ -7,7 +7,7 @@ wzzip -P -jhrs -r -x@pwlib/exclusions.txt pwlib_min.zip pwlib/src/ptlib
 wzzip -P -jhrs pwlib_min.zip pwlib/tools/MergeSym/*.ds? pwlib/tools/MergeSym/MergeSym.cxx
 wzzip -P -jhrs pwlib_min.zip pwlib/tools/asnparser/*.ds? pwlib/tools/asnparser/main.* pwlib/tools/asnparser/precompile.cpp pwlib/tools/asnparser/*.y pwlib/tools/asnparser/*.l
 
-wzzip -P -jhrs pwlib_full.zip pwlib/Readme.txt pwlib/mpl-1.0.htm pwlib/*.dsw
+wzzip -P -jhrs pwlib_full.zip pwlib/Readme.txt pwlib/History.txt pwlib/mpl-1.0.htm pwlib/*.dsw
 wzzip -P -jhrs -r -x@pwlib/exclusions.txt pwlib_full.zip pwlib/include
 wzzip -P -jhrs -r -x@pwlib/exclusions.txt pwlib_full.zip pwlib/src
 wzzip -P -jhrs pwlib_full.zip pwlib/tools/MergeSym/*.ds? pwlib/tools/MergeSym/MergeSym.cxx
