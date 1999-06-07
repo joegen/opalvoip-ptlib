0a5c8df97b0b1c5aa96a1befcea77d8927f627e4
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
1999-06-07 01:56:26 +0000

    Added header comment on license.

--- tools/asnparser/main.h
+++ tools/asnparser/main.h
@@ -1,3 +1,37 @@
+/*
+ * main.h
+ *
+ * PWLib application header file for asnparser
+ *
+ * ASN.1 compiler to produce C++ classes.
+ *
+ * Copyright (c) 1997-1999 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: main.h,v $
+ * Revision 1.5  1999/06/07 01:56:26  robertj
+ * Added header comment on license.
+ *
+ */
+
 #ifndef _MAIN_H
 #define _MAIN_H
 
