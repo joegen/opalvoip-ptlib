e3dde9eb652a3ab1b8a8ae5f3b0744feebf8cc8c
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
2001-05-22 12:49:32 +0000

    svnimporter: adding tags and branches to revision 5348

---
+++ include/ptlib/msos/ptlib/ipsock.h
@@ -0,0 +1,92 @@
+/*
+ * ipsock.h
+ *
+ * Internet Protocol socket I/O channel class.
+ *
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: ipsock.h,v $
+ * Revision 1.7  2001/05/22 12:49:32  robertj
+ * Did some seriously wierd rewrite of platform headers to eliminate the
+ *   stupid GNU compiler warning about braces not matching.
+ *
+ * Revision 1.6  2001/01/24 06:06:34  yurik
+ * Windows CE port-related changes - actually my first change of openh323 code
+ *
+ * Revision 1.5  1998/11/30 02:55:15  robertj
+ * New directory structure
+ *
+ * Revision 1.4  1998/09/24 03:30:09  robertj
+ * Added open software license.
+ *
+ * Revision 1.3  1997/01/10 13:15:39  robertj
+ * Added unix style error codes for WinSock codes compatible with GetErrorNumber().
+ *
+ * Revision 1.2  1996/08/08 10:09:04  robertj
+ * Directory structure changes for common files.
+ *
+ * Revision 1.1  1994/07/27 06:00:10  robertj
+ * Initial revision
+ *
+ */
+
+#ifndef _PIPSOCKET
+
+#ifndef _WIN32_WCE
+#define EINPROGRESS             (WSAEINPROGRESS|0x40000000)
+#define ENOTSOCK                (WSAENOTSOCK|0x40000000)
+#define EMSGSIZE                (WSAEMSGSIZE|0x40000000)
+#define ESOCKTNOSUPPORT         (WSAESOCKTNOSUPPORT|0x40000000)
+#define EOPNOTSUPP              (WSAEOPNOTSUPP|0x40000000)
+#define EPFNOSUPPORT            (WSAEPFNOSUPPORT|0x40000000)
+#define EAFNOSUPPORT            (WSAEAFNOSUPPORT|0x40000000)
+#define EADDRINUSE              (WSAEADDRINUSE|0x40000000)
+#define EADDRNOTAVAIL           (WSAEADDRNOTAVAIL|0x40000000)
+#define ENETDOWN                (WSAENETDOWN|0x40000000)
+#define ENETUNREACH             (WSAENETUNREACH|0x40000000)
+#define ENETRESET               (WSAENETRESET|0x40000000)
+#define ECONNABORTED            (WSAECONNABORTED|0x40000000)
+#define ECONNRESET              (WSAECONNRESET|0x40000000)
+#define ENOBUFS                 (WSAENOBUFS|0x40000000)
+#define EISCONN                 (WSAEISCONN|0x40000000)
+#define ENOTCONN                (WSAENOTCONN|0x40000000)
+#define ESHUTDOWN               (WSAESHUTDOWN|0x40000000)
+#define ETOOMANYREFS            (WSAETOOMANYREFS|0x40000000)
+#define ETIMEDOUT               (WSAETIMEDOUT|0x40000000)
+#define ECONNREFUSED            (WSAECONNREFUSED|0x40000000)
+#define EHOSTDOWN               (WSAEHOSTDOWN|0x40000000)
+#define EHOSTUNREACH            (WSAEHOSTUNREACH|0x40000000)
+#endif
+
+
+///////////////////////////////////////////////////////////////////////////////
+// PIPSocket
+
+#include "../../ipsock.h"
+
+
+#endif
+
+
+// End Of File ///////////////////////////////////////////////////////////////
