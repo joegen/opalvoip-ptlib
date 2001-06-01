6c2231d2c28d7d30ce982be8a156a332945042bb
Craig Southeren <craigs@postincrement.com>
2001-06-01 07:28:23 +0000

    Added handling for binary data in multi-part MIME fields

--- src/ptclib/httpsrvr.cxx
+++ src/ptclib/httpsrvr.cxx
@@ -27,6 +27,9 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: httpsrvr.cxx,v $
+ * Revision 1.36  2001/06/01 07:28:23  craigs
+ * Added handling for binary data in multi-part MIME fields
+ *
  * Revision 1.35  2001/03/14 01:49:54  craigs
  * Added ability to handle multi-part form POST commands
  *
@@ -336,26 +339,113 @@ void PHTTPConnectionInfo::DecodeMultipartFormInfo(const PString & type, const PS
     if (pos == P_MAX_INDEX) 
       return;
   }
-  PString sep = type.Mid(pos+1).Trim();
+  PString seperator = type.Mid(pos+1).Trim();
 
   // remove "boundary"
-  pos = sep.Find("boundary");
+  pos = seperator.Find("boundary");
   if (pos == P_MAX_INDEX)
     return;
-  sep = sep.Mid(8).Trim();
+  seperator = seperator.Mid(8).Trim();
 
   // remove "="
-  pos = sep.Find("=");
+  pos = seperator.Find("=");
   if (pos == P_MAX_INDEX)
     return;
-  sep = sep.Mid(1).Trim();
+  seperator = seperator.Mid(1).Trim();
 
-  PStringStream data(entityBody);
-  PString line;
+  // seperators have a "--" according to RFC 1521
+  seperator = PString("--") + seperator;
 
-  // the first seperator has a leading "--"
-  sep = PString("--") + sep;
+  PINDEX sepLen = seperator.GetLength();
+  const char * sep = (const char *)seperator;
 
+  // split body into parts, assuming binary data
+  const char * body = (const char *)entityBody;
+  PINDEX entityOffs = 0;
+  PINDEX entityLen = entityBody.GetSize()-1;
+
+  BOOL ignore = TRUE;
+  BOOL last = FALSE;
+
+  PMultipartFormInfo * info = NULL;
+
+  while (!last && (entityOffs < entityLen)) {
+
+    // find end of part
+    PINDEX partStart = entityOffs;
+    PINDEX partLen;
+    BOOL foundSep = FALSE;
+
+    // collect length of part until seperator
+    for (partLen = 0; (partStart + partLen) < entityLen; partLen++) {
+      if ((partLen >= sepLen) && (memcmp(body + partStart + partLen - sepLen, sep, sepLen) == 0)) {
+        foundSep = TRUE;
+        break;
+      }
+    }
+
+    // move entity ptr to the end of the part
+    entityOffs = partStart + partLen;
+
+    // if no seperator found, then this is the last part
+    // otherwise, look for "--" trailer on seperator and remove CRLF
+    if (!foundSep)
+      last = TRUE;
+    else {
+      partLen -= sepLen;
+
+      // determine if this is the last block
+      if (((entityOffs + 2) <= entityLen) && (body[entityOffs] == '-') && (body[entityOffs+1] == '-')) {
+        last = TRUE;
+        entityOffs += 2;
+      }
+
+      // remove crlf
+      if (((entityOffs + 2) <= entityLen) && (body[entityOffs] == '\r') && (body[entityOffs+1] == '\n')) 
+        entityOffs += 2;
+    }
+
+    // ignore everything up to the first seperator, 
+    // then adjust seperator to include leading CRLF
+    if (ignore) {
+      ignore = FALSE;
+      seperator = PString("\r\n") + seperator;
+      sepLen = seperator.GetLength();
+      sep = (const char *)seperator;
+      continue;
+    }
+
+    // extract the MIME header, by looking for a double CRLF
+    PINDEX ptr;
+    PINDEX nlCount = 0;
+    for (ptr = partStart;(ptr < (partStart + partLen)) && (nlCount < 2); ptr++) {
+      if (body[ptr] == '\r') {
+        nlCount++;
+        if ((ptr < entityLen-1) && (body[ptr+1] == '\n'))
+          ptr++;
+      } else
+        nlCount = 0;
+    }
+
+    // create the new part info
+    info = new PMultipartFormInfo;
+
+    // read MIME information
+    PStringStream strm(PString(body + partStart, ptr - partStart));
+    info->mime.ReadFrom(strm);
+
+    // save the entity body, being careful of binary files
+    int savedLen = partStart + partLen - ptr;
+    char * saved = info->body.GetPointer(savedLen + 1);
+    memcpy(saved, body + ptr, savedLen);
+    saved[savedLen] = '\0';
+
+    // add the data to the array
+    multipartFormInfoArray.Append(info);
+    info = NULL;
+  }
+  
+#if 0
   // ignore until first separator
   do {
     data >> line;
@@ -376,18 +466,49 @@ void PHTTPConnectionInfo::DecodeMultipartFormInfo(const PString & type, const PS
     // get the content type
     PString type = info->mime(PHTTP::ContentTypeTag);
 
+    // check the encoding
+    PString encoding = info->mime("Content-Transfer-Encoding");
+
     // accumulate text until another seperator or end of data
-    PString body;
+    PString & buf = info->body;
+    PINDEX len = 0;
+    buf.SetSize(len+1);
+    buf[0] = '\0';
+    PINDEX sepLen = sep.GetLength();
+    const char * sepPtr = (const char *)sep;
+    while (data.good()) {
+      buf.SetSize(len);
+      data >> buf[len++];
+      if ((len >= sepLen) && (memcmp(((const char *)buf) + len - sepLen, sepPtr, sepLen) == 0)) {
+        char ch;
+        data >> ch;
+        if (ch != 0x0d)
+          data.putback(ch); 
+        else {
+          data >> ch;
+          if (ch != 0x0a)
+            data.putback(ch); 
+        }
+        len -= sepLen;
+        break;
+      }
+    }
+    buf.SetSize(len+1);
+    buf[len] = '\0';
+
+    /*
     while (data.good()) {
       data >> line;
       if (line.Find(sep) == 0)
         break;
       info->body += line + "\n";
     } 
+    */
 
     multipartFormInfoArray.Append(info);
     info = NULL;
   }
+#endif
 }
 
 BOOL PHTTPServer::ProcessCommand()
