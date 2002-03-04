b7f4d1a7c3d458bebdddce2ccd18d341a0aa2504
yurik <yurik@023b2edf-31b2-4de3-b41e-bca80c47788f>
2002-03-04 22:58:10 +0000

    Attempt to minimize number of vfw functions in order to load them from a dll
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@6341 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/wince/ConsoleCE/cevfw.cpp
+++ src/ptlib/wince/ConsoleCE/cevfw.cpp
@@ -7,130 +7,15 @@
 #include <mmsystemx.h>
 #include <cevfw.h>
 
-// Vfw
-DWORD capGetVideoFormat(HWND hwnd, LPCAPSTATUS s, UINT wSize)
+BOOL VFWAPI capDefGetDriverDescription (UINT, LPSTR, int, LPSTR, int ) { return FALSE; }
+HWND VFWAPI capDefCreateCaptureWindow(LPCSTR, DWORD, int, int, int, int, HWND, int) { return NULL; }
+
+// Functions
+CAPGETDRIVERDESCRIPTIONPROC capGetDriverDescription; // = capDefGetDriverDescription;
+CAPCREATECAPTUREWINDOWPROC capCreateCaptureWindow; // = capDefCreateCaptureWindow;
+
+
+void test()
 {
-	// Not yet implemented
-	return -1L;
+	capGetDriverDescription = capDefGetDriverDescription;
 }
-
-DWORD capGetVideoFormatSize(HWND hwnd)
-{
-	// Not yet implemented
-	return -1L;
-}
-
-BOOL capGetStatus(HWND hwnd, LPCAPSTATUS s, int wSize)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-DWORD capGetVideoFormat(HWND hwnd, LPVOID s, int wSize)
-{
-	// Not yet implemented
-	return -1L;
-}
-
-BOOL capSetVideoFormat(HWND hwnd, LPVOID s, int wSize)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-
-BOOL capGrabFrameNoStop(HWND hwnd)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-
-BOOL capGetDriverDescription (UINT wDriverIndex,
-        LPSTR lpszName, int cbName,
-        LPSTR lpszVer, int cbVer)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-
-HWND capCreateCaptureWindow (
-        LPCSTR lpszWindowName,
-        DWORD dwStyle,
-        int x, int y, int nWidth, int nHeight,
-        HWND hwndParent, int nID)
-{
-	// Not yet implemented
-	return NULL;
-}
-
-BOOL capSetCallbackOnError(HWND hwnd, CAPERRORCALLBACK fpProc)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-BOOL capSetCallbackOnFrame(HWND hwnd, CAPVIDEOCALLBACK fpProc)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-BOOL capSetCallbackOnVideoStream(HWND hwnd, CAPVIDEOCALLBACK fpProc)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-
-BOOL capDriverConnect(HWND hwnd, int i) 
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-BOOL capDriverDisconnect(HWND hwnd)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-
-BOOL capSetUserData(HWND hwnd, LPVOID lUser)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-LPVOID capGetUserData(HWND hwnd)
-{
-	// Not yet implemented
-	return NULL;
-}
-
-BOOL capCaptureGetSetup(HWND hwnd, LPCAPTUREPARMS s, int wSize)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-BOOL capCaptureSetSetup(HWND hwnd, LPCAPTUREPARMS s, int wSize)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-
-BOOL capDriverGetCaps(HWND hwnd, LPCAPDRIVERCAPS s, int wSize)
-{
-	// Not yet implemented
-	return FALSE;
-}
-
-BOOL capPreview(HWND hwnd, BOOL f) 
-{
-	// Not yet implemented
-	return FALSE;
-}
-
