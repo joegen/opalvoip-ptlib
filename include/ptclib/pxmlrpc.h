1e66d46234625c869c46abb6c24be83d41b86bfb
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
2002-08-13 03:02:07 +0000

    Removed previous fix for memory leak, as object was already deleted.

--- include/ptclib/pxmlrpc.h
+++ include/ptclib/pxmlrpc.h
@@ -6,6 +6,9 @@
  * Copyright 2002 Equivalence
  *
  * $Log: pxmlrpc.h,v $
+ * Revision 1.6  2002/08/13 03:02:07  robertj
+ * Removed previous fix for memory leak, as object was already deleted.
+ *
  * Revision 1.5  2002/08/13 01:55:00  craigs
  * Fixed memory leak on PXMLRPCRequest class
  *
@@ -114,7 +117,6 @@ class PXMLRPCRequest : public PXML
   PCLASSINFO(PXMLRPCRequest, PXML);
   public:
     PXMLRPCRequest(const PString & method);
-    ~PXMLRPCRequest();
 
     PXMLRPCParams * GetParams()  { return params; }
 
