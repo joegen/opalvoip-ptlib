714a8f50d8b934721a34a40b34c2b6dd002ae21b
rogerh <rogerh@023b2edf-31b2-4de3-b41e-bca80c47788f>
2002-11-25 16:22:53 +0000

    Add Find IP, a program to find the real IP address of a machine. Handy for NAT users.

---
+++ samples/find_ip/find_ip.cxx
@@ -0,0 +1,77 @@
+//
+// find_ip.cxx
+//
+// When used behind NAT, this finds the IP address of the NAT box.
+//
+// Roger Hardiman
+//
+
+#include <ptlib.h>
+#include <ptclib/http.h>
+#include <ptclib/html.h>
+
+class FindIP : public PProcess
+{
+  PCLASSINFO(FindIP, PProcess)
+  public:
+    void Main();
+    BOOL get_ip(PString server_url, PString &ip_address);
+};
+
+PCREATE_PROCESS(FindIP)
+
+void FindIP::Main()
+{
+  cout << "Find IP - Find the IP address of your NAT box" << endl;
+  PString ip;
+
+  if (get_ip("http://checkip.dyndns.org/",ip)) {
+    cout << ip << endl;
+  }
+
+  if (get_ip("http://www.ip-calculator.com/",ip)) {
+    cout << ip << endl;
+  }
+
+  if (get_ip("http://www.lawrencegoetz.com/programs/ipinfo/",ip)) {
+    cout << ip << endl;
+  }
+
+  if (get_ip("http://www.whatismyipaddress.com/",ip)) {
+    cout << ip << endl;
+  }
+}
+
+BOOL FindIP::get_ip(PString server_url, PString &ip_address){
+
+  ip_address = "";
+   BOOL result = FALSE;
+
+  PHTTPClient web("webserver");
+  PINDEX len;
+  cout << "----------------------------------------" << endl;
+  cout << "Connecting to " << server_url << endl;
+  if (web.GetDocument(server_url,len)) {
+    PString html = web.ReadString(len);
+    if (!html.IsEmpty()) {
+//    cout << "The web server returned the data" << endl << html << endl;
+      // Now parse the HTML and pull out the IP address. Just look
+      // for anything of the form N.N.N.N where N is an integer.
+      PRegularExpression regex("[0-9]*[.][0-9]*[.][0-9]*[.][0-9]*");
+      PINDEX pos,len;
+      if (html.FindRegEx(regex, pos, len)) {
+        ip_address = html.Mid(pos,len);
+        cout << "Your real IP is " << ip_address << endl;
+	result = TRUE;
+      }
+    } else {
+      PError << "Page is empty." << endl;
+    }
+  } else {
+    PError << "Could not get page." << endl;
+  }
+  cout << "----------------------------------------" << endl;
+  return result;
+}
+
+// End of find_ip.cxx
