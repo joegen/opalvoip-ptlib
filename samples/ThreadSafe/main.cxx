9aa5c843eb14243d5603d86443553d8f5747c3a4
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
2002-12-11 03:38:45 +0000

    Added more tests

--- samples/ThreadSafe/main.cxx
+++ samples/ThreadSafe/main.cxx
@@ -8,6 +8,9 @@
  * Copyright 2002 Equivalence
  *
  * $Log: main.cxx,v $
+ * Revision 1.3  2002/12/11 03:38:45  robertj
+ * Added more tests
+ *
  * Revision 1.2  2002/05/02 00:30:26  robertj
  * Added dump of thread times during start up.
  *
@@ -25,20 +28,91 @@
 PCREATE_PROCESS(ThreadSafe);
 
 
+///////////////////////////////////////////////////////////////////////////////
+
+TestObject::TestObject(ThreadSafe & proc, unsigned val)
+  : process(proc)
+{
+  value = val;
+
+  process.mutexObjects.Wait();
+  process.totalObjects++;
+  process.currentObjects++;
+  process.mutexObjects.Signal();
+}
+
+
+TestObject::~TestObject()
+{
+  process.mutexObjects.Wait();
+  process.currentObjects--;
+  process.mutexObjects.Signal();
+}
+
+
+PObject::Comparison TestObject::Compare(const PObject & obj)
+{
+  PAssert(obj.IsDescendant(Class()), PInvalidCast);
+  unsigned othervalue = ((const TestObject &)obj).value;
+  if (value < othervalue)
+    return LessThan;
+  if (value > othervalue)
+    return GreaterThan;
+  return EqualTo;
+}
+
+
+void TestObject::PrintOn(ostream & strm) const
+{
+  strm << value;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
 ThreadSafe::ThreadSafe()
   : PProcess("Equivalence", "ThreadSafe", 1, 0, AlphaCode, 1)
 {
-  threadCount = 99;
+  threadCount = 0;
   totalObjects = 0;
   currentObjects = 0;
+}
+
+
+ThreadSafe::~ThreadSafe()
+{
+  unsorted.RemoveAll();
+  sorted.RemoveAll();
+  sparse.RemoveAll();
 }
 
 
 void ThreadSafe::Main()
 {
   PArgList & args = GetArguments();
+  args.Parse("1-test1."
+             "2-test2."
+             "3-test3."
+             "t-trace.");
+
+  if (args.HasOption('t'))
+    PTrace::Initialise(args.GetOptionCount('t'));
+
+  if (args.HasOption('1'))
+    Test1(args);
+  else if (args.HasOption('2'))
+    Test2(args);
+  else if (args.HasOption('3'))
+    Test3(args);
+}
+
+
+void ThreadSafe::Test1(PArgList & args)
+{
   if (args.GetCount() > 0)
     threadCount = args[0].AsUnsigned();
+  else
+    threadCount = 99;
 
   cout << "Starting " << threadCount << " threads." << endl;
 
@@ -47,25 +121,25 @@ void ThreadSafe::Main()
     cout << setw(4) << (i+1) << '=' << duration;
     if (i%5 == 4)
       cout << '\n';
-    PThread::Create(PCREATE_NOTIFIER(TestThread), duration.GetMilliSeconds());
+    PThread::Create(PCREATE_NOTIFIER(Test1Thread), (INT)duration.GetMilliSeconds());
   }
   cout << endl;
 
   startTick = PTimer::Tick();
   while (threadCount > 0) {
-    Test();
+    Test1Output();
     Sleep(5000);
   }
 
-  Test();
+  Test1Output();
   sorted.RemoveAll();
   unsorted.RemoveAll();
   sparse.RemoveAll();
-  Test();
+  Test1Output();
 }
 
 
-void ThreadSafe::Test()
+void ThreadSafe::Test1Output()
 {
   sorted.DeleteObjectsToBeRemoved();
   unsorted.DeleteObjectsToBeRemoved();
@@ -86,7 +160,7 @@ void ThreadSafe::Test()
 }
 
 
-void ThreadSafe::TestThread(PThread &, INT duration)
+void ThreadSafe::Test1Thread(PThread &, INT duration)
 {
   PRandom random;
   PSafePtr<TestObject> ptr;
@@ -179,41 +253,105 @@ void ThreadSafe::TestThread(PThread &, INT duration)
 }
 
 
-TestObject::TestObject(ThreadSafe & proc, unsigned val)
-  : process(proc)
+void ThreadSafe::Test2(PArgList &)
 {
-  value = val;
+  sparse.SetAt(0, new TestObject(*this, 0));
 
-  process.mutexObjects.Wait();
-  process.totalObjects++;
-  process.currentObjects++;
-  process.mutexObjects.Signal();
+  threadCount = 2;
+  PThread::Create(PCREATE_NOTIFIER(Test2Thread1));
+  PThread::Create(PCREATE_NOTIFIER(Test2Thread2));
+
+  while (threadCount > 0)
+    Sleep(1000);
 }
 
 
-TestObject::~TestObject()
+void ThreadSafe::Test2Thread1(PThread &, INT)
 {
-  process.mutexObjects.Wait();
-  process.currentObjects--;
-  process.mutexObjects.Signal();
+  cout << "Thread 1 before read only lock" << endl;
+  PSafePtr<TestObject> ptr = sparse.FindWithLock(0, PSafeReadOnly);
+
+  cout << "Thread 1 after read only lock, pausing ..." << endl;
+  Sleep(3000);
+
+  cout << "Thread 1 before read write lock" << endl;
+  ptr = sparse.FindWithLock(0, PSafeReadWrite);
+
+  cout << "Thread 1 after read write lock, exiting" << endl;
+
+  threadCount--;
 }
 
 
-PObject::Comparison TestObject::Compare(const PObject & obj)
+void ThreadSafe::Test2Thread2(PThread &, INT)
 {
-  PAssert(obj.IsDescendant(Class()), PInvalidCast);
-  unsigned othervalue = ((const TestObject &)obj).value;
-  if (value < othervalue)
-    return LessThan;
-  if (value > othervalue)
-    return GreaterThan;
-  return EqualTo;
+  Sleep(1000);
+
+  cout << "Thread 2 before read write lock" << endl;
+  PSafePtr<TestObject> ptr = sparse.FindWithLock(0, PSafeReadWrite);
+
+  cout << "Thread 2 after read write lock, exiting" << endl;
+  threadCount--;
 }
 
 
-void TestObject::PrintOn(ostream & strm) const
+void ThreadSafe::Test3(PArgList &)
 {
-  strm << value;
+  for (PINDEX i = 0; i < 10; i++)
+    unsorted.Append(new TestObject(*this, i));
+
+  threadCount = 2;
+  PThread::Create(PCREATE_NOTIFIER(Test3Thread1));
+  PThread::Create(PCREATE_NOTIFIER(Test3Thread2));
+
+  while (threadCount > 0)
+    Sleep(1000);
+}
+
+
+void ThreadSafe::Test3Thread1(PThread &, INT)
+{
+  {
+    cout << "Thread 1 before read only lock" << endl;
+    PSafePtr<TestObject> ptr = unsorted.GetWithLock(2, PSafeReadOnly);
+
+    cout << "Thread 1 after read only lock, pausing ..." << endl;
+    Sleep(2000);
+
+    cout << "Thread 1 before read write lock" << endl;
+    ptr.SetSafetyMode(PSafeReadWrite);
+
+    cout << "Thread 1 after read write lock, before ptr going out of scope" << endl;
+  }
+  cout << "Thread 1 after ptr out of scope, exiting" << endl;
+
+  threadCount--;
+}
+
+
+void ThreadSafe::Test3Thread2(PThread &, INT)
+{
+  Sleep(1000);
+
+  cout << "Thread 2 before enumeration" << endl;
+  PSafePtr<TestObject> ptr = unsorted.GetWithLock(0, PSafeReadOnly);
+  while (ptr != NULL) {
+    if (ptr->value == 2) {
+      cout << "Thread 2 before read write lock" << endl;
+      ptr->LockReadWrite();
+      cout << "Thread 2 after read write lock" << endl;
+
+      Sleep(2000);
+
+      cout << "Thread 2 before read write unlock" << endl;
+      ptr->UnlockReadWrite();
+      cout << "Thread 2 after read write unlock" << endl;
+    }
+    ptr++;
+  }
+
+  cout << "Thread 2 after enumeration, exiting" << endl;
+  threadCount--;
 }
 
 
