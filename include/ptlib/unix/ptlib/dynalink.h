3ccc3efcb69650a7c5ca0204ae79838672289bcd
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
2003-01-24 10:21:06 +0000

    svnimporter: adding tags and branches to revision 7456

---
+++ include/ptlib/unix/ptlib/dynalink.h
@@ -0,0 +1,58 @@
+/*
+ * dynalink.h
+ *
+ * Dynamic Link Library class.
+ *
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $log$
+ */
+
+#if !defined(__BEOS__) && !defined(P_VXWORKS) && !defined(P_RTEMS)
+
+#ifndef _PDYNALINK
+
+#ifdef P_USE_PRAGMA
+#pragma interface
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+// PDynaLink
+
+#define _PDYNALINK_PLATFORM_INCLUDE
+#include "../../dynalink.h"
+
+#endif
+#ifdef _PDYNALINK_PLATFORM_INCLUDE
+#undef _PDYNALINK_PLATFORM_INCLUDE
+
+  protected:
+    void * dllHandle;
+
+#endif
+
+#endif // !__BEOS__ && !P_VXWORKS
+
+
+// End Of File ////////////////////////////////////////////////////////////////
