c72606769d6dc25744bcf335836d5ee6915d5c8e
csoutheren <csoutheren@023b2edf-31b2-4de3-b41e-bca80c47788f>
2003-09-17 06:29:40 +0000

    Initial version

---
+++ include/unistd.h
@@ -0,0 +1,5 @@
+// The output from the GNU flex tool always contains "#include <unistd.h>"
+// and Microsoft does not provide that file. So this file allows it to compile
+#ifndef _WIN32
+#include "unistd.h"
+#endif
