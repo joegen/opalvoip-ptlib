3a5afe536dff031912d782ebad2b44c5276ba4ad
Craig Southeren <craigs@postincrement.com>
2003-09-18 10:47:44 +0000

    Removed because it causes problems with Unix builds Windows users will have to create unistd.h

--- include/unistd.h
+++
@@ -1,6 +0,0 @@
-// The output from the GNU flex tool always contains "#include <unistd.h>"
-// and Microsoft does not provide that file. So this file allows it to compile
-#ifndef _WIN32
-#include "unistd.h"
-#endif
-
