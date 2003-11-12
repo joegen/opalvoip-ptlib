76179901d285c912589b4c2834bfc698bbc083bf
csoutheren <csoutheren@023b2edf-31b2-4de3-b41e-bca80c47788f>
2003-11-12 03:26:17 +0000

    Initial version of plugin code from Snark of GnomeMeeting with changes    by Craig Southeren os Post Increment

---
+++ include/ptlib/plugin.h
@@ -0,0 +1,140 @@
+/*
+ * plugin.h
+ *
+ * Plugin Class Declarations
+ *
+ * Portable Windows Library
+ *
+ * Contributor(s): Snark at GnomeMeeting
+ *
+ * $Log: plugin.h,v $
+ * Revision 1.2  2003/11/12 03:26:17  csoutheren
+ * Initial version of plugin code from Snark of GnomeMeeting with changes
+ *    by Craig Southeren os Post Increment
+ *
+ *
+ */
+
+#ifndef _PLUGIN_H
+#define _PLUGIN_H
+
+#define PWLIB_PLUGIN_API_VERSION 0
+
+//////////////////////////////////////////////////////
+//
+//  Ancestor Service descriptor for plugins
+//
+
+class PPluginServiceDescriptor 
+{
+  public:
+    PPluginServiceDescriptor(unsigned (*_GetPluginAPIVersion)())
+      : GetPluginAPIVersion(_GetPluginAPIVersion)
+    { }
+
+    unsigned (*GetPluginAPIVersion)();
+};
+
+
+//////////////////////////////////////////////////////
+//
+// Define a service provided by a plugin, which consists of the following:
+//
+//    serviceType - the base class name of the service which is used to identify
+//                  the service type, such as PSoundChannel, 
+//
+//    serviceName - the name of the service provided by the plugin. This will usually
+//                  be related to the class implementing the service, such as:
+//                       service name = OSS, class name = PSoundChannelOSS
+//
+//    descriptor  - a pointer to a class containing pointers to any static functions
+//                  for this class
+//   
+//
+
+class PPluginService: public PObject
+{
+  public:
+    PPluginService(const PString & _serviceName,
+                   const PString & _serviceType,
+                   PPluginServiceDescriptor *_descriptor)
+    {
+      serviceName = _serviceName;
+      serviceType = _serviceType;
+      descriptor  = _descriptor;
+    }
+
+    PString serviceName;
+    PString serviceType;
+    PPluginServiceDescriptor * descriptor;
+};
+
+//////////////////////////////////////////////////////
+
+#define PCREATE_PLUGIN_VERSION_DECLARE \
+
+//////////////////////////////////////////////////////
+
+#define PCREATE_PLUGIN_REGISTERER(serviceName, serviceType, descriptor) \
+class PPlugin_##serviceType##_##serviceName##_Registration { \
+  public: \
+    PPlugin_##serviceType##_##serviceName##_Registration(PPluginManager * pluginMgr) \
+    { pluginMgr->RegisterService(#serviceName, #serviceType, descriptor); } \
+    int kill_warning; \
+}; \
+
+//////////////////////////////////////////////////////
+
+#define PCREATE_STATIC_PLUGIN_VERSION_FN(serviceName, serviceType) \
+unsigned PPlugin_##serviceType##_##serviceName##_GetVersion() \
+  { return PWLIB_PLUGIN_API_VERSION; } 
+
+#define PCREATE_PLUGIN_STATIC(serviceName, serviceType, descriptor) \
+PCREATE_PLUGIN_REGISTERER(serviceName, serviceType, descriptor) \
+PPlugin_##serviceType##_##serviceName##_Registration \
+     PPlugin_##serviceType##_##serviceName##_Registration_Instance(&PPluginManager::GetPluginManager()); 
+
+
+//////////////////////////////////////////////////////
+
+#define PCREATE_DYNAMIC_PLUGIN_VERSION_FN(serviceName, serviceType) \
+extern "C" unsigned PWLibPlugin_GetAPIVersion () \
+{ return PWLIB_PLUGIN_API_VERSION; } 
+
+
+#define PCREATE_PLUGIN_DYNAMIC(serviceName, serviceType, descriptor) \
+PCREATE_PLUGIN_REGISTERER(serviceName, serviceType, descriptor) \
+extern "C" void PWLibPlugin_TriggerRegister (PPluginManager * pluginMgr) { \
+PPlugin_##serviceType##_##serviceName##_Registration \
+     pplugin_##serviceType##_##serviceName##_Registration_Instance(pluginMgr); \
+     pplugin_##serviceType##_##serviceName##_Registration_Instance.kill_warning = 0; \
+} \
+
+//////////////////////////////////////////////////////
+
+#ifdef P_HAS_PLUGINS
+#  define PCREATE_PLUGIN(serviceName, serviceType, descriptor) \
+    PCREATE_PLUGIN_DYNAMIC(serviceName, serviceType, descriptor)
+
+#  define PCREATE_PLUGIN_VERSION_FN(serviceName, serviceType) \
+    PCREATE_DYNAMIC_PLUGIN_VERSION_FN(serviceName, serviceType)
+
+#  define PPLUGIN_VERSION_FN(serviceName, serviceType) \
+    PWLibPlugin_GetAPIVersion
+
+#else
+
+#  define PCREATE_PLUGIN(serviceName, serviceType, descriptor) \
+    PCREATE_PLUGIN_STATIC(serviceName, serviceType, descriptor)
+
+#  define PCREATE_PLUGIN_VERSION_FN(serviceName, serviceType) \
+    PCREATE_STATIC_PLUGIN_VERSION_FN(serviceName, serviceType)
+
+#  define PPLUGIN_VERSION_FN(serviceName, serviceType) \
+    PPlugin_##serviceType##_##serviceName##_GetVersion
+
+#endif
+
+//////////////////////////////////////////////////////
+
+#endif
