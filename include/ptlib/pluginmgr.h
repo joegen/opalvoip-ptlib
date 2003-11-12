81f9b190529d9b14e20b2879390fa59cf12b1496
Craig Southeren <craigs@postincrement.com>
2003-11-12 03:26:17 +0000

    Initial version of plugin code from Snark of GnomeMeeting with changes    by Craig Southeren os Post Increment

---
+++ include/ptlib/pluginmgr.h
@@ -0,0 +1,67 @@
+/*
+ * pluginmgr.h
+ *
+ * Plugin Manager Class Declarations
+ *
+ * Portable Windows Library
+ *
+ * Contributor(s): Snark at GnomeMeeting
+ *
+ * $Log: pluginmgr.h,v $
+ * Revision 1.2  2003/11/12 03:26:17  csoutheren
+ * Initial version of plugin code from Snark of GnomeMeeting with changes
+ *    by Craig Southeren os Post Increment
+ *
+ *
+ */
+
+#ifndef _PLUGINMGR_H
+#define _PLUGINMGR_H
+
+#define DEFAULT_PLUGINDIR "/usr/lib/pwlib"
+
+#include <ptlib/plugin.h>
+
+
+//////////////////////////////////////////////////////
+//
+//  Manager for plugins
+//
+
+class PPluginDynamic;
+
+class PPluginManager : public PObject
+{
+  PCLASSINFO(PPluginManager, PObject);
+
+  public:
+    PPluginManager (); 
+    ~PPluginManager ();
+
+    // functions to load/unload a dynamic plugin 
+    BOOL LoadPlugin (const PString & fileName);
+    void LoadPluginDirectory (const PDirectory &directory);
+
+    // functions to access the plugins' services 
+    PStringList GetPluginTypes() const;
+    PStringList GetPluginsProviding (const PString & serviceType) const;
+    PPluginServiceDescriptor * GetServiceDescriptor (const PString & serviceName, const PString & serviceType);
+
+    // function to register a service (used by the plugins themselves)
+    BOOL RegisterService (const PString & serviceName, const PString & serviceType, PPluginServiceDescriptor * descriptor);
+
+    // static functions for accessing global instances of plugin managers
+    static PPluginManager & GetPluginManager();
+
+  protected:
+    PMutex pluginListMutex;
+    PList<PPluginDynamic> pluginList;
+    
+    PMutex serviceListMutex;
+    PList<PPluginService> serviceList;
+
+    static PPluginManager * systemPluginMgr;
+    static PMutex           systemPluginMgrInitMutex;
+};
+
+#endif // ifndef _PLUGINMGR_H
