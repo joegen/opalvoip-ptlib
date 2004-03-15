04bc83b413e34ee9370a862215400301430f38c2
rogerhardiman <rogerhardiman@023b2edf-31b2-4de3-b41e-bca80c47788f>
2004-03-15 09:33:50 +0000

    Add warning about putting comments into version.h
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@8908 023b2edf-31b2-4de3-b41e-bca80c47788f

--- version.h
+++ version.h
@@ -25,6 +25,13 @@
 #ifndef _PWLib_VERSION_H
 #define _PWLib_VERSION_H
 
+
+
+/* WARNING: You MUST NOT add any comments to the #defines which follow
+   or add extra commented out #defines as this will confuse the parser
+   which extracts the version numbers
+ */
+
 #define MAJOR_VERSION 1
 #define MINOR_VERSION 6
 #define BUILD_TYPE    ReleaseCode
