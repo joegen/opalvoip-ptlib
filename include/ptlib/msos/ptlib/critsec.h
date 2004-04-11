9fd33ced99d8f66b5d397c3b1587f58cdcb36876
csoutheren <csoutheren@023b2edf-31b2-4de3-b41e-bca80c47788f>
2004-04-11 02:55:18 +0000

    svnimporter: adding tags and branches to revision 9113

---
+++ include/ptlib/msos/ptlib/critsec.h
@@ -0,0 +1,36 @@
+/*
+ * critsec.h
+ *
+ * Critical section mutex class.
+ *
+ * Portable Windows Library
+ *
+ * Copyright (C) 2004 Post Increment
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Post Increment
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: critsec.h,v $
+ * Revision 1.1  2004/04/11 02:55:18  csoutheren
+ * Added PCriticalSection for Windows
+ * Added compile time option for PContainer to use critical sections to provide thread safety under some circumstances
+ *
+ */
+
+  // Windows specific critical section implementation
+  CRITICAL_SECTION criticalSection; 
+
+// End Of File ///////////////////////////////////////////////////////////////
