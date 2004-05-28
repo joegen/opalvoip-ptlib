e26b4255fc5790dbe84f7be8cca5866f6da2eccf
Craig Southeren <craigs@postincrement.com>
2004-05-28 06:50:42 +0000

    Reorganised DNS functions to use templates, and exposed more internals to allow new DNS lookup types to be added

--- include/ptclib/pdns.h
+++ include/ptclib/pdns.h
@@ -24,6 +24,9 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: pdns.h,v $
+ * Revision 1.6  2004/05/28 06:50:42  csoutheren
+ * Reorganised DNS functions to use templates, and exposed more internals to allow new DNS lookup types to be added
+ *
  * Revision 1.5  2003/07/22 23:52:20  dereksmithies
  * Fix from Fabrizio Ammollo to cope with when P_DNS is disabled. Thanks!
  *
@@ -49,89 +52,271 @@
 #pragma interface
 #endif
 
-
 #include <ptlib/sockets.h>
 
+#include <ptclib/random.h>
 
-// implement DNS lookup for MX and SRV records
+#if defined(_WIN32)
 
-class PDNS : public PObject
-{
+#  include <windns.h>
+#  pragma comment(lib, P_DNS_LIBRARY)
+
+#else
+
+#  define  P_HAS_RESOLVER 1         // set if using Unix-style DNS routines
+#  include <arpa/nameser.h>
+#  include <resolv.h>
+#  if defined(P_MACOSX) && (P_MACOSX >= 700)
+#    include <arpa/nameser_compat.h>
+#  endif
+
+#endif  // _WIN32
+
+#ifdef P_HAS_RESOLVER
+
+//////////////////////////////////////////////////////////////////////////
+//
+// these classes provide an emulation of the Microsoft DNS API 
+// on non-Window systems
+//
+
+#define	DNS_STATUS		          int
+#define	DNS_TYPE_SRV		        T_SRV
+#define	DNS_TYPE_MX		          T_MX
+#define	DNS_TYPE_A		          T_A
+#define	DNS_TYPE_NAPTR		      T_NAPTR
+#define	DnsFreeRecordList	      0
+#define	DNS_QUERY_STANDARD	    0
+#define	DNS_QUERY_BYPASS_CACHE	0
+
+typedef struct _DnsAData {
+  DWORD IpAddress;
+} DNS_A_DATA;
+
+typedef struct {
+  char   pNameExchange[MAXDNAME];
+  WORD   wPreference;
+} DNS_MX_DATA;
+
+typedef struct {
+  char pNameHost[MAXDNAME];
+} DNS_PTR_DATA;
+
+typedef struct _DnsSRVData {
   public:
-    class Record : public PObject
-    {
-        PCLASSINFO(Record, PObject);
-      public:
-        Record();
+    char   pNameTarget[MAXDNAME];
+    WORD   wPriority;
+    WORD   wWeight;
+    WORD   wPort;
+} DNS_SRV_DATA;
 
-        PString            hostName;
-        PIPSocket::Address hostAddress;
-        BOOL               used;
-    };
+typedef struct _DnsRecordFlags
+{
+    unsigned   Section     : 2;
+    unsigned   Delete      : 1;
+    unsigned   CharSet     : 2;
+    unsigned   Unused      : 3;
+    unsigned   Reserved    : 24;
+} DNS_RECORD_FLAGS;
 
-    class SRVRecord : public Record
-    {
-        PCLASSINFO(SRVRecord, Record);
-      public:
-        Comparison Compare(const PObject & obj) const;
-        void PrintOn(ostream & strm) const;
+typedef enum _DnsSection
+{
+  DnsSectionQuestion,
+  DnsSectionAnswer,
+  DnsSectionAuthority,
+  DnsSectionAddtional,
+} DNS_SECTION;
 
-        WORD port;
-        WORD priority;
-        WORD weight;
-    };
 
-    class MXRecord : public Record
-    {
-        PCLASSINFO(MXRecord, Record);
-      public:
-        Comparison Compare(const PObject & obj) const;
-        void PrintOn(ostream & strm) const;
+class DnsRecord {
+  public:
+    DnsRecord * pNext;
+    char        pName[MAXDNAME];
+    WORD        wType;
+    WORD        wDataLength;
 
-        WORD preference;
-    };
+    union {
+      DWORD               DW;     // flags as DWORD
+      DNS_RECORD_FLAGS    S;      // flags as structure
+    } Flags;
 
-    PDECLARE_SORTED_LIST(SRVRecordList, SRVRecord)
-      public:
-        void PrintOn(ostream & strm) const;
+    union {
+      DNS_A_DATA   A;
+      DNS_MX_DATA  MX;
+      DNS_PTR_DATA NS;
+      DNS_SRV_DATA SRV;
+    } Data;
+};
 
-        SRVRecord * GetFirst();
-        SRVRecord * GetNext();
+typedef DnsRecord * PDNS_RECORD;
 
-      protected:
-        PINDEX     priPos;
-        PWORDArray priList;
-    };
+extern void DnsRecordListFree(PDNS_RECORD rec, int FreeType);
 
-    PDECLARE_SORTED_LIST(MXRecordList, MXRecord)
-      public:
-        void PrintOn(ostream & strm) const;
+extern DNS_STATUS DnsQuery_A(const char * service,
+		      WORD requestType,
+		      DWORD options,
+		      void *,
+		      PDNS_RECORD * results,
+		      void *);
 
-        MXRecord * GetFirst();
-        MXRecord * GetNext();
 
-      protected:
-        PINDEX lastIndex;
-    };
+#endif // P_HAS_RESOLVER
 
-    static BOOL GetSRVRecords(
+namespace PDNS {
+
+//////////////////////////////////////////////////////////////////////////
+//
+//  this template automates the creation of a list of records for
+//  a specific type of DNS lookup
+//
+
+template <unsigned type, class RecordListType, class RecordType>
+BOOL Lookup(const PString & name, RecordListType & recordList)
+{
+  if (name.IsEmpty())
+    return FALSE;
+
+  recordList.RemoveAll();
+
+  PDNS_RECORD results = NULL;
+  DNS_STATUS status = DnsQuery_A((const char *)name, 
+                                 type,
+                                 DNS_QUERY_STANDARD, 
+                                 NULL, 
+                                 &results, 
+                                 NULL);
+  if (status != 0)
+    return FALSE;
+
+  // find records matching the correct type
+  PDNS_RECORD dnsRecord = results;
+  while (dnsRecord != NULL) {
+    RecordType * record = recordList.HandleDNSRecord(dnsRecord, results);
+    if (record != NULL)
+      recordList.Append(record);
+    dnsRecord = dnsRecord->pNext;
+  }
+
+  if (results != NULL)
+    DnsRecordListFree(results, DnsFreeRecordList);
+
+  return recordList.GetSize() != 0;
+}
+
+/////////////////////////////////////////////////////////////
+
+class SRVRecord : public PObject
+{
+  PCLASSINFO(SRVRecord, PObject);
+  public:
+    SRVRecord()
+    { used = FALSE; }
+
+    Comparison Compare(const PObject & obj) const;
+    void PrintOn(ostream & strm) const;
+
+    PString            hostName;
+    PIPSocket::Address hostAddress;
+    BOOL               used;
+    WORD port;
+    WORD priority;
+    WORD weight;
+};
+
+PDECLARE_SORTED_LIST(SRVRecordList, PDNS::SRVRecord)
+  public:
+    void PrintOn(ostream & strm) const;
+
+    SRVRecord * GetFirst();
+    SRVRecord * GetNext();
+
+    PDNS::SRVRecord * HandleDNSRecord(PDNS_RECORD dnsRecord, PDNS_RECORD results);
+
+  protected:
+    PINDEX     priPos;
+    PWORDArray priList;
+};
+
+/**
+  * return a list of DNS SRV record with the specified service type
+  */
+
+inline BOOL GetRecords(const PString & service, SRVRecordList & serviceList)
+{ return Lookup<DNS_TYPE_SRV, SRVRecordList, SRVRecord>(service, serviceList); }
+
+/**
+  * provided for backwards compatibility
+  */
+inline BOOL GetSRVRecords(
+      const PString & service,
+      SRVRecordList & serviceList
+)
+{ return GetRecords(service, serviceList); }
+
+/**
+  * return a list of DNS SRV record with the specified service, type and domain
+  */
+
+BOOL GetSRVRecords(
       const PString & service,
       const PString & type,
       const PString & domain,
       SRVRecordList & serviceList
-    );
+);
 
-    static BOOL GetSRVRecords(
-      const PString & service,
-      SRVRecordList & serviceList
-    );
+////////////////////////////////////////////////////////////////
 
-    static BOOL GetMXRecords(
-      const PString & domain,
-      MXRecordList & serviceList
-    );
+class MXRecord : public PObject
+{
+  PCLASSINFO(MXRecord, PObject);
+  public:
+    MXRecord()
+    { used = FALSE; }
+    Comparison Compare(const PObject & obj) const;
+    void PrintOn(ostream & strm) const;
+
+    PString            hostName;
+    PIPSocket::Address hostAddress;
+    BOOL               used;
+    WORD               preference;
 };
 
+PDECLARE_SORTED_LIST(MXRecordList, PDNS::MXRecord)
+  public:
+    void PrintOn(ostream & strm) const;
+
+    MXRecord * GetFirst();
+    MXRecord * GetNext();
+
+    PDNS::MXRecord * HandleDNSRecord(PDNS_RECORD dnsRecord, PDNS_RECORD results);
+
+  protected:
+    PINDEX lastIndex;
+};
+
+/**
+  * return a list of MX records for the specified domain
+  */
+inline BOOL GetRecords(
+      const PString & domain,
+      MXRecordList & serviceList
+)
+{ return Lookup<DNS_TYPE_MX, MXRecordList, MXRecord>(domain, serviceList); }
+
+/**
+  * provided for backwards compatibility
+  */
+inline BOOL GetMXRecords(
+      const PString & domain,
+      MXRecordList & serviceList
+)
+{
+  return GetRecords(domain, serviceList);
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+}; // namespace PDNS
 
 #endif // _PDNS_H
 #endif // P_DNS
