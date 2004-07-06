5d56092bf06c487664fdb37ea760605d8b5fd318
csoutheren <csoutheren@023b2edf-31b2-4de3-b41e-bca80c47788f>
2004-07-06 10:14:19 +0000

    Updated for new factory code
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@9766 023b2edf-31b2-4de3-b41e-bca80c47788f

--- samples/factory/main.cxx
+++ samples/factory/main.cxx
@@ -1,7 +1,9 @@
+
 #include <ptlib.h>
 
 #include <ptclib/http.h>
 #include <ptclib/ptts.h>
+#include <ptclib/pwavfile.h>
 
 #include "abstract.h"
 
@@ -17,14 +19,14 @@ class Factory : public PProcess
 
 PCREATE_PROCESS(Factory)
 
-template <class FactoryClass>
-void TestFactory(const char * title)
+template <class FactoryClass, typename TypeClass>
+void DisplayFactory(const char * title)
 {
   cout << "Testing " << title << endl;
-  PFactory<FactoryClass>::KeyList_T keys = PFactory<FactoryClass>::GetKeyList();
+  PFactory<FactoryClass, TypeClass>::KeyList_T keys = PFactory<FactoryClass, TypeClass>::GetKeyList();
 
   cout << "keys = " << endl;
-  PFactory<FactoryClass>::KeyList_T::const_iterator r;
+  PFactory<FactoryClass, TypeClass>::KeyList_T::const_iterator r;
   for (r = keys.begin(); r != keys.end(); ++r) {
     cout << "  " << *r << endl;
   }
@@ -32,10 +34,11 @@ void TestFactory(const char * title)
   cout << endl;
 }
 
-void Factory::Main()
+template <class BaseClass, class TypeClass>
+void DisplayTestFactory()
 {
-  PFactory<MyAbstractClass>::KeyList_T keyList = PFactory<MyAbstractClass>::GetKeyList();
-  PFactory<MyAbstractClass>::KeyList_T::const_iterator r;
+  PFactory<BaseClass, TypeClass>::KeyList_T keyList = PFactory<MyAbstractClass, TypeClass>::GetKeyList();
+  PFactory<BaseClass, TypeClass>::KeyList_T::const_iterator r;
 
   cout << "List of concrete types:" << endl;
   for (r = keyList.begin(); r != keyList.end(); ++r)
@@ -46,15 +49,22 @@ void Factory::Main()
   for (i = 0; i < keyList.size(); i++) {
     for (int j = 0; j < 3; j++)
     {
-      MyAbstractClass * c = PFactory<MyAbstractClass>::CreateInstance(keyList[i]);
+      MyAbstractClass * c = PFactory<MyAbstractClass, TypeClass>::CreateInstance(keyList[i]);
       if (c == NULL) 
         cout << "Cannot instantiate class " << keyList[i] << endl;
       else
         cout << keyList[i] << "::Function returned " << c->Function() << ", instance " << (void *)c << endl;
     }
   }
+}
 
-  TestFactory<PURLScheme>("PURLScheme");
-  TestFactory<PTextToSpeech>("PTextToSpeech");
-  TestFactory<PPluginModuleManager>("PPluginModuleManager");
+void Factory::Main()
+{
+  DisplayTestFactory<MyAbstractClass, PString>();
+  DisplayTestFactory<MyAbstractClass, unsigned>();
+
+  DisplayFactory<PURLScheme, PString>("PURLScheme");
+  DisplayFactory<PTextToSpeech, PString>("PTextToSpeech");
+  DisplayFactory<PPluginModuleManager, PString>("PPluginModuleManager");
+  DisplayFactory<PWAVFileConverter, unsigned>("PWAVFileConverter");
 }
