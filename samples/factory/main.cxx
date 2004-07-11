eb37cae73103c0ef604d024c39ac85b5059eca8a
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2004-07-11 08:33:15 +0000

    Fixed bizarre MSVC 6 bug by making template functons in a class.

--- samples/factory/main.cxx
+++ samples/factory/main.cxx
@@ -7,7 +7,6 @@
 
 #include "abstract.h"
 
-extern unsigned PTraceCurrentLevel;
 
 class Factory : public PProcess
 {
@@ -19,51 +18,49 @@ class Factory : public PProcess
 
 PCREATE_PROCESS(Factory)
 
-template <class FactoryClass, typename TypeClass>
-void DisplayFactory(const char * title)
-{
-  cout << "Testing " << title << endl;
-  PFactory<FactoryClass, TypeClass>::KeyList_T keys = PFactory<FactoryClass, TypeClass>::GetKeyList();
 
-  cout << "keys = " << endl;
-  PFactory<FactoryClass, TypeClass>::KeyList_T::const_iterator r;
-  for (r = keys.begin(); r != keys.end(); ++r) {
-    cout << "  " << *r << endl;
-  }
+template <class BaseClass, typename TypeClass = PString>
+class Display
+{
+public:
+  static void ConcreteTypes(const char * title)
+  {
+    cout << "\nConcrete types for " << title << ':' << endl;
+    PFactory<BaseClass, TypeClass>::KeyList_T keys = PFactory<BaseClass, TypeClass>::GetKeyList();
+    PFactory<BaseClass, TypeClass>::KeyList_T::const_iterator r;
+    for (r = keys.begin(); r != keys.end(); ++r)
+      cout << "  " << *r << endl;
   
-  cout << endl;
-}
+    cout << endl;
+  }
 
-template <class BaseClass, class TypeClass>
-void DisplayTestFactory()
-{
-  PFactory<BaseClass, TypeClass>::KeyList_T keyList = PFactory<MyAbstractClass, TypeClass>::GetKeyList();
-  PFactory<BaseClass, TypeClass>::KeyList_T::const_iterator r;
-
-  cout << "List of concrete types:" << endl;
-  for (r = keyList.begin(); r != keyList.end(); ++r)
-    cout << "  " << *r << endl;
-  cout << endl;
-
-  unsigned i;
-  for (i = 0; i < keyList.size(); i++) {
-    for (int j = 0; j < 3; j++)
-    {
-      MyAbstractClass * c = PFactory<MyAbstractClass, TypeClass>::CreateInstance(keyList[i]);
-      if (c == NULL) 
-        cout << "Cannot instantiate class " << keyList[i] << endl;
-      else
-        cout << keyList[i] << "::Function returned " << c->Function() << ", instance " << (void *)c << endl;
+  static void TestFactory()
+  {
+    PFactory<BaseClass, TypeClass>::KeyList_T keyList = PFactory<MyAbstractClass, TypeClass>::GetKeyList();
+    unsigned i;
+    for (i = 0; i < keyList.size(); i++) {
+      for (int j = 0; j < 3; j++)
+      {
+        MyAbstractClass * c = PFactory<MyAbstractClass, TypeClass>::CreateInstance(keyList[i]);
+        if (c == NULL) 
+          cout << "Cannot instantiate class " << keyList[i] << endl;
+        else
+          cout << keyList[i] << "::Function returned \"" << c->Function() << "\", instance " << (void *)c << endl;
+      }
     }
   }
-}
+};
+
 
 void Factory::Main()
 {
-  DisplayTestFactory<MyAbstractClass, PString>();
-  DisplayTestFactory<MyAbstractClass, unsigned>();
+  Display<MyAbstractClass>::ConcreteTypes("MyAbstractClass by PString");
+  Display<MyAbstractClass>::TestFactory();
 
-  DisplayFactory<PURLScheme, PString>("PURLScheme");
-  DisplayFactory<PTextToSpeech, PString>("PTextToSpeech");
-  DisplayFactory<PPluginModuleManager, PString>("PPluginModuleManager");
+  Display<MyAbstractClass, unsigned>::ConcreteTypes("MyAbstractClass by unsigned");
+  Display<MyAbstractClass, unsigned>::TestFactory();
+
+  Display<PURLScheme>::ConcreteTypes("PURLScheme");
+  Display<PTextToSpeech>::ConcreteTypes("PTextToSpeech");
+  Display<PPluginModuleManager>::ConcreteTypes("PPluginModuleManager");
 }
