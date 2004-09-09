c7bc34d8e47d2f2eef7842961a20c017aba6b8c8
csoutheren <csoutheren@023b2edf-31b2-4de3-b41e-bca80c47788f>
2004-09-09 04:00:00 +0000

    Added DTMF encoding functions

--- include/ptclib/dtmf.h
+++ include/ptclib/dtmf.h
@@ -12,6 +12,9 @@
  * Made into a C++ class by Roger Hardiman <roger@freebsd.org>, January 2002
  *
  * $Log: dtmf.h,v $
+ * Revision 1.3  2004/09/09 04:00:00  csoutheren
+ * Added DTMF encoding functions
+ *
  * Revision 1.2  2002/09/16 01:08:59  robertj
  * Added #define so can select if #pragma interface/implementation is used on
  *   platform basis (eg MacOS) rather than compiler, thanks Robert Monaghan.
@@ -49,4 +52,76 @@ class PDTMFDecoder : public PObject
     int h[8], k[8], y[8];
     int nn, so, ia;
 };
+
+/**
+  * this class can be used to generate PCM data for tones (such as DTMF) 
+  * at a sample rate of 8khz
+  */
+
+class PDTMFEncoder : public PBYTEArray
+{
+  PCLASSINFO(PDTMFEncoder, PBYTEArray)
+
+  public:
+    enum { DefaultToneLen = 100 };
+
+    /**
+      * Create PCM data for the specified DTMF sequence 
+      */
+    inline PDTMFEncoder(const char * dtmf = NULL, unsigned len = DefaultToneLen)
+    { if (dtmf != NULL) AddTone(dtmf, len); }
+
+
+    /**
+      * Add the PCM data for the specified tone to the buffer
+      */
+    void AddTone(char ch, unsigned len = DefaultToneLen);
+
+    /**
+      * Add the PCM data for the specified tone sequence to the buffer
+      */
+    void AddTone(const PString & str, unsigned len = DefaultToneLen);
+
+    /**
+      * Add the PCM data for the specified dual-frequency tone to the buffer
+      * freq2 can be zero, which will generate a single frequency tone
+      */
+    void AddTone(double freq1, double freq2 = 0, unsigned len = DefaultToneLen);
+
+    /**
+      * Generate PCM data for a single cadence of the US standard ring tone
+      * of 440/480hz for 2 seconds, followed by 5 seconds of silence
+      */
+    void GenerateRingBackTone()
+    {
+      AddTone(440, 480, 2000);
+      AddTone(0,   0,   4000);
+    }
+
+    /**
+      * Generate PCM data for 1 second of US standard dial tone 
+      * of 350/440hz 
+      */
+    void GenerateDialTone()
+    {
+      AddTone(350, 440, 1000);
+    }
+
+    /**
+      * Generate PCM data for a single cadence of the US standard busy tone
+      * of 480/620hz for 1/2 second, 1/2 second of silence
+      */
+    void GenerateBusyTone()
+    {
+      AddTone(480, 620, 500);
+      AddTone(0,   0,   500);
+    }
+
+  protected:
+    static PMutex & GetMutex();
+    static BOOL sineTabInit;
+    static void MakeSineTable();
+    static double sinetab[1 << 11];
+};
+
 #endif /* _DTMF_H */
