647c800e88704879f1b80b5d7fed13ae30e37e59
dereksmithies <dereksmithies@023b2edf-31b2-4de3-b41e-bca80c47788f>
2005-03-10 09:27:42 +0000

    Improve text output.

--- samples/address/local.cxx
+++ samples/address/local.cxx
@@ -8,6 +8,9 @@
  *                          the address of one network interface
  *
  * $Log: local.cxx,v $
+ * Revision 1.2  2005/03/10 09:27:42  dereksmithies
+ * Improve text output.
+ *
  * Revision 1.1  2005/03/10 09:19:01  dereksmithies
  * Initial release of code to illustrate the reading of the machines external ip address and hostname
  *
@@ -19,25 +22,43 @@
 
 class LocalAddress : public PProcess
 {
-  PCLASSINFO(LocalAddress, PProcess)
-  public:
+    PCLASSINFO(LocalAddress, PProcess);
+public:
+    LocalAddress();
+    
     void Main();
 };
 
-PCREATE_PROCESS(LocalAddress)
+PCREATE_PROCESS(LocalAddress);
+
+LocalAddress::LocalAddress()
+    : PProcess("PwLib Example Factory", "local", 1, 0, ReleaseCode, 0)
+{
+}
+	
 
 void LocalAddress::Main()
 {
+    PStringStream progName;
+    progName << "Product Name: " << GetName() << endl
+	     << "Manufacturer: " << GetManufacturer() << endl
+	     << "Version     : " << GetVersion(TRUE) << endl
+	     << "System      : " << GetOSName() << '-'
+	     << GetOSHardware() << ' '
+	     << GetOSVersion();
+    cout << endl <<  progName << endl << endl;
+    
+
     PUDPSocket localSocket;
     PIPSocket::Address addr;
     if(localSocket.GetNetworkInterface(addr)) {
-        cout << "local address is " <<addr.AsString() << endl;
+        cout << "local address is    " << addr.AsString() << endl;
         if (addr == 0)
             cout << "sorry, that is a 0.0.0.0 address" << endl;
     } else
 	cout << "Sorry, failed to get local address" << endl;
     
-    cout << localSocket.GetHostName() << endl;
+    cout << "local host name is  " << localSocket.GetHostName() << endl;
 }
 
 // End of local.cxx
