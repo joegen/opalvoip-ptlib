2dbfce31617e8cf4da59db2167035cf46b92e984
Craig Southeren <craigs@postincrement.com>
2005-06-09 00:31:58 +0000

    svnimporter: adding tags and branches to revision 10593

---
+++ samples/wavfile/precompile.cxx
@@ -0,0 +1,25 @@
+/*
+ * precompile.cxx
+ *
+ * PWLib application source file for dnssrv
+ *
+ * Precompiled header generation file.
+ *
+ * Copyright 2003 Equivalence
+ *
+ * $Log: precompile.cxx,v $
+ * Revision 1.1  2005/06/09 00:31:58  csoutheren
+ * Finally got around to adding sample program to demomstrate PWAVFile
+ *
+ * Revision 1.1  2004/07/03 01:09:07  rjongbloed
+ * Added MSVC6 build for factory
+ *
+ * Revision 1.1  2003/04/15 04:12:38  craigs
+ * Initial version
+ *
+ */
+
+#include <ptlib.h>
+
+
+// End of File ///////////////////////////////////////////////////////////////
