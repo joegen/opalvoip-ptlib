acea8fa0a711f7ab696a486dfe2cb4af854f572b
dereksmithies <dereksmithies@023b2edf-31b2-4de3-b41e-bca80c47788f>
2005-08-18 00:24:50 +0000

    Tidyup so compiles on linux, tidy up copyright headers, add cvs logging.

--- samples/wavfile/main.cxx
+++ samples/wavfile/main.cxx
@@ -1,3 +1,38 @@
+/*
+ * main.cxx - do wave file things.
+ *
+ *
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: main.cxx,v $
+ * Revision 1.2  2005/08/18 00:24:50  dereksmithies
+ * Tidyup so compiles on linux, tidy up copyright headers, add cvs logging.
+ *
+ *
+ *
+ */
+
 #include <ptlib.h>
 
 #include <ptclib/pwavfile.h>
@@ -18,7 +53,7 @@ PCREATE_PROCESS(WAVFileTest)
 void WAVFileTest::Main()
 {
   PArgList & args = GetArguments();
-  args.Parse("p:c:");
+  args.Parse("p:  c: h. ");
 
   if (args.HasOption('h')) {
     cout << "usage: wavfile [-p device][-c fmt] fn" << endl;
