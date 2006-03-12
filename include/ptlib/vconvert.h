8006e78929b150d9022f3c8539252c3fb4e60c0f
dsandras <dsandras@023b2edf-31b2-4de3-b41e-bca80c47788f>
2006-03-12 11:09:48 +0000

    Applied patch from Luc Saillard to fix problems with MJPEG. Thanks!

--- include/ptlib/vconvert.h
+++ include/ptlib/vconvert.h
@@ -26,6 +26,9 @@
  *                 Mark Cooke (mpc@star.sr.bham.ac.uk)
  *
  * $Log: vconvert.h,v $
+ * Revision 1.20  2006/03/12 11:09:48  dsandras
+ * Applied patch from Luc Saillard to fix problems with MJPEG. Thanks!
+ *
  * Revision 1.19  2006/02/22 11:17:53  csoutheren
  * Applied patch #1425825
  * MaxOSX compatibility
@@ -239,6 +242,13 @@ class PColourConverter : public PObject
       PINDEX * bytesReturned = NULL ///< Bytes written to dstFrameBuffer
     ) = 0;
 
+    virtual BOOL Convert(
+      const BYTE * srcFrameBuffer,  ///< Frame store for source pixels
+      BYTE * dstFrameBuffer,        ///< Frame store for destination pixels
+      unsigned int srcFrameBytes,
+      PINDEX * bytesReturned = NULL ///< Bytes written to dstFrameBuffer
+    ) = 0;
+
     /**Convert from one colour format to another.
        This version will copy the data from one frame buffer to the same frame
        buffer. Not all conversions can do this so an intermediate store and
@@ -324,6 +334,7 @@ class cls : public ancestor { \
   cls(const PString & srcFmt, const PString & dstFmt, unsigned w, unsigned h) \
     : ancestor(srcFmt, dstFmt, w, h) { } \
   virtual BOOL Convert(const BYTE *, BYTE *, PINDEX * = NULL); \
+  virtual BOOL Convert(const BYTE *, BYTE *, unsigned int , PINDEX * = NULL); \
 }; \
 static class cls##_Registration : public PColourConverterRegistration { \
   public: \
@@ -333,6 +344,8 @@ static class cls##_Registration : public PColourConverterRegistration { \
 } p_##cls##_registration_instance; \
 PColourConverter * cls##_Registration::Create(unsigned w, unsigned h) const \
   { PINDEX tab = Find('\t'); return new cls(Left(tab), Mid(tab+1), w, h); } \
+BOOL cls::Convert(const BYTE *srcFrameBuffer, BYTE *dstFrameBuffer, unsigned int __srcFrameBytes, PINDEX * bytesReturned) \
+  { srcFrameBytes = __srcFrameBytes;return Convert(srcFrameBuffer, dstFrameBuffer, bytesReturned); } \
 BOOL cls::Convert(const BYTE *srcFrameBuffer, BYTE *dstFrameBuffer, PINDEX * bytesReturned)
 
 
@@ -358,6 +371,7 @@ class PSynonymColour : public PColourConverter {
       unsigned w, unsigned h
     ) : PColourConverter(srcFmt, dstFmt, w, h) { }
     virtual BOOL Convert(const BYTE *, BYTE *, PINDEX * = NULL);
+    virtual BOOL Convert(const BYTE *, BYTE *, unsigned int , PINDEX * = NULL);
 };
 
 
