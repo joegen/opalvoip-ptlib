60418bc2f8447e4fbaa163d67860b3dce6d9c8f7
Craig Southeren <craigs@postincrement.com>
2006-03-12 22:07:45 +0000

    Incremented major version for release

--- version.h
+++ version.h
@@ -33,9 +33,9 @@
  */
 
 #define MAJOR_VERSION 1
-#define MINOR_VERSION 9
+#define MINOR_VERSION 10
 #define BUILD_TYPE    ReleaseCode
-#define BUILD_NUMBER 5
+#define BUILD_NUMBER 0
 
 
 #endif  // _PWLib_VERSION_H
