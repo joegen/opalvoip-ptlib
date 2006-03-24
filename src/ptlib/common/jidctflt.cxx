6f370baee24338b273e04ad865c62fa86635bccf
dereksmithies <dereksmithies@023b2edf-31b2-4de3-b41e-bca80c47788f>
2006-03-24 23:29:23 +0000

    Code uses inline assembly, which does not compile on an AMD64 bit box. Disable the inline assembly for all 64 bit boxes. In my view, the assembly should be removed for all cases.

--- src/ptlib/common/jidctflt.cxx
+++ src/ptlib/common/jidctflt.cxx
@@ -80,7 +80,8 @@
 
 #define DEQUANTIZE(coef,quantval)  (((FAST_FLOAT) (coef)) * (quantval))
 
-#if defined(__GNUC__) && (defined(__i686__) || defined(__x86_64__))
+
+#if defined(__GNUC__) && (defined(__i686__) || defined(__x86_64__)) && (!defined(P_64BIT))
 
 static inline unsigned char descale_and_clamp(int x, int shift)
 {
