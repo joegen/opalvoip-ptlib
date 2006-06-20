3d026f3aea93beb4057291b6da6d52f4bc7e3317
Craig Southeren <craigs@postincrement.com>
2006-06-20 06:08:06 +0000

    Applied patch 1471691 Fix vidinput_bsd plugin Thanks to Joerg Pulz

--- plugins/vidinput_bsd/vidinput_bsd.h
+++ plugins/vidinput_bsd/vidinput_bsd.h
@@ -1,13 +1,24 @@
+#ifndef _PVIDEOIOBSDCAPTURE
 
-//#include <sys/mman.h>
-//#include <sys/time.h>
+#define _PVIDEOIOBSDCAPTURE
+
+#ifdef __GNUC__   
+#pragma interface
+#endif
+
+#include <sys/mman.h>
 
 #include <ptlib.h>
 #include <ptlib/videoio.h>
 #include <ptlib/vconvert.h>
 
 #if defined(P_FREEBSD)
+#include <sys/param.h>
+# if __FreeBSD_version >= 502100
+#include <dev/bktr/ioctl_meteor.h>
+# else
 #include <machine/ioctl_meteor.h>
+# endif
 #endif
 
 #if defined(P_OPENBSD) || defined(P_NETBSD)
@@ -20,19 +31,24 @@
 #endif
 #endif
 
-class PVideoInputDevice_BSDCAPTURE: public PVideoInputDevice
+#if !P_USE_INLINES
+#include <ptlib/contain.inl>
+#endif
+
+
+class PVideoInputDevice_BSDCAPTURE : public PVideoInputDevice
 {
+
+  PCLASSINFO(PVideoInputDevice_BSDCAPTURE, PVideoInputDevice);
 
 public:
   PVideoInputDevice_BSDCAPTURE();
   ~PVideoInputDevice_BSDCAPTURE();
 
-  static PStringList GetInputDeviceNames();
-
-  PStringList GetDeviceNames() const
-  { return GetInputDeviceNames(); }
-
-  BOOL Open(const PString &deviceName, BOOL startImmediate);
+  BOOL Open(
+    const PString &deviceName,
+    BOOL startImmediate = TRUE
+  );
 
   BOOL IsOpen();
 
@@ -43,11 +59,24 @@ public:
 
   BOOL IsCapturing();
 
+  static PStringList GetInputDeviceNames();
+
+  PStringList GetDeviceNames() const
+  { return GetInputDeviceNames(); }
+
   PINDEX GetMaxFrameBytes();
 
-  BOOL GetFrame(PBYTEArray & frame);
-  BOOL GetFrameData(BYTE*, PINDEX*);
-  BOOL GetFrameDataNoDelay(BYTE*, PINDEX*);
+//  BOOL GetFrame(
+//    PBYTEArray & frame
+//  );
+  BOOL GetFrameData(
+    BYTE * buffer,
+    PINDEX * bytesReturned = NULL
+  );
+  BOOL GetFrameDataNoDelay(
+    BYTE * buffer,
+    PINDEX * bytesReturned = NULL
+  );
 
   BOOL GetFrameSizeLimits(unsigned int&, unsigned int&,
 			  unsigned int&, unsigned int&);
@@ -99,3 +128,5 @@ public:
   int    mmap_size;
  
 };
+
+#endif
