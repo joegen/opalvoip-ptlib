3442e3da81ac00543881acca1f9df29bc602a7e6
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2007-06-26 10:56:14 +0000

    Update symbols
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@12261 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ include/ptlib/msos/CopyDTF_2003.bat
@@ -0,0 +1,4 @@
+ren ptlibd_2003.dtf ptlibd_2003_old.dtf
+copy ..\..\..\lib\debug\ptlibd.def ptlibd_2003.dtf
+ren ptlib_2003.dtf ptlib_2003_old.dtf
+copy ..\..\..\lib\release\ptlib.def ptlib_2003.dtf
