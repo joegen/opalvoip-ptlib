71bc84238091110360b48efd2e7cf7cddda2feb9
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2007-09-17 11:14:39 +0000

    Added "No Trace" build configuration.

--- include/ptlib/msos/CopyDTF_2005.bat
+++ include/ptlib/msos/CopyDTF_2005.bat
@@ -3,3 +3,5 @@ move ptlibd_2005.dtf backup\
 copy ..\..\..\lib\debug\ptlibd.def ptlibd_2005.dtf
 move ptlib_2005.dtf backup\
 copy ..\..\..\lib\release\ptlib.def ptlib_2005.dtf
+move ptlibn_2005.dtf backup\
+copy "..\..\..\lib\No Trace\ptlibn.def" ptlibn_2005.dtf
