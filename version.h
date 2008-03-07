272938cd6de4df28df5814dd1472b92e16ab60ff
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2008-03-07 06:24:34 +0000

    Update version number for beta v2.0.3

--- version.h
+++ version.h
@@ -34,8 +34,8 @@
 
 #define MAJOR_VERSION 2
 #define MINOR_VERSION 0
-#define BUILD_TYPE ReleaseCode
-#define BUILD_NUMBER 2
+#define BUILD_TYPE BetaCode
+#define BUILD_NUMBER 3
 
 
 #endif  // _PWLib_VERSION_H
