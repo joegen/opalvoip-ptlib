03fc99b5a47731f0992acd968b4d0a4fa1f6bc0b
Robert Jongbloed <robertj@voxlucida.com.au>
2008-08-18 01:05:24 +0000

    Update release version number to 2.1.3

--- version.h
+++ version.h
@@ -34,9 +34,9 @@
  */
 
 #define MAJOR_VERSION 2
-#define MINOR_VERSION 3
-#define BUILD_TYPE BetaCode
-#define BUILD_NUMBER 0
+#define MINOR_VERSION 1
+#define BUILD_TYPE ReleaseCode
+#define BUILD_NUMBER 3
 
 
 #endif  // _PTLib_VERSION_H
