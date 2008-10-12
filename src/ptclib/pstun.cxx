53c98cc8704f0a4ff4e54baf32c39060f8f37863
dsandras <dsandras@023b2edf-31b2-4de3-b41e-bca80c47788f>
2008-10-12 16:56:13 +0000

    STUN was not working with Open NAT.

--- src/ptclib/pstun.cxx
+++ src/ptclib/pstun.cxx
@@ -712,6 +712,8 @@ PBoolean PSTUNClient::CreateSocket(PUDPSocket * & socket, const PIPSocket::Addre
   socket = NULL;
 
   switch (GetNatType(PFalse)) {
+
+    case OpenNat :
     case ConeNat :
     case RestrictedNat :
     case PortRestrictedNat :
