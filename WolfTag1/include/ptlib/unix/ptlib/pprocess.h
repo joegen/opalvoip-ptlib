19f97f891a63f647034e3411f6be6d57d38d7885
csoutheren <csoutheren@023b2edf-31b2-4de3-b41e-bca80c47788f>
2008-10-30 06:43:38 +0000

    Snapshot

---
+++ WolfTag1/include/ptlib/unix/ptlib/pprocess.h
@@ -0,0 +1,121 @@
+/*
+ * pprocess.h
+ *
+ * Operating System process (running program) class.
+ *
+ * Portable Windows Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Windows Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Portions are Copyright (C) 1993 Free Software Foundation, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Revision$
+ * $Author$
+ * $Date$
+ */
+
+PDICTIONARY(PXFdDict, POrdinalKey, PThread);
+
+///////////////////////////////////////////////////////////////////////////////
+// PProcess
+
+  public:
+    friend class PApplication;
+    friend class PServiceProcess;
+    friend void PXSignalHandler(int);
+    friend class PHouseKeepingThread;
+    friend PString PX_GetThreadName(pthread_t id);
+
+    ~PProcess();
+
+    PDirectory PXGetHomeDir ();
+    char ** PXGetArgv() const { return p_argv; }
+    int     PXGetArgc() const { return p_argc; }
+    char ** PXGetEnvp() const { return p_envp; }
+
+    friend void PXSigHandler(int);
+    virtual void PXOnSignal(int);
+    virtual void PXOnAsyncSignal(int);
+    void         PXCheckSignals();
+
+    static void PXShowSystemWarning(PINDEX code);
+    static void PXShowSystemWarning(PINDEX code, const PString & str);
+
+  protected:
+    void         CommonConstruct();
+    void         CommonDestruct();
+
+    virtual void _PXShowSystemWarning(PINDEX code, const PString & str);
+    int pxSignals;
+
+  protected:
+    void CreateConfigFilesDictionary();
+    PAbstractDictionary * configFiles;
+
+
+#if defined(P_PTHREADS) || defined(P_MAC_MPTHREADS) || defined (__BEOS__)
+
+  public:
+    void SignalTimerChange();
+    PBoolean PThreadKill(pthread_t id, unsigned signal);
+
+  protected:
+    PDICTIONARY(ThreadDict, POrdinalKey, PThread);
+    ThreadDict activeThreads;
+    PMutex     threadMutex;
+    PSyncPoint breakBlock;
+    class PHouseKeepingThread * housekeepingThread;
+
+#elif defined(VX_TASKS)
+
+  public:
+    void SignalTimerChange();
+
+  private:
+    PDICTIONARY(ThreadDict, POrdinalKey, PThread);
+    ThreadDict activeThreads;
+    PMutex activeThreadMutex;
+    PLIST(ThreadList, PThread);
+    ThreadList autoDeleteThreads;
+    PMutex deleteThreadMutex;
+
+    PDECLARE_CLASS(HouseKeepingThread, PThread)
+        public:
+        HouseKeepingThread();
+        void Main();
+        PSyncPoint breakBlock;
+    };
+    friend class HouseKeepingThread;
+    HouseKeepingThread * houseKeeper;
+    // Thread for doing timers, thread clean up etc.
+
+  friend PThread * PThread::Current();
+  friend int PThread::ThreadFunction(void * thread);
+
+
+#else
+
+  public:
+    void PXAbortIOBlock(int fd);
+  protected:
+    PXFdDict     ioBlocks[3];
+#endif
+
+// End Of File ////////////////////////////////////////////////////////////////
