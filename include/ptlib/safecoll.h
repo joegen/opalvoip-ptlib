a02d8abd834a70bc68f13c4ba0f5d919da88f932
Robert Jongbloed <robertj@voxlucida.com.au>
2009-05-13 04:17:54 +0000

    Back ported test for multiple instances of an object in a safe collection. This is bad, must be prevented.

--- include/ptlib/safecoll.h
+++ include/ptlib/safecoll.h
@@ -925,9 +925,10 @@ template <class Coll, class Base> class PSafeColl : public PSafeCollection
       PSafetyMode mode = PSafeReference
     ) {
         PWaitAndSignal mutex(collectionMutex);
-        if (!obj->SafeReference())
-          return NULL;
-        return PSafePtr<Base>(*this, mode, collection->Append(obj));
+        if (PAssert(collection->GetObjectsIndex(obj) == P_MAX_INDEX, "Cannot insert safe object twice") &&
+            obj->SafeReference())
+          return PSafePtr<Base>(*this, mode, collection->Append(obj));
+        return NULL;
       }
 
     /**Remove an object to the collection.
@@ -1054,7 +1055,8 @@ template <class Coll, class Key, class Base> class PSafeDictionaryBase : public 
       {
         collectionMutex.Wait();
         SafeRemove(((Coll *)collection)->GetAt(key));
-        if (obj->SafeReference())
+        if (PAssert(collection->GetObjectsIndex(obj) == P_MAX_INDEX, "Cannot insert safe object twice") &&
+            obj->SafeReference())
           ((Coll *)collection)->SetAt(key, obj);
         collectionMutex.Signal();
       }
