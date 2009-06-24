e4ff654af86fe40194d503dbfa0cec4f00b09b5f
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-06-24 02:20:32 +0000

    Fixed incorrect divisor for new clock_gettime version of PTimer::Tick()

--- src/ptlib/unix/osutil.cxx
+++ src/ptlib/unix/osutil.cxx
@@ -306,7 +306,7 @@ PTimeInterval PTimer::Tick()
 #ifdef _POSIX_MONOTONIC_CLOCK
   struct timespec ts;
   clock_gettime(CLOCK_MONOTONIC, &ts);
-  return ts.tv_sec*1000LL + ts.tv_nsec/100000LL;
+  return ts.tv_sec*1000LL + ts.tv_nsec/1000000LL;
 #else
   #warning System does not have clock_gettime with CLOCK_MONOTONIC, using gettimeofday
   struct timeval tv;
