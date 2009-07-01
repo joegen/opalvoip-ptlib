020a8157c85b39fca67fe231fc800d0f5c4fccfe
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-07-01 00:13:04 +0000

    Fixed name conflict.

--- src/ptlib/common/sockets.cxx
+++ src/ptlib/common/sockets.cxx
@@ -279,7 +279,7 @@ class PHostByName : PHostByName_private
   friend void PIPSocket::ClearNameCache();
 };
 
-PMutex creationMutex;
+static PMutex creationMutex;
 static PHostByName & pHostByName()
 {
   PWaitAndSignal m(creationMutex);
