6632f984e6e94798120528e6dd3e5683c6620f14
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-08-24 08:44:41 +0000

    Further changes for 64 bit Unix support, thanks Alexander V. Chernikov
    Patch Independently supplied by Thomas Klausner as well.

--- src/ptlib/unix/tlibthrd.cxx
+++ src/ptlib/unix/tlibthrd.cxx
@@ -250,7 +250,7 @@ PBoolean PProcess::PThreadKill(pthread_t id, unsigned sig)
 {
   PWaitAndSignal m(activeThreadMutex);
 
-  if (!activeThreads.Contains((unsigned)id)) 
+  if (!activeThreads.Contains((uintptr_t)id)) 
     return PFalse;
 
   return pthread_kill(id, sig) == 0;
@@ -259,8 +259,8 @@ PBoolean PProcess::PThreadKill(pthread_t id, unsigned sig)
 void PProcess::PXSetThread(pthread_t id, PThread * thread)
 {
   activeThreadMutex.Wait();
-  PThread * currentThread = activeThreads.GetAt((PINDEX)id);
-  activeThreads.SetAt((PINDEX)id, thread);
+  PThread * currentThread = activeThreads.GetAt((uintptr_t)id);
+  activeThreads.SetAt((uintptr_t)id, thread);
   activeThreadMutex.Signal();
 
   if (currentThread != NULL) 
@@ -393,7 +393,7 @@ PThread::~PThread()
     if (id != 0) {
       process.activeThreadMutex.Wait();
       pthread_detach(id);
-      process.activeThreads.SetAt((unsigned)id, NULL);
+      process.activeThreads.SetAt((uintptr_t)id, NULL);
       process.activeThreadMutex.Signal();
     }
 
