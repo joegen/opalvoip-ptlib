fe3696970b5003d868bd5b0ff867227f15771dfc
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-09-21 00:26:30 +0000

    Update version number for beta v2.6.6

--- version.h
+++ version.h
@@ -35,8 +35,8 @@
 
 #define MAJOR_VERSION 2
 #define MINOR_VERSION 6
-#define BUILD_TYPE ReleaseCode
-#define BUILD_NUMBER 5
+#define BUILD_TYPE BetaCode
+#define BUILD_NUMBER 6
 
 
 #endif  // _PTLib_VERSION_H
