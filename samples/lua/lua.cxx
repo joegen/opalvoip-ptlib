d9398afa433f7e14fcfe74f867d20f84aa554ddf
Craig Southeren <craigs@postincrement.com>
2010-04-21 03:48:41 +0000

    Now check in sample code that works!!

--- samples/lua/lua.cxx
+++ samples/lua/lua.cxx
@@ -16,114 +16,11 @@ extern "C" {
 
 #pragma comment(lib, P_LUA_LIBRARY)
 
-#if 0
-
-template <class InstanceType>
-class LuaClassBindings
-{
-  public:
-    typedef int (InstanceType::* MemberFunction)(lua_State *);
-
-    LuaClassBindings(const char * name = NULL)
-      : m_name(NULL)
-    { }
-
-    bool Bind(const char * name, MemberFunction func)
-    {
-      MemberBinding binding;
-      binding.m_name = name;
-      binding.m_func = func;
-      m_memberBindings.push_back(binding);
-      return true;
-    }
-
-    virtual int Invoke(InstanceType * instance, int function, lua_State * L)
-    {
-      return m_memberBindings[function].Invoke(instance, L); 
-    }
-
-    struct MemberBinding {
-      const char *   m_name;
-      MemberFunction m_func;
-
-      virtual int Invoke(InstanceType * instance, lua_State * L)
-      {
-        return (instance->*m_func)(L); 
-      }
-    };
-
-    const char * m_name;
-    std::vector<MemberBinding> m_memberBindings;
-};
-
-template <class InstanceType>
-class LuaClassInstance 
-{
-  public:
-    typedef LuaClassBindings<InstanceType> BindingsType;
-    LuaClassInstance(PLuaContext & lua, BindingsType & bindings, InstanceType * instance, const char * name = NULL)
-      : m_instance(instance)
-      , m_bindings(bindings)
-      , m_name(name)
-    {
-      // using binding name if no instance name specified
-      if (m_name == NULL)
-        m_name = bindings.m_name;
-
-      // populate the table
-      lua_newtable(lua);
-      for (size_t i = 0; i < m_bindings.m_memberBindings.size(); i++) {
-        lua_pushlightuserdata(lua, (void *)this);
-        lua_pushinteger      (lua, i);
-        lua_pushcclosure     (lua, &CallBack, 2);
-        lua_setfield(lua, -2, m_bindings.m_memberBindings[i].m_name);
-      }
-
-      // register the table
-      lua_setglobal(lua, m_name);
-    }
-
-    BindingsType & m_bindings;
-    InstanceType * m_instance;
-    const char * m_name;
-
-    static int CallBack(lua_State * L)
-    {
-      LuaClassInstance * luaClassInstance = (LuaClassInstance *)lua_touserdata(L, lua_upvalueindex(1));
-      return (luaClassInstance == NULL) ? 0 : luaClassInstance->m_bindings.Invoke(luaClassInstance->m_instance, lua_tointeger(L, lua_upvalueindex(2)), L);
-    }
-};
-
-#define PLUA_DECLARE_FUNCTION(fn_name) \
-int fn_name(lua_State *) 
-
-#define PLUA_BIND_FUNCTION(bindings, cls, fn_name) \
-  bindings.Bind(#fn_name, &cls::fn_name)
-
-class XLuaContext : public PLuaContext
-{
-  public:
-    template<class InstanceType>
-    LuaClassInstance<InstanceType> * Instantiate(LuaClassBindings<InstanceType> & bindings, InstanceType * instance, const char * name)
-    { return new LuaClassInstance<InstanceType>(*this, bindings, instance, name);  }
-};
-
-
-
-
-/////////////////////////////////////////////////////////
-#endif
-
 class LuaProcess : public PProcess
 {
   PCLASSINFO(LuaProcess, PProcess)
   public:
     void Main();
-    PLUA_DECLARE_FUNCTION(SetTraceLevel)
-    {
-      cerr << "SetTraceLevel called" << endl;
-      return 0;
-    }
 };
 
 class MyClass {
@@ -133,7 +30,7 @@ class MyClass {
     { }
     const char * m_str;
 
-    PLUA_DECLARE_FUNCTION(print)
+    int print(lua_State *)
     {
       cerr << m_str << endl;
       return 0;
