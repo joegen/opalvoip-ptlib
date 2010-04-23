cb0ce001951539c2d5f3800b3b39d2f86330f368
Craig Southeren <craigs@postincrement.com>
2010-04-23 07:47:12 +0000

    Implementing simpler binding method using metatables Add function to retrieve last error message

--- samples/lua/lua.cxx
+++ samples/lua/lua.cxx
@@ -8,13 +8,6 @@
 #include <ptlib/pprocess.h>
 #include <ptclib/lua.h>
 
-extern "C" {
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
-};
-
-#pragma comment(lib, P_LUA_LIBRARY)
 
 class LuaProcess : public PProcess
 {
@@ -23,6 +16,7 @@ class LuaProcess : public PProcess
     void Main();
 };
 
+
 class MyClass {
   public:
     MyClass(const char * str)
@@ -30,29 +24,32 @@ class MyClass {
     { }
     const char * m_str;
 
-    int print(lua_State *)
+    PLUA_BINDING_START(MyClass)
+      PLUA_BINDING(print)
+    PLUA_BINDING_END()
+
+    PLUA_FUNCTION_NOARGS(print)
     {
-      cerr << m_str << endl;
+      cerr << m_str << endl; 
       return 0;
     }
 };
-
 
 PCREATE_PROCESS(LuaProcess)
 
 void LuaProcess::Main()
 {
-  PLua::ClassBindings<MyClass> myClassBinding("MyClass");
-  PLUA_BIND_FUNCTION(myClassBinding, MyClass, print);
-
-  MyClass class1("class1");
-  MyClass class2("class2");
-
   PLua lua;
-  lua.Instantiate<MyClass>(myClassBinding, &class1, "class1");
-  lua.Instantiate<MyClass>(myClassBinding, &class2, "class2");
 
-  lua.Run("class1.print()\nclass2.print()");
+  // populate the table
+  MyClass class1("class1");
+  class1.BindToInstance(lua, "class1");
+
+  MyClass class2("class2");
+  class2.BindToInstance(lua, "class2");
+
+  if (!lua.Run("class1.print()\nclass2.print()"))
+    cout << lua.GetLastErrorText() << endl;
 }
 
 // End of hello.cxx
