a82e61070ab63f2c6ccaf459fde39d49c3f38b55
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2011-06-17 03:40:12 +0000

    Updated string test application and added VS2010 build file.

--- samples/strtest/main.cxx
+++ samples/strtest/main.cxx
@@ -1,3 +1,11 @@
+//
+// main.cxx
+//
+// String Tests
+//
+// Copyright 2011 Vox Lucida Pty. Ltd.
+//
+
 
 #include <ptlib.h>
 #include <ptlib/pprocess.h>
@@ -5,7 +13,81 @@
 
 ////////////////////////////////////////////////
 //
-// test #1 - string concurrency test
+// test #1 - PString test
+//
+
+void Test1()
+{
+  {
+    PString pstring1("hello world");
+    PString pstring2(pstring1);
+
+    strcpy((char *)(const char *)pstring2, "overwrite");
+
+    cout << pstring1 << endl;
+    cout << pstring2 << endl;
+  }
+  {
+    PString pstring1("hello world");
+    PString pstring2(pstring1);
+
+    strcpy(pstring2.GetPointer(), "overwrite");
+
+    cout << pstring1 << endl;
+    cout << pstring2 << endl;
+  }
+}
+
+
+////////////////////////////////////////////////
+//
+// test #2 - String stream test
+//
+
+void Test2()
+{
+  for (PINDEX i = 0; i < 2000; ++i) 
+  {
+    PStringStream str;
+    str << "Test #" << 2;
+  }
+
+  PStringStream s;
+  s << "This is a test of the string stream, integer: " << 5 << ", real: " << 5.6;
+  cout << '"' << s << '"' << endl;
+}
+
+
+////////////////////////////////////////////////
+//
+// test #3 - PBYTEArray test
+//
+
+void Test3()
+{
+  {
+    PBYTEArray buffer1(1024);
+    PBYTEArray buffer2(buffer1);
+
+    cout << "base address of PBYTEArray 1 = " << (void *)(buffer1.GetPointer()) << endl;
+    cout << "base address of PBYTEArray 2 = " << (void *)(buffer1.GetPointer()) << endl;
+  }
+
+  {
+    PString str1("hello");
+    PString str2(str1);
+
+    str2 = "world";
+
+    cout << "base address of PString 1 = " << (void *)(str1.GetPointer()) << endl;
+    cout << "base address of PString 2 = " << (void *)(str2.GetPointer()) << endl;
+  }
+}
+
+
+////////////////////////////////////////////////
+//
+// test #4 - string concurrency test
 //
 
 #define SPECIALNAME     "openH323"
@@ -76,13 +158,8 @@ struct StdStringConv : public StringConv<std::string> {
   static const char * ToConstCharStar(const std::string & s) { return s.c_str(); }
 };
 
-void Test1()
+void Test4()
 {
-  /////////////////////
-  //
-  // test #1 - string concurrency test
-  //
-
   // uncomment this to test std::string
   //StringHolder<std::string, StdStringConv> holder(SPECIALNAME);
   
@@ -97,147 +174,7 @@ void Test1()
   finishFlag = PTrue;
   thread->WaitForTermination(9000);
   cerr << "finish" << endl;
-}
-
-////////////////////////////////////////////////
-//
-// test #2 - SIP URL test
-//
-
-#include <ptclib/url.h>
-
-void Test2()
-{
-  const char * urls[] = {
-    "sip:12345678@voxgratia.org",
-    "sip:12345678:5060@voxgratia.org",
-    "sip:12345678:1234@voxgratia.org",
-    NULL
-  };
-
-  const char ** url = urls;
-  while (*url != NULL) {
-    PURL sipURL(*url);
-    cout << "SIP URL : original = " << *url << ", URL = " << sipURL << endl;
-    ++url;
-  }
-}
-
-////////////////////////////////////////////////
-//
-// test #3 - PBYTEArray test
-//
-
-void Test3()
-{
-  {
-    PBYTEArray buffer1(1024);
-    PBYTEArray buffer2(buffer1);
-
-    cout << "base address of PBYTEArray 1 = " << (void *)(buffer1.GetPointer()) << endl;
-    cout << "base address of PBYTEArray 2 = " << (void *)(buffer1.GetPointer()) << endl;
-  }
-
-  {
-    PString str1("hello");
-    PString str2(str1);
-
-    str2 = "world";
-
-    cout << "base address of PString 1 = " << (void *)(str1.GetPointer()) << endl;
-    cout << "base address of PString 2 = " << (void *)(str2.GetPointer()) << endl;
-  }
-}
-
-////////////////////////////////////////////////
-//
-// test #4 - PString test
-//
-
-void Test4()
-{
-  {
-    PString pstring1("hello world");
-    PString pstring2(pstring1);
-
-    strcpy((char *)(const char *)pstring2, "overwrite");
-
-    cout << pstring1 << endl;
-    cout << pstring2 << endl;
-  }
-  {
-    PString pstring1("hello world");
-    PString pstring2(pstring1);
-
-    strcpy(pstring2.GetPointer(), "overwrite");
-
-    cout << pstring1 << endl;
-    cout << pstring2 << endl;
-  }
-}
-
-////////////////////////////////////////////////
-//
-// test #5 - queue channel test
-//
-
-#include <ptclib/qchannel.h>
-
-class Test5Thread : public PThread
-{
-  public:
-    Test5Thread(PQueueChannel & _qchan)
-      : PThread(100, NoAutoDeleteThread), qchan(_qchan)
-    {
-      Resume();
-    }
-
-    void Main()
-    {
-      PThread::Sleep(2000);
-
-      cout << "qchannel started" << endl;
-      for (;;) {
-        char buffer[29];
-        if (!qchan.Read(buffer, sizeof(buffer)))
-          break;
-        cout << "qchan: buffer read" << endl;
-        PThread::Sleep(100);
-      }
-      cout << "qchannel ended" << endl;
-    }
-
-  protected:
-    PQueueChannel & qchan;
-};
-
-void Test5()
-{
-
-  PTimer timer(5000);
-
-  for (;;) {
-    cout << "timer = " << timer.GetMilliSeconds() << endl;
-    PThread::Sleep(200);
-  }
-
-  PQueueChannel qChannel(100);
-  PThread * thrd = new Test5Thread(qChannel);
-
-  char buffer[37];
-  memset(buffer, 'a', sizeof(buffer));
-
-  for (int i = 0; i < 3; ++i) {
-    cout << "writing buffer " << i << endl;
-    if (!qChannel.Write(buffer, sizeof(buffer))) {
-      cout << "write failed" << endl;
-    }
-  }
-  cout << "all buffers written" << endl;
-
-  thrd->WaitForTermination();
-
-  cout << "main done" << endl;
+  delete thread;
 }
 
 ////////////////////////////////////////////////
@@ -256,30 +193,9 @@ PCREATE_PROCESS(StringTest);
 
 void StringTest::Main()
 {
-  PArgList & args = GetArguments();
-
-  if (args.GetCount() < 1) {
-    cout << "usage: strtest num [args...]\n"
-         << "\n"
-         << "where num is one of the following tests\n"
-         << "\n"
-         << "    1     string concurrency test\n"
-         << "    2     SIP URL test\n"
-         << "    3     PBYTEArray test\n"
-         << "    4     string test\n"
-         << "    5     queuechannel test\n"
-         << endl;
-    return;
-  }
-
-  switch (args[0].AsInteger()) {
-    case 1:   Test1(); return;
-    case 2:   Test2(); return;
-    case 3:   Test3(); return;
-    case 4:   Test4(); return;
-    case 5:   Test5(); return;
-    default:  break;
-  }
-
-  cout << "error: unknown test number " << args[0] << endl;
+  //PMEMORY_ALLOCATION_BREAKPOINT(16314);
+  Test1(); cout << "End of test #1\n" << endl;
+  Test2(); cout << "End of test #2\n" << endl;
+  Test3(); cout << "End of test #3\n" << endl;
+  Test4(); cout << "End of test #4\n" << endl;
 }
