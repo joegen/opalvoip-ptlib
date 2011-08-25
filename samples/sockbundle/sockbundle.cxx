d9ec4605eada31c11bad3c1f02cb2934c6cdb604
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2011-08-25 10:05:12 +0000

    Fixed compilation

--- samples/sockbundle/sockbundle.cxx
+++ samples/sockbundle/sockbundle.cxx
@@ -39,13 +39,13 @@ void SockBundleProcess::Main()
          PTrace::Blocks | PTrace::Timestamp | PTrace::Thread | PTrace::FileAndLine);
 #endif
 
-  PMonitoredSocketBundle bundle;
+  PMonitoredSocketBundle bundle(0, false, NULL);
   if (!bundle.Open(5080)) {
     cout << "Cannot open monitored socket bundle" << endl;
     return;
   }
 
-  PSingleMonitoredSocket single(bundle.GetInterfaces()[0]);
+  PSingleMonitoredSocket single(bundle.GetInterfaces()[0], false, NULL);
   if (!single.Open(1719)) {
     cout << "Cannot open single monitored socket" << endl;
     return;
