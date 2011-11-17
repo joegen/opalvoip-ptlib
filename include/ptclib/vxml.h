323d6e2a2aad624730225ae518e85cf769ad7346
Robert Jongbloed <robertj@voxlucida.com.au>
2011-11-17 06:38:35 +0000

    Fixed VXML scripts being interrupted and new ones started.

--- include/ptclib/vxml.h
+++ include/ptclib/vxml.h
@@ -286,7 +286,7 @@ class PVXMLSession : public PIndirectChannel
     virtual bool ProcessNode();
     virtual bool ProcessEvents();
     virtual bool ProcessGrammar();
-    virtual bool NextNode();
+    virtual bool NextNode(bool skipChildren);
 
     void SayAs(const PString & className, const PString & text);
     void SayAs(const PString & className, const PString & text, const PString & voice);
@@ -619,6 +619,7 @@ class PVXMLChannel : public PDelayChannel
     PVXMLQueue      m_playQueue;
     PVXMLPlayable * m_currentPlayItem;
     PSimpleTimer    m_silenceTimer;
+    PSyncPointAck   m_flushQueue;
 };
 
 
