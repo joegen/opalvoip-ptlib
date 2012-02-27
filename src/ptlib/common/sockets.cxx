061c9e3c0e4c109b219565a83e3d6ec1785bff1e
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2012-02-27 06:38:45 +0000

    At various points in PTLib (and OPAL) the interface name is used in situations where there can be confusion in parsing. For example a URL can be http:://[::%interface]:2345 and a ] in interface name blows it to pieces. Similarly, "ip:port" style description, which can be used a LOT, will also fail. At this late stage it is too hard to change all the other places, so we hack the relatively rare cases by translating those special characters.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@27063 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/common/sockets.cxx
+++ src/ptlib/common/sockets.cxx
@@ -2055,31 +2055,48 @@ bool PIPSocket::Address::IsRFC1918() const
 
 
 PIPSocket::InterfaceEntry::InterfaceEntry()
-  : ipAddr(PIPSocket::GetInvalidAddress())
-  , netMask(PIPSocket::GetInvalidAddress())
+  : m_ipAddress(PIPSocket::GetInvalidAddress())
+  , m_netMask(PIPSocket::GetInvalidAddress())
 {
 }
 
-
-PIPSocket::InterfaceEntry::InterfaceEntry(const PString & _name,
-                                          const Address & _addr,
-                                          const Address & _mask,
-                                          const PString & _macAddr)
-  : name(_name.Trim()),
-    ipAddr(_addr),
-    netMask(_mask),
-    macAddr(_macAddr)
+PIPSocket::InterfaceEntry::InterfaceEntry(const PString & name,
+                                          const Address & address,
+                                          const Address & mask,
+                                          const PString & macAddress)
+  : m_name(name.Trim())
+  , m_ipAddress(address)
+  , m_netMask(mask)
+  , m_macAddress(macAddress)
 {
+  SanitiseName(m_name);
+}
+
+
+void PIPSocket::InterfaceEntry::SanitiseName(PString & name)
+{
+  /* HACK!!
+     At various points in PTLib (and OPAL) the interface name is used in
+     situations where there can be confusion in parsing. For example a URL can
+     be http:://[::%interface]:2345 and a ] in interface name blows it to
+     pieces. Similarly, "ip:port" style description which can be used a LOT,
+     will also fail. At this late stage it is too hard to change all the other
+     places, so we hack the fairly rare cases by translating those special
+     characters.
+   */ 
+  name.Replace('[', '{', true);
+  name.Replace(']', '}', true);
+  name.Replace(':', ';', true);
 }
 
 
 void PIPSocket::InterfaceEntry::PrintOn(ostream & strm) const
 {
-  strm << ipAddr;
-  if (!macAddr)
-    strm << " <" << macAddr << '>';
-  if (!name)
-    strm << " (" << name << ')';
+  strm << m_ipAddress;
+  if (!m_macAddress)
+    strm << " <" << m_macAddress << '>';
+  if (!m_name)
+    strm << " (" << m_name << ')';
 }
 
 
