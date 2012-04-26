67f9a0a610ac89d1c91df69aaa87aa6bfe0e3d24
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2012-04-26 07:48:22 +0000

    Merged revision(s) 25539, 26670 from ptlib/trunk: add encryption (StartTLS) support for LDAP compile fix for Solaris 11

--- src/ptclib/pldap.cxx
+++ src/ptclib/pldap.cxx
@@ -119,6 +119,15 @@ PBoolean PLDAPSession::SetOption(int optcode, void * value)
   return ldap_set_option(ldapContext, optcode, value);
 }
 
+PBoolean PLDAPSession::StartTLS()
+{
+#ifdef LDAP_EXOP_START_TLS
+  errorNumber = ldap_start_tls_s(ldapContext, NULL, NULL);
+  return errorNumber == LDAP_SUCCESS;
+#else
+  return LDAP_OPERATIONS_ERROR;
+#endif
+}
 
 PBoolean PLDAPSession::Bind(const PString & who,
                         const PString & passwd,
