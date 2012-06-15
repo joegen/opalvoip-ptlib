2cedd73cb3c4b47d7c13274d9c416bddac5be4a1
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2012-06-15 02:06:25 +0000

    Fixed PSYSTEMLOG macro so do not get GNU compiler warning, and the thing the GNU compiler was warning against can't happen.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@27834 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/ptlib/syslog.h
+++ include/ptlib/syslog.h
@@ -356,10 +356,8 @@ The current log level is checked and if allowed, the second argument is evaluate
 as a stream output sequence which is them output to the system log.
 */
 #define PSYSTEMLOG(level, variables) \
-  if (PSystemLog::GetTarget().GetThresholdLevel() >= PSystemLog::level) { \
-    PSystemLog P_systemlog(PSystemLog::level); \
-    P_systemlog << variables; \
-  } else (void)0
+  if (PSystemLog::GetTarget().GetThresholdLevel() < PSystemLog::level) (void)0; \
+  else PSystemLog(PSystemLog::level) << variables
 
 
 #endif
