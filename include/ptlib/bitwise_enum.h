62e73b9c810b5ff3b1c3de429dfb60289b27ff04
Robert Jongbloed <robertj@voxlucida.com.au>
2012-06-26 07:47:38 +0000

    Added stream operations to PBitwiseEnum

--- include/ptlib/bitwise_enum.h
+++ include/ptlib/bitwise_enum.h
@@ -39,19 +39,23 @@
    This is typically use within the P_DECLARE_BITWISE_ENUM() which allows the
    enumeration to be correctly defined.
  */
-template <typename Enumeration,
-          Enumeration maxValue,
-          Enumeration initialValue = (Enumeration)0,
-          typename IntType = unsigned>
+template <typename BaseEnum, BaseEnum MaxValue, typename BaseInt = unsigned>
 class PBitwiseEnum
 {
+  public:
+    typedef BaseEnum Enumeration;
+    typedef BaseInt  IntType;
+
   protected:
     Enumeration m_enum;
 
   public:
-    __inline PBitwiseEnum(Enumeration e = initialValue) : m_enum(e) { }
+    __inline PBitwiseEnum(Enumeration e = (Enumeration)0) : m_enum(e) { }
     __inline PBitwiseEnum(const PBitwiseEnum & e) : m_enum(e.m_enum) { }
-    __inline explicit PBitwiseEnum(IntType bitNum) : m_enum((Enumeration)(1 << bitNum)) { if (m_enum > maxValue) m_enum  = maxValue; }
+    __inline explicit PBitwiseEnum(unsigned bitNum) : m_enum((Enumeration)(1 << bitNum)) { if (m_enum > MaxValue) m_enum  = MaxValue; }
+
+    enum IteratorBounds { First, Last };
+    __inline PBitwiseEnum(IteratorBounds b) : m_enum(b == First ? (Enumeration)1 : MaxValue) { }
 
     __inline PBitwiseEnum & operator=(const PBitwiseEnum & e) { m_enum = e.m_enum; return *this; }
     __inline PBitwiseEnum & operator=(Enumeration          e) { m_enum = e; return *this; }
@@ -60,18 +64,19 @@ class PBitwiseEnum
     __inline operator Enumeration&()             { return m_enum; }
     __inline operator const Enumeration&() const { return m_enum; }
     __inline Enumeration * operator&()           { return &m_enum; }
+    __inline unsigned AsBits() const             { return m_enum; }
 
     PBitwiseEnum operator++()
     {
       Enumeration previous = m_enum;
-      if (m_enum < maxValue)
+      if (m_enum < MaxValue)
         m_enum = static_cast<Enumeration>(m_enum << 1);
       return previous;
     }
 
     PBitwiseEnum operator++(int)
     {
-      if (m_enum < maxValue)
+      if (m_enum < MaxValue)
         m_enum = static_cast<Enumeration>(static_cast<IntType>(m_enum) << 1);
       return *this;
     }
@@ -142,13 +147,25 @@ class PBitwiseEnum
 #define P_DECLARE_BITWISE_ENUM_7(_0,_1,_2,_3,_4,_5,_6,_7)P_DECLARE_BITWISE_ENUM_6(_0,_1,_2,_3,_4,_5,_6),_7=64
 #define P_DECLARE_BITWISE_ENUM_8(_0,_1,_2,_3,_4,_5,_6,_7,_8)P_DECLARE_BITWISE_ENUM_6(_0,_1,_2,_3,_4,_5,_6,_7),_8=128
 
-#define P_DECLARE_BITWISE_ENUM_END(name, count) \
+#define P_DECLARE_BITWISE_NAMES_1(_0,_1)#_0,#_1
+#define P_DECLARE_BITWISE_NAMES_2(_0,_1,_2)P_DECLARE_BITWISE_NAMES_1(_0,_1),#_2
+#define P_DECLARE_BITWISE_NAMES_3(_0,_1,_2,_3)P_DECLARE_BITWISE_NAMES_2(_0,_1,_2),#_3
+#define P_DECLARE_BITWISE_NAMES_4(_0,_1,_2,_3,_4)P_DECLARE_BITWISE_NAMES_3(_0,_1,_2,_3),#_4
+#define P_DECLARE_BITWISE_NAMES_5(_0,_1,_2,_3,_4,_5)P_DECLARE_BITWISE_NAMES_4(_0,_1,_2,_3,_4),#_5
+#define P_DECLARE_BITWISE_NAMES_6(_0,_1,_2,_3,_4,_5,_6)P_DECLARE_BITWISE_NAMES_5(_0,_1,_2,_3,_4,_5),#_6
+#define P_DECLARE_BITWISE_NAMES_7(_0,_1,_2,_3,_4,_5,_6,_7)P_DECLARE_BITWISE_NAMES_6(_0,_1,_2,_3,_4,_5,_6),#_7
+#define P_DECLARE_BITWISE_NAMES_8(_0,_1,_2,_3,_4,_5,_6,_7,_8)P_DECLARE_BITWISE_NAMES_6(_0,_1,_2,_3,_4,_5,_6,_7),#_8
+
+#define P_DECLARE_BITWISE_ENUM_FRIENDS(name) \
   __inline friend name##_Bits operator+(name##_Bits lhs, name##_Bits rhs) \
     { return static_cast<name##_Bits>(static_cast<unsigned>(lhs) | static_cast<unsigned>(rhs)); } \
   __inline friend name##_Bits operator|(name##_Bits lhs, name##_Bits rhs) \
     { return static_cast<name##_Bits>(static_cast<unsigned>(lhs) | static_cast<unsigned>(rhs)); } \
   __inline friend name##_Bits operator-(name##_Bits lhs, name##_Bits rhs) \
-    { return static_cast<name##_Bits>(static_cast<unsigned>(lhs) & ~static_cast<unsigned>(rhs)); } \
+    { return static_cast<name##_Bits>(static_cast<unsigned>(lhs) & ~static_cast<unsigned>(rhs)); }
+
+#define P_DECLARE_BITWISE_ENUM_END(name, count) \
+  P_DECLARE_BITWISE_ENUM_FRIENDS(name) \
   typedef PBitwiseEnum<name##_Bits, (name##_Bits)(1<<count)> name
 
 
@@ -180,6 +197,29 @@ class PBitwiseEnum
 #define P_DECLARE_BITWISE_ENUM_EX(name, count, values, ...) \
   enum name##_Bits { P_DECLARE_BITWISE_ENUM_##count values , ##__VA_ARGS__ }; \
   P_DECLARE_BITWISE_ENUM_END(name, count)
+
+
+extern void PPrintBitwiseEnum(std::ostream & strm, unsigned bits, char const * const * names);
+extern unsigned PReadBitwiseEnum(std::istream & strm, char const * const * names);
+
+
+#define P_DECLARE_STREAMABLE_BITWISE_ENUM_EX(name, count, values, ...) \
+  enum name##_Bits { P_DECLARE_BITWISE_ENUM_##count values }; \
+  P_DECLARE_BITWISE_ENUM_FRIENDS(name) \
+  class name : public PBitwiseEnum<name##_Bits, (name##_Bits)(1<<count)>{ \
+    public: typedef PBitwiseEnum<name##_Bits, (name##_Bits)(1<<count)> BaseClass; \
+    __inline name(BaseClass::Enumeration e = (BaseClass::Enumeration)0) : BaseClass(e) { } \
+    __inline explicit name(unsigned bitNum) : BaseClass(bitNum) { } \
+    __inline name(IteratorBounds b) : BaseClass(b) { } \
+    static char const * const * Names() { static char const * const Strings[] = { __VA_ARGS__, NULL }; return Strings; } \
+    friend __inline std::ostream & operator<<(std::ostream & strm, const name & e) { PPrintBitwiseEnum(strm, e.AsBits(), name::Names()); return strm; } \
+    friend __inline std::istream & operator>>(std::istream & strm, name & e) { e = (BaseClass::Enumeration)PReadBitwiseEnum(strm, name::Names()); return strm; } \
+    PString ToString() { PStringStream strm; strm >> *this; return strm; } \
+    bool FromString(const PString & s) { PStringStream strm(s); strm >> *this; return strm.good(); } \
+  }
+
+#define P_DECLARE_STREAMABLE_BITWISE_ENUM(name, count, values) \
+        P_DECLARE_STREAMABLE_BITWISE_ENUM_EX(name, count, values, P_DECLARE_BITWISE_NAMES_##count values)
 
 
 #endif // PTLIB_BITWISE_ENUM_H
