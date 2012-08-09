37f02b8ce3c08e33f730b85f51e9e8ec494e0038
Robert Jongbloed <robertj@voxlucida.com.au>
2012-08-09 03:59:59 +0000

    Changed PEthSocket so can use built in filter expressions as per "tcpdump".

--- samples/ether/ether.cxx
+++ samples/ether/ether.cxx
@@ -30,6 +30,7 @@
 #include <ptlib/pprocess.h>
 #include <ptlib/sockets.h>
 
+static PMutex g_coutMutex;
 
 class MyProcess : public PProcess
 {
@@ -37,24 +38,41 @@ class MyProcess : public PProcess
   public:
     MyProcess()
       : PProcess("Vox Lucida", "Ethernet", 1, 0, ReleaseCode, 1)
-      , m_running(true)
     { }
 
     virtual void Main();
 
     virtual bool OnInterrupt(bool)
     {
-      m_running = false;
+      m_exit.Signal();
       return true;
     }
 
   protected:
-    bool m_running;
+    PSyncPoint m_exit;
 };
 
 
 PCREATE_PROCESS(MyProcess)
 
+
+class TestThread : public PThread
+{
+  public:
+    TestThread(PINDEX idx, PArgList & args);
+
+    bool IsOpen() const { return m_socket.IsOpen(); }
+
+    void Stop();
+
+  protected:
+    virtual void Main();
+
+    PINDEX     m_index;
+    PEthSocket m_socket;
+    bool       m_binary;
+    bool       m_running;
+};
 
 
 void MyProcess::Main()
@@ -62,20 +80,20 @@ void MyProcess::Main()
   cout << "Ethernet Test Utility" << endl;
 
   PArgList & args = GetArguments();
-  if (args.Parse(
-          "i-interface: Interface name to sniff\n"
-          "I-ip:        IP address\n"
-          "T-tcp:       TCP port\n"
-          "U-udp:       UDP port\n"
-          "B-binary.    Payload is binary\n"
+  PArgList::ParseResult parseResult = args.Parse(
+          "l-list.      List available interfaces\n"
+          "b-binary.    Payload is binary\n"
 #if PTRACING
           "o-output:    Trace log output file\n"
           "t-trace.     Trace log level\n"
 #endif
           "h-help.      Help text\n"
-          ) < PArgList::ParseNoArguments || args.HasOption('h') ||
-                     (args.HasOption('T') && args.HasOption('U'))) {
+          );
+  if (parseResult < PArgList::ParseNoArguments || args.HasOption('h') ||
+          (parseResult == PArgList::ParseNoArguments && !args.HasOption('l'))) {
+    cerr << "\nusage: " << GetName() << " <interface> [ <filter> ]\n";
     args.Usage(cerr);
+    cerr << "\nThe <interface> is either #N for the N'th interface, or the full string name.\n";
     return;
   }
 
@@ -85,25 +103,43 @@ void MyProcess::Main()
          PTrace::Blocks | PTrace::Timestamp | PTrace::Thread | PTrace::FileAndLine);
 #endif
 
-  PEthSocket sock;
-
-  if (!args.HasOption('i')) {
-    PStringArray interfaces = sock.EnumInterfaces();
-    if (interfaces.IsEmpty())
-      cout << "No interfaces available for ethernet sniffing." << endl;
-    else {
-      cout << "Available interfaces:\n";
-      for (PINDEX i = 0; i < interfaces.GetSize(); ++i)
-        cout << "  #" << i << ' ' << interfaces[i] << '\n';
-      cout << endl;
-    }
+  if (args.HasOption('l')) {
+    cout << "Available interfaces:\n";
+    PStringArray interfaces = PEthSocket::EnumInterfaces();
+    for (PINDEX i = 0; i < interfaces.GetSize(); ++i)
+      cout << "  #" << i << ' ' << interfaces[i] << '\n';
+    cout << endl;
     return;
   }
 
-  PString interf = args.GetOptionString('i');
+  PList<TestThread> tests;
+  do {
+    tests.Append(new TestThread(tests.GetSize()+1, args));
+    if (!tests.back().IsOpen())
+      return;
+  } while (args.Parse(NULL) > PArgList::ParseNoArguments);
+
+  m_exit.Wait();
+
+  g_coutMutex.Wait();
+  cout << "Exiting sniffers ..." << endl;
+  g_coutMutex.Signal();
+
+  for (PList<TestThread>::iterator it = tests.begin(); it != tests.end(); ++it)
+    it->Stop();
+}
+
+
+TestThread::TestThread(PINDEX idx, PArgList & args)
+  : PThread(0, NoAutoDeleteThread, NormalPriority, "Sniffer")
+  , m_index(idx)
+  , m_binary(args.HasOption('b'))
+  , m_running(true)
+{
+  PString interf = args[0];
   if (interf[0] == '#') {
     PINDEX idx = interf.Mid(1).AsUnsigned();
-    PStringArray interfaces = sock.EnumInterfaces(false);
+    PStringArray interfaces = PEthSocket::EnumInterfaces(false);
     if (idx >= interfaces.GetSize()) {
       cerr << "Invalid index for interface." << endl;
       return;
@@ -111,20 +147,39 @@ void MyProcess::Main()
     interf = interfaces[idx];
   }
 
-  sock.SetReadTimeout(1000);
-  if (!sock.Connect(interf)) {
+  m_socket.SetReadTimeout(1000);
+  if (!m_socket.Connect(interf)) {
     cerr << "Could not open interface \"" << interf << '"' << endl;
     return;
   }
 
-  cout << "Listening on \"" << interf << '"' << endl;
+  PString filter;
+  for (PINDEX arg = 1; arg < args.GetCount(); ++arg)
+    filter &= args[arg];
+  if (!m_socket.SetFilter(filter)) {
+    cerr << "Could not use filter \"" << filter << '"' << endl;
+    m_socket.Close();
+  }
 
-  PIPSocket::Address ip = PIPSocket::GetDefaultIpAny();
-  if (args.HasOption('I'))
-    ip = args.GetOptionString('I');
+  Resume();
+}
 
-  unsigned tcp = args.GetOptionString('T', "0").AsUnsigned();
-  unsigned udp = args.GetOptionString('U', "0").AsUnsigned();
+
+void TestThread::Stop()
+{
+  m_running = false;
+  WaitForTermination();
+}
+
+
+void TestThread::Main()
+{
+  g_coutMutex.Wait();
+  cout << m_index << ": Listening on \"" << m_socket.GetName() << '"';
+  if (!m_socket.GetFilter().IsEmpty())
+    cout << " filtered by \"" << m_socket.GetFilter() << '"';
+  cout << endl;
+  g_coutMutex.Signal();
 
   PBYTEArray buffer;
   PEthSocket::Address src, dst;
@@ -132,50 +187,57 @@ void MyProcess::Main()
   PINDEX payloadSize;
   BYTE * payloadPtr;
   while (m_running) {
-    if (sock.ReadPacket(buffer, dst, src, type, payloadSize, payloadPtr)) {
-      if (tcp == 0 && udp == 0)
-        cout << dst << "<=" << src << ' ' << hex << setfill('0') << setw(4) << type << dec << ' ' << payloadSize << endl;
-      else if (type == PEthSocket::TypeIP && (payloadPtr[9] == (udp != 0 ? 0x11 : 6))) {
-        PIPSocket::Address srcIP(4, payloadPtr+12);
-        PIPSocket::Address dstIP(4, payloadPtr+16);
-        if (ip.IsAny() || srcIP == ip || dstIP == ip) {
-          size_t len = (payloadPtr[0]&0xf)*4; // low 4 bits in DWORDS, is this in bytes
-          payloadSize -= len;
-          payloadPtr += len;
+    if (!m_socket.ReadPacket(buffer, dst, src, type, payloadSize, payloadPtr)) {
+      if (m_socket.GetErrorCode(PChannel::LastReadError) == PChannel::Timeout)
+        continue;
 
-          WORD srcPort = ((PUInt16b *)payloadPtr)[0];
-          WORD dstPort = ((PUInt16b *)payloadPtr)[1];
-          if (udp != 0) {
-            payloadSize -= 8;
-            payloadPtr += 8;
-          }
-          else {
-            payloadSize -= 20;
-            payloadPtr += 20;
-          }
-
-          if (srcPort == tcp || dstPort == tcp || srcPort == udp || dstPort == udp) {
-            cout << dstIP << ':' << dstPort << "<=" << srcIP << ':' << dstPort << ' ';
-            if (args.HasOption('B'))
-              cout << PBYTEArray(payloadPtr, std::min(payloadSize, 16), false);
-            else {
-              PString str((char *)payloadPtr, payloadSize);
-              cout << str.Left(str.FindOneOf("\r\n"));
-            }
-            cout << endl;
-          }
-        }
-      }
-    }
-    else if (sock.GetErrorCode(PChannel::LastReadError) != PChannel::Timeout) {
-      cerr << "Error reading interface \"" << interf << '"'  << endl;
+      cerr << m_index << ": Error reading interface \"" << m_socket.GetName() << '"'  << endl;
       break;
     }
+
+    g_coutMutex.Wait();
+    cout << m_index << ": ";
+    if (type != PEthSocket::TypeIP)
+      cout << dst << "<=" << src << ' ' << hex << setfill('0') << setw(4) << type << dec << ' ' << payloadSize;
+    else {
+      PIPSocket::Address srcIP(4, payloadPtr+12);
+      PIPSocket::Address dstIP(4, payloadPtr+16);
+      int proto = payloadPtr[9];
+
+      size_t len = (payloadPtr[0]&0xf)*4; // low 4 bits in DWORDS, is this in bytes
+      payloadSize -= len;
+      payloadPtr += len;
+
+      WORD srcPort = ((PUInt16b *)payloadPtr)[0];
+      WORD dstPort = ((PUInt16b *)payloadPtr)[1];
+      cout << dstIP << ':' << dstPort << "<=" << srcIP << ':' << srcPort << ' ';
+
+      if (proto != 0x11) {
+        payloadSize -= 8;
+        payloadPtr += 8;
+        cout << "UDP ";
+      }
+      else {
+        payloadSize -= 20;
+        payloadPtr += 20;
+        cout << "TCP ";
+      }
+
+      if (m_binary)
+        cout << PBYTEArray(payloadPtr, std::min(payloadSize, 16), false);
+      else {
+        PString str((char *)payloadPtr, payloadSize);
+        cout << str.Left(str.FindOneOf("\r\n"));
+      }
+    }
+
+    cout << endl;
+    g_coutMutex.Signal();
   }
 
-  sock.Close();
-
-  cout << "Ended sniff." << endl;
+  g_coutMutex.Wait();
+  cout << m_index << ": Finished sniffer." << endl;
+  g_coutMutex.Signal();
 }
 
 
