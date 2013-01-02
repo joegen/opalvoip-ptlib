fa241205bf1450ee2d04a660f10c8c2e6a520dd6
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2013-01-02 02:44:11 +0000

    Back port of portability issue with pthread_yield(), in particular some Mac OS-X versions.

--- src/ptlib/unix/tlibthrd.cxx
+++ src/ptlib/unix/tlibthrd.cxx
@@ -1535,7 +1535,7 @@ PTimedMutex::~PTimedMutex()
     for (PINDEX i = 0; i < 100; ++i) {
       if ((result = pthread_mutex_destroy(&m_mutex)) != EBUSY)
         break;
-      pthread_yield();
+      usleep(100);
     }
   }
 
