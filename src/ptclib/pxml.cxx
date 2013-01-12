9234c3a6feae90bb0f9957511458783809d01716
Craig Southeren <craigs@postincrement.com>
2013-01-12 01:09:35 +0000

    Merged revision(s) 28824-28825 from ptlib/trunk: --------------------- r28825 Replace ancient PXML sample code with working sample and test for "billion  laugh" attack --------------------- r28824 Fixed PXML display handling and added limit on size of data objects to defend against "billion  laugh" attack.

--- src/ptclib/pxml.cxx
+++ src/ptclib/pxml.cxx
@@ -59,6 +59,15 @@
 
 ////////////////////////////////////////////////////
 
+PXMLBase::PXMLBase(int opts)
+  : m_options(opts)
+  , m_maxEntityLength(PXML::DEFAULT_MAX_ENTITY_LENGTH)
+{
+}
+
+
+////////////////////////////////////////////////////
+
 static void PXML_StartElement(void * userData, const char * name, const char ** attrs)
 {
   ((PXMLParser *)userData)->StartElement(name, attrs);
@@ -198,6 +207,13 @@ void PXMLParser::EndElement(const char * /*name*/)
 
 void PXMLParser::AddCharacterData(const char * data, int len)
 {
+  unsigned checkLen = len + ((lastElement != NULL) ? lastElement->GetString().GetLength() : 0);
+  if (checkLen >= m_maxEntityLength) {
+    PTRACE(2, "PXML\tAborting XML parse at size " << m_maxEntityLength << " - possible 'billion laugh' attack");
+    XML_StopParser((XML_Parser)expat, XML_FALSE);
+    return;
+  }
+
   PString str(data, len);
 
   if (lastElement != NULL) {
@@ -376,6 +392,7 @@ bool PXML::Load(const PString & data, PXMLParser::Options options)
 
   {
     PXMLParser parser(options);
+    parser.SetMaxEntityLength(m_maxEntityLength);
     int done = 1;
     stat = parser.Parse(data, data.GetLength(), done) != 0;
   
@@ -562,6 +579,7 @@ void PXML::ReadFrom(istream & strm)
   rootMutex.Signal();
 
   PXMLParser parser(m_options);
+  parser.SetMaxEntityLength(m_maxEntityLength);
   while (strm.good()) {
     PString line;
     strm >> line;
