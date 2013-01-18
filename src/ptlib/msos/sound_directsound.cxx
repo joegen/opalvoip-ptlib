60ff97d3f830a6f249838f6ef384e9ea30b8e12e
Craig Southeren <craigs@postincrement.com>
2013-01-18 13:50:13 +0000

    Uplift changes from Eridani stable:
    Revision: 28931
    ---------------
    Backport various patches from trunk, including
    fixes for cross-compilation, MingW32/64 compilation,
    and various 64 bit cleanups 
    
    Revision: 28933
    ---------------
    Update Win32 configure
    
    Revision: 28935
    ---------------
    In order to remove *one* warning on the 64 bit compile, it was 
    necessary to change PNOTIFIER to use P_INT_PTR instead of INT.
    Now it clean compiles on Win32 and Win64
    Also add in missing library for static builds
    
    Revision: 28936
    ---------------
    Fix gcc build

--- src/ptlib/msos/sound_directsound.cxx
+++ src/ptlib/msos/sound_directsound.cxx
@@ -35,6 +35,10 @@
 
 #if P_DIRECTSOUND
 
+#if _MSC_VER >= 1700
+#include "initguid.h"
+#endif
+
 #define PTraceModule() "dsound"
 
 #include <ptlib/pprocess.h>
@@ -47,9 +51,11 @@
 #include <tchar.h>
 #include <math.h>
 
+
 #if _MSC_VER >= 1700
-#include "initguid.h"
+  #pragma comment(lib, "dxguid.lib")
 #endif
+
 
 #include <ks.h>
 #include <dsconf.h>
