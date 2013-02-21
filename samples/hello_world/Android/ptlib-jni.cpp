e54597b7986141e0682a93918412a28fd788ee57
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2013-02-21 02:12:08 +0000

    First "hello world" application working with Android emulator.

--- samples/hello_world/Android/ptlib-jni.cpp
+++ samples/hello_world/Android/ptlib-jni.cpp
@@ -17,25 +17,85 @@
 
 #include <ptlib.h>
 #include <ptlib/pprocess.h>
+#include <ptlib/pluginmgr.h>
 
 #include <jni.h>
 
-class JniProcess : public PLibraryProcess {
+class Pjstring
+{
+  JNIEnv     * m_jni;
+  jstring      m_jstr;
+  const char * m_cstr;
+
 public:
-   JniProcess()
-      : PLibraryProcess("Vox Lucida", "PTLibHello", 1,  0, AlphaCode, 0)
-   {
-   }
-} myProcess;
+  Pjstring(JNIEnv* jni, jstring str)
+    : m_jni(jni)
+    , m_jstr(str)
+    , m_cstr(jni->GetStringUTFChars(str, 0))
+  {
+  }
+
+  ~Pjstring()
+  {
+    Release();
+  }
+
+  void Release()
+  {
+    if (m_cstr != NULL) {
+      m_jni->ReleaseStringUTFChars(m_jstr, m_cstr);
+      m_cstr = NULL;
+    }
+  }
+
+  Pjstring & operator=(jstring str)
+  {
+    Release();
+    m_cstr = m_jni->GetStringUTFChars(m_jstr = str, 0);
+  }
+
+  operator const char *() const { return m_cstr; }
+  operator PString() const { return m_cstr; }
+};
 
 
 extern "C" {
 
+void 
+Java_com_ptlib_hello_PTLibHello_initialisePTLib(JNIEnv* env,
+                                                jobject thiz,
+                                                jstring cfgDir,
+                                                jstring tmpDir,
+                                                jstring pluginDir)
+{
+  PLibraryProcess * process = new PLibraryProcess("Vox Lucida", "PTLibHello", 1,  0, PProcess::AlphaCode, 0, true);
+
+  Pjstring tmp(env, tmpDir);
+  static PConstString const logFile("/ptlib.log");
+  PTRACE_INITIALISE(5, tmp + logFile);
+
+  tmp = cfgDir;
+  process->SetConfigurationPath(tmp);
+
+  tmp = pluginDir;
+  PPluginManager::AddPluginDirs(tmp);
+
+  process->Startup();
+}
+
+
 jstring
-Java_com_ptlib_hello_PTLibHello_getVersion( JNIEnv* env,
+Java_com_ptlib_hello_PTLibHello_getAppVersion( JNIEnv* env,
                                                   jobject thiz )
 {
-    return env->NewStringUTF(myProcess.GetVersion());
+    return env->NewStringUTF(PProcess::Current().GetVersion());
+}
+
+jstring
+Java_com_ptlib_hello_PTLibHello_getLibVersion( JNIEnv* env,
+                                                  jobject thiz )
+{
+    return env->NewStringUTF(PProcess::GetLibVersion());
 }
 
 };
