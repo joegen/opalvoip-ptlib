9d0533231f3d6e7f22aeb5c676d7e19c099166fa
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2013-06-14 03:48:22 +0000

    Merged revision(s) 29946-29958 from ptlib/branches/v2_12: Don't output trace file file line number if it is zero.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@29959 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/common/osutils.cxx
+++ src/ptlib/common/osutils.cxx
@@ -662,8 +662,12 @@ ostream & PTraceInfo::InternalBegin(bool topLevel, unsigned level, const char * 
           file = fileName;
       }
     }
+    stream << setw(16) << file;
 
-    stream << setw(16) << file << '(' << lineNum << ")\t";
+    if (lineNum > 0)
+      stream << '(' << lineNum << ')';
+
+    stream << '\t';
   }
 
   if (HasOption(ObjectInstance)) {
