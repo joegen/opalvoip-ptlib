b72982c2713a5389a19d28a835fdc873b4a17631
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2013-09-10 05:43:49 +0000

    Merged revision(s) 30466-30485 from ptlib/branches/v2_12: Added a bunch of defines for various audio/video driver/device names. Applications can use these to avoid spelling errors.

--- include/ptlib/sound.h
+++ include/ptlib/sound.h
@@ -763,7 +763,12 @@ template <class className> class PSoundChannelPluginServiceDescriptor : public P
   static PSoundChannelPluginServiceDescriptor<className> className##_descriptor; \
   PCREATE_PLUGIN(name, PSoundChannel, &className##_descriptor)
 
+#define P_NULL_AUDIO_DRIVER "NullAudio"
+#define P_NULL_AUDIO_DEVICE "Null Audio"
+PPLUGIN_STATIC_LOAD(NullAudio, PSoundChannel)
+
 #ifdef _WIN32
+  #define P_WINDOWS_MULTIMEDIA_DRIVER "WindowsMultimedia"
   PPLUGIN_STATIC_LOAD(WindowsMultimedia, PSoundChannel);
 #elif defined(P_ANDROID)
   PPLUGIN_STATIC_LOAD(OpenSL_ES, PSoundChannel);
@@ -774,10 +779,12 @@ template <class className> class PSoundChannelPluginServiceDescriptor : public P
 #endif
 
 #if defined(P_DIRECTSOUND)
+  #define P_DIRECT_SOUND_DRIVER "DirectSound"
   PPLUGIN_STATIC_LOAD(DirectSound, PSoundChannel);
 #endif
 
 #if defined(P_WAVFILE)
+  #define P_WAV_FILE_DRIVER "WAVFile"
   PPLUGIN_STATIC_LOAD(WAVFile, PSoundChannel)
 #endif
 
