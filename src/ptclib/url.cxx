8503b993a0525b3a59236915a80a63aec058e7c9
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2014-02-06 04:31:12 +0000

    Applied patch #260 Fix PURL::schemeInfo initialization, thanks Valeriy V. Argunov

--- src/ptclib/url.cxx
+++ src/ptclib/url.cxx
@@ -398,6 +398,10 @@ PBoolean PURL::InternalParse(const char * cstr, const char * defaultScheme)
   queryVars.RemoveAll();
   m_contents.MakeEmpty();
 
+  // get information which tells us how to parse URL for this
+  // particular scheme
+  schemeInfo = NULL;
+
   if (cstr == NULL)
     return false;
 
@@ -408,10 +412,6 @@ PBoolean PURL::InternalParse(const char * cstr, const char * defaultScheme)
   // if string is empty, return
   if (*cstr == '\0')
     return false;
-
-  // get information which tells us how to parse URL for this
-  // particular scheme
-  schemeInfo = NULL;
 
   // Character set as per RFC2396
   //    scheme        = alpha *( alpha | digit | "+" | "-" | "." )
