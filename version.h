05c3d76228cba177e4eed6db186782343cde6e3c
Robert Jongbloed <robertj@voxlucida.com.au>
2014-02-17 05:51:21 +0000

    Update version on trunk

--- version.h
+++ version.h
@@ -34,9 +34,9 @@
  */
 
 #define MAJOR_VERSION 2
-#define MINOR_VERSION 13
-#define BUILD_TYPE BetaCode
-#define BUILD_NUMBER 3
+#define MINOR_VERSION 15
+#define BUILD_TYPE AlphaCode
+#define BUILD_NUMBER 0
 
 
 #endif  // _PTLib_VERSION_H
