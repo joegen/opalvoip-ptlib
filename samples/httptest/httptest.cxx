a3664df61a4cfde5c76aa396ba7535e74bc263b4
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2014-04-10 06:23:35 +0000

    Merged revision(s) 31717-31725 from ptlib/branches/v2_14: --------------------- r31722 Fixed getting correct default sound device on Windows. --------------------- r31721 Added example of secure (https) server.

--- samples/httptest/httptest.cxx
+++ samples/httptest/httptest.cxx
@@ -7,6 +7,7 @@
 #include <ptlib.h>
 #include <ptlib/pprocess.h>
 #include <ptlib/sockets.h>
+#include <ptclib/pssl.h>
 #include <ptclib/http.h>
 #include <ptclib/threadpool.h>
 
@@ -14,15 +15,25 @@
 class HTTPConnection
 {
   public:
-    HTTPConnection(PHTTPSpace & httpNameSpace)
-      :  m_httpNameSpace(httpNameSpace)
+    HTTPConnection(
+      PHTTPSpace & httpNameSpace
+#if P_SSL
+      , PSSLContext * context
+#endif
+    ) : m_httpNameSpace(httpNameSpace)
+#if P_SSL
+      , m_context(context)
+#endif
     {
     }
 
     void Work();
 
-    PHTTPSpace & m_httpNameSpace;
-    PTCPSocket m_socket;
+    PHTTPSpace  & m_httpNameSpace;
+#if P_SSL
+    PSSLContext * m_context;
+#endif
+    PTCPSocket    m_socket;
 };
 
 
@@ -41,41 +52,43 @@ PCREATE_PROCESS(HTTPTest)
 void HTTPTest::Main()
 {
   PArgList & args = GetArguments();
-  args.Parse("h-help."
-             "p-port:"
-             "T-theads:"
-             "Q-queue:"
-#if PTRACING
-             "o-output:"
-             "t-trace."
+  args.Parse("h-help.    print this help message.\n"
+             "p-port:    port number to listen on(default 80 or 443).\n"
+#if P_SSL
+             "s-secure.     SSL/TLS mode.\n"
+             "-ca:          SSL/TLS client certificate authority file/directory.\n"
+             "-certificate: SSL/TLS server certificate.\n"
+             "-private-key: SSL/TLS server private key.\n"
 #endif
+             "T-theads:  max number of threads in pool(default 10)\n"
+             "Q-queue:   max queue size for listening sockets(default 100).\n"
+             PTRACE_ARGLIST
        );
 
-#if PTRACING
-  PTrace::Initialise(args.GetOptionCount('t'),
-                     args.HasOption('o') ? (const char *)args.GetOptionString('o') : NULL,
-         PTrace::Blocks | PTrace::Timestamp | PTrace::Thread | PTrace::FileAndLine);
-#endif
+  PTRACE_INITIALISE(args);
 
-  if (args.HasOption('h')) {
-    PError << "usage: " << GetFile().GetTitle() << "[options]\n"
-              "\n"
-              "Available options are:\n"
-              "   -h --help             : print this help message.\n"
-              "   -p --port n           : port number to listen on (default 80).\n"
-              "   -T --threads n        : max number of threads in pool (default 10)\n"
-              "   -Q --queue n          : max queue size for listening sockets (default 100).\n"
-#if PTRACING
-              "   -o or --output file   : file name for output of log messages\n"       
-              "   -t or --trace         : degree of verbosity in log (more times for more detail)\n"     
-#endif
-           << endl;
+  if (!args.IsParsed() || args.HasOption('h')) {
+    args.Usage(PError);
     return;
   }
 
   m_pool.SetMaxWorkers(args.GetOptionString('T', "10").AsUnsigned());
 
-  PTCPSocket listener((WORD)args.GetOptionString('p', "80").AsUnsigned());
+#if P_SSL
+  PSSLContext * sslContext = args.HasOption('s') ? new PSSLContext : NULL;
+  if (sslContext != NULL) {
+    if (!sslContext->SetCredentials(args.GetOptionString("ca", "."),
+                                    args.GetOptionString("certificate", "certificate.pem"),
+                                    args.GetOptionString("private-key", "privatekey.pem"),
+                                    true))
+    {
+      cerr << "Could not set credentials for SSL" << endl;
+      return;
+    }
+  }
+#endif
+
+    PTCPSocket listener(args.GetOptionAs('p', (WORD)(sslContext != NULL ? 443 : 80)));
   if (!listener.Listen(args.GetOptionString('Q', "100").AsUnsigned())) {
     cerr << "Could not listen on port " << listener.GetPort() << endl;
     return;
@@ -84,10 +97,10 @@ void HTTPTest::Main()
   PHTTPSpace httpNameSpace;
   httpNameSpace.AddResource(new PHTTPString("index.html", "Hello", "text/plain"));
 
-  cout << "Listening for HTTP on port " << listener.GetPort() << endl;
+  cout << "Listening for " << (sslContext != NULL ? "https" : "http") << " on port " << listener.GetPort() << endl;
 
   for (;;) {
-    HTTPConnection * connection = new HTTPConnection(httpNameSpace);
+    HTTPConnection * connection = new HTTPConnection(httpNameSpace, sslContext);
     if (connection->m_socket.Accept(listener))
       m_pool.AddWork(connection);
     else {
@@ -105,9 +118,20 @@ void HTTPConnection::Work()
 {
   PTRACE(3, "HTTPTest\tStarted work on " << m_socket.GetPeerAddress());
 
-#if 1
-
   PHTTPServer httpServer(m_httpNameSpace);
+
+#if P_SSL
+  if (m_context != NULL) {
+    PSSLChannel * ssl = new PSSLChannel(m_context);
+    if (!ssl->Open(m_socket))
+      return;
+    if (!ssl->Accept())
+      return;
+    if (!httpServer.Open(ssl))
+      return;
+  }
+  else
+#endif
   if (!httpServer.Open(m_socket))
     return;
 
@@ -116,16 +140,6 @@ void HTTPConnection::Work()
     ++count;
 
   PTRACE(3, "HTTPTest\tEnded work on " << m_socket.GetPeerAddress() << ", " << count << " transactions.");
-
-#else
-
-  char buffer[1000];
-  if (!m_socket.Read(buffer, sizeof(buffer)))
-    cerr << "Error in read: " << m_socket.GetErrorText(PChannel::LastReadError) << endl;
-  else if (!m_socket.WriteString("HTTP/1.1 200 OK\r\nContent-Length: 0\r\nConnection: close\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"))
-    cerr << "Error in write: " << m_socket.GetErrorText(PChannel::LastWriteError) << endl;
-
-#endif
 }
 
 
