482dc593b3634c22bc128968bb82366eb82be37b
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2014-04-16 02:45:16 +0000

    Fixed merge error
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@31777 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptlib/common/collect.cxx
+++ src/ptlib/common/collect.cxx
@@ -1160,7 +1160,6 @@ void PHashTableInfo::AppendElement(PObject * key, PObject * data PTRACE_PARAM(, 
   PTRACE_IF(1, list.m_size > 20 && list.m_size * 10 > totalSize / 8, owner, "PTLib",
             "Poor hash function used, more than 80% of " << totalSize <<
             " items in same bucket for class " << owner->GetClass());
-  }
 #endif
 }
 
