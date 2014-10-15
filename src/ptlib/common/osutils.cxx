860d8bf009c1a5849215a635f35386fbd4ebe276
Robert Jongbloed <robertj@voxlucida.com.au>
2014-10-15 09:11:28 +0000

    Added function to return CPU times as percentage.

--- src/ptlib/common/osutils.cxx
+++ src/ptlib/common/osutils.cxx
@@ -2501,6 +2501,13 @@ PThread::Times::Times()
 }
 
 
+float PThread::Times::AsPercentage() const
+{
+  PTimeInterval cpu = m_kernel + m_user;
+  return (float)(cpu == 0 ? 0.0 : (100.0 * cpu.GetMilliSeconds() / m_real.GetMilliSeconds()));
+}
+
+
 void PThread::PrintOn(ostream & strm) const
 {
   strm << GetThreadName();
