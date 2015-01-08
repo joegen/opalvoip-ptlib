636be9403aea9629b7f75e7db77309aa16d27670
Robert Jongbloed <robertj@voxlucida.com.au>
2015-01-08 07:22:50 +0000

    More tweaks for unusual build environments

--- src/ptlib/unix/tlib.cxx
+++ src/ptlib/unix/tlib.cxx
@@ -608,6 +608,296 @@ void PProcess::CommonDestruct()
   m_keepingHouse = false;
 }
 
+
+PBoolean PProcess::SetMaxHandles(int newMax)
+{
+#ifndef P_RTEMS
+  // get the current process limit
+  struct rlimit rl;
+  PAssertOS(getrlimit(RLIMIT_NOFILE, &rl) == 0);
+
+  // set the new current limit
+  rl.rlim_cur = newMax;
+  if (setrlimit(RLIMIT_NOFILE, &rl) == 0) {
+    PAssertOS(getrlimit(RLIMIT_NOFILE, &rl) == 0);
+    maxHandles = rl.rlim_cur;
+    if (maxHandles == newMax) {
+      PTRACE(2, "PTLib\tNew maximum per-process file handles set to " << maxHandles);
+      return true;
+    }
+  }
+#endif // !P_RTEMS
+
+  PTRACE(1, "PTLib\tCannot set per-process file handle limit to "
+         << newMax << " (is " << maxHandles << ") - check permissions");
+  return false;
+}
+
+
+#if defined(P_LINUX)
+
+static inline unsigned long long jiffies_to_msecs(const unsigned long j)
+{
+  static long sysconf_HZ = sysconf(_SC_CLK_TCK);
+  return (j * 1000LL) / sysconf_HZ;
+}
+
+
+static bool InternalGetTimes(const char * filename, PThread::Times & times)
+{
+  /* From the man page on the "stat" file
+  Status information about the process. This is used by ps(1). It is defined in /usr/src/linux/fs/proc/array.c.
+  The fields, in order, with their proper scanf(3) format specifiers, are:
+  pid         %d   The process ID.
+  comm        %s   The filename of the executable, in parentheses. This is visible
+  whether or not the executable is swapped out.
+  state       %c   One character from the string "RSDZTW" where R is running, S is
+  sleeping in an interruptible wait, D is waiting in uninterruptible
+  disk sleep, Z is zombie, T is traced or stopped (on a signal), and
+  W is paging.
+  ppid        %d   The PID of the parent.
+  pgrp        %d   The process group ID of the process.
+  session     %d   The session ID of the process.
+  tty_nr      %d   The tty the process uses.
+  tpgid       %d   The process group ID of the process which currently owns the tty
+  that the process is connected to.
+  flags       %lu  The kernel flags word of the process. For bit meanings, see the
+  PF_* defines in <linux/sched.h>. Details depend on the kernel
+  version.
+  minflt      %lu  The number of minor faults the process has made which have not
+  required loading a memory page from disk.
+  cminflt     %lu  The number of minor faults that the process's waited-for children
+  have made.
+  majflt      %lu  The number of major faults the process has made which have required
+  loading a memory page from disk.
+  cmajflt     %lu  The number of major faults that the process's waited-for children
+  have made.
+  utime       %lu  The number of jiffies that this process has been scheduled in user
+  mode.
+  stime       %lu  The number of jiffies that this process has been scheduled in kernel
+  mode.
+  cutime      %ld  The number of jiffies that this process's waited-for children have
+  been scheduled in user mode. (See also times(2).)
+  cstime      %ld  The number of jiffies that this process's waited-for children have
+  been scheduled in kernel mode.
+  priority    %ld  The standard nice value, plus fifteen. The value is never negative
+  in the kernel.
+  nice        %ld  The nice value ranges from 19 (nicest) to -19 (not nice to others).
+  num_threads %ld  Number of threads.
+  itrealvalue %ld  The time in jiffies before the next SIGALRM is sent to the process
+  due to an interval timer.
+  starttime   %lu  The time in jiffies the process started after system boot.
+  vsize       %lu  Virtual memory size in bytes.
+  rss         %ld  Resident Set Size: number of pages the process has in real memory,
+  minus 3 for administrative purposes. This is just the pages which
+  count towards text, data, or stack space. This does not include
+  pages which have not been demand-loaded in, or which are swapped out.
+  rlim        %lu  Current limit in bytes on the rss of the process
+  (usually 4294967295 on i386).
+  startcode   %lu  The address above which program text can run.
+  endcode     %lu  The address below which program text can run.
+  startstack  %lu  The address of the start of the stack.
+  kstkesp     %lu  The current value of esp (stack pointer), as found in the kernel
+  stack page for the process.
+  kstkeip     %lu  The current EIP (instruction pointer).
+  signal      %lu  The bitmap of pending signals.
+  blocked     %lu  The bitmap of blocked signals.
+  sigignore   %lu  The bitmap of ignored signals.
+  sigcatch    %lu  The bitmap of caught signals.
+  wchan       %lu  This is the "channel" in which the process is waiting. It is the
+  address of a system call, and can be looked up in a namelist if you
+  need a textual name. (If you have an up-to-date /etc/psdatabase, then
+  try ps -l to see the WCHAN field in action.)
+  nswap       %lu  Number of pages swapped (not maintained).
+  cnswap      %lu  Cumulative nswap for child processes (not maintained).
+  exit_signal %d   Signal to be sent to parent when we die.
+  processor   %d   CPU number last executed on.
+  rt_priority %lu  (since kernel 2.5.19) Real-time scheduling priority (see sched_setscheduler(2)).
+  policy      %lu  (since kernel 2.5.19) Scheduling policy (see sched_setscheduler(2)).
+  delayacct_blkio_ticks %llu (since Linux 2.6.18) Aggregated block I/O delays, measured in
+  clock ticks (centiseconds).
+  */
+
+  for (int retry = 0; retry < 3; ++retry) {
+    std::ifstream statfile(filename);
+
+    char line[1000];
+    statfile.getline(line, sizeof(line));
+    if (!statfile.good())
+      continue;
+
+    int pid;
+    char comm[100];
+    char state;
+    int ppid, pgrp, session, tty_nr, tpgid;
+    unsigned long flags, minflt, cminflt, majflt, cmajflt, utime, stime;
+    long cutime, cstime, priority, nice, num_threads, itrealvalue;
+    unsigned long starttime, vsize;
+    long rss;
+    unsigned long rlim, startcode, endcode, startstack, kstkesp, kstkeip, signal, blocked, sigignore, sigcatch, wchan, nswap, cnswap;
+    int exit_signal, processor;
+    unsigned long rt_priority, policy;
+    unsigned long long delayacct_blkio_ticks;
+
+    // 17698 (maxmcu) R 1 17033 8586 34833 17467 4202560 7
+    // 0 0 0 0 0 0 0 -100 0 16
+    // 0 55172504 258756608 6741 4294967295 134512640 137352760 3217892976 8185700 15991824
+    // 0 0 4 201349635 0 0 0 -1 7 99
+    // 2 0
+
+    int count = sscanf(line,
+                       "%d%s %c%d%d%d%d%d%lu%lu"
+                       "%lu%lu%lu%lu%lu%ld%ld%ld%ld%ld"
+                       "%ld%lu%lu%ld%lu%lu%lu%lu%lu%lu"
+                       "%lu%lu%lu%lu%lu%lu%lu%d%d%lu"
+                       "%lu%llu",
+                       &pid, comm, &state, &ppid, &pgrp, &session, &tty_nr, &tpgid, &flags, &minflt,
+                       &cminflt, &majflt, &cmajflt, &utime, &stime, &cutime, &cstime, &priority, &nice, &num_threads,
+                       &itrealvalue, &starttime, &vsize, &rss, &rlim, &startcode, &endcode, &startstack, &kstkesp, &kstkeip,
+                       &signal, &blocked, &sigignore, &sigcatch, &wchan, &nswap, &cnswap, &exit_signal, &processor, &rt_priority,
+                       &policy, &delayacct_blkio_ticks);
+    if (count != 42)
+      continue;
+
+    times.m_kernel = jiffies_to_msecs(stime);
+    times.m_user = jiffies_to_msecs(utime);
+    return true;
+  }
+
+  return false;
+}
+
+
+bool PThread::GetTimes(Times & times)
+{
+  // Do not use any PTLib functions in here as they could to a PTRACE, and this deadlock
+  times.m_name = m_threadName;
+  times.m_threadId = m_threadId;
+  times.m_uniqueId = PX_linuxId;
+  times.m_real = (PX_endTick != 0 ? PX_endTick : PTimer::Tick()) - PX_startTick;
+
+  return InternalGetTimes(PSTRSTRM("/proc/" << getpid() << "/task/" << PX_linuxId << "/stat"), times);
+}
+
+
+bool PProcess::GetProcessTimes(Times & times) const
+{
+  times.m_name = "Process Total";
+  times.m_real = PTime() - GetStartTime();
+  return InternalGetTimes("/proc/self/stat", times);
+}
+
+
+bool PProcess::GetSystemTimes(Times & times)
+{
+  times.m_name = "System Total";
+
+  for (int retry = 0; retry < 3; ++retry) {
+    std::ifstream statfile("/proc/stat");
+
+    char dummy[10];
+    uint64_t user, nice, system, idle, iowait, irq, softirq, steal;
+    statfile >> dummy >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
+    if (statfile.good()) {
+      times.m_kernel = jiffies_to_msecs(system);
+      times.m_user = jiffies_to_msecs(user);
+      times.m_real = times.m_kernel + times.m_user + jiffies_to_msecs(idle);
+      return true;
+    }
+  }
+  return false;
+}
+
+
+void PProcess::GetMemoryUsage(MemoryUsage & usage)
+{
+  ifstream proc("/proc/self/statm");
+  size_t virtPages, resPages;
+  proc >> virtPages >> resPages;
+  if (proc.good()) {
+    usage.m_virtual = virtPages * 4096;
+    usage.m_resident = resPages * 4096;
+  }
+  else
+    usage.m_virtual = usage.m_resident = 0;
+
+#if P_HAS_MALLOC_INFO
+  usage.m_max = usage.m_current = usage.m_blocks = 0;
+
+  char * buffer = NULL;
+  size_t size;
+  FILE * mem = open_memstream(&buffer, &size);
+  malloc_info(0, mem);
+  fclose(mem);
+
+#if P_EXPAT
+  PXML xml;
+  if (xml.Load(buffer)) {
+    PXMLElement * root = xml.GetRootElement();
+    PXMLElement * element = root->GetElement("system", "type", "current");
+    if (element != NULL)
+      usage.m_current = (size_t)element->GetAttribute("size").AsUnsigned64();
+    element = root->GetElement("system", "type", "max");
+    if (element != NULL)
+      usage.m_max = (size_t)element->GetAttribute("size").AsUnsigned64();
+  }
+#else // P_EXPAT
+  // Find last </heap> in XML
+  size_t offset = 0;
+  static PRegularExpression HeapRE("< */ *heap *>", PRegularExpression::Extended);
+  PINDEX pos, len;
+  while (HeapRE.Execute(buffer + offset, pos, len))
+    offset += pos + len;
+
+  // The <system type="xxx" size=yyy> after the <heap>s is total
+
+  PStringArray substrings(2);
+
+  static PRegularExpression MaxRE("< *system *type *= *\"max\" *size *= *\"([0-9]+)", PRegularExpression::Extended);
+  if (MaxRE.Execute(buffer + offset, substrings))
+    usage.m_max = (size_t)substrings[1].AsUnsigned64();
+
+  static PRegularExpression CurrentRE("< *system *type *= *\"current\" *size *= *\"([0-9]+)", PRegularExpression::Extended);
+  if (CurrentRE.Execute(buffer + offset, substrings))
+    usage.m_current = (size_t)substrings[1].AsUnsigned64();
+#endif // P_EXPAT
+
+  free(buffer);
+#else // P_HAS_MALLOC_INFO
+  struct mallinfo info = mallinfo();
+  usage.m_max = info.uordblks + info.fordblks;
+  usage.m_current = info.uordblks;
+  usage.m_blocks = info.hblks;
+#endif // P_HAS_MALLOC_INFO
+}
+
+#else //P_LINUX
+
+void PProcess::GetMemoryUsage(MemoryUsage & usage)
+{
+}
+
+
+bool PThread::GetTimes(Times & times)
+{
+  return false;
+}
+
+
+bool PProcess::GetProcessTimes(Times & times) const
+{
+  return false;
+}
+
+
+bool PProcess::GetSystemTimes(Times & times)
+{
+  return false;
+}
+
+#endif // P_LINUX
+
+
 //////////////////////////////////////////////////////////////////////////////
 // P_fd_set
 
@@ -692,50 +982,17 @@ P_fd_set & P_fd_set::operator-=(intptr_t fd)
 #include "tlibrtems.cxx"
 #else
 
+#warning No thread support, practically nothing will work!
+
+
 void PProcess::HouseKeeping()
 {
-  while (m_keepingHouse) {
-    PTimeInterval delay = m_timerList->Process();
-    if (delay > 10000)
-      delay = 10000;
-
-    m_signalHouseKeeper.Wait(delay);
-
-    InternalCleanAutoDeleteThreads();
-
-    PXCheckSignals();
-  }
 }
 
 
 void PProcess::Construct()
 {
   CommonConstruct();
-}
-
-
-PBoolean PProcess::SetMaxHandles(int newMax)
-{
-#ifndef P_RTEMS
-  // get the current process limit
-  struct rlimit rl;
-  PAssertOS(getrlimit(RLIMIT_NOFILE, &rl) == 0);
-
-  // set the new current limit
-  rl.rlim_cur = newMax;
-  if (setrlimit(RLIMIT_NOFILE, &rl) == 0) {
-    PAssertOS(getrlimit(RLIMIT_NOFILE, &rl) == 0);
-    maxHandles = rl.rlim_cur;
-    if (maxHandles == newMax) {
-      PTRACE(2, "PTLib\tNew maximum per-process file handles set to " << maxHandles);
-      return true;
-    }
-  }
-#endif // !P_RTEMS
-
-  PTRACE(1, "PTLib\tCannot set per-process file handle limit to "
-         << newMax << " (is " << maxHandles << ") - check permissions");
-  return false;
 }
 
 
@@ -810,6 +1067,7 @@ PThread::Priority PThread::GetPriority() const
 
 void PThread::Sleep(const PTimeInterval & timeout)
 {
+  usleep(timeout.GetInterval() * 1000);
 }
 
 
@@ -850,29 +1108,6 @@ PUniqueThreadIdentifier PThread::GetUniqueIdentifier() const
 PUniqueThreadIdentifier PThread::GetCurrentUniqueIdentifier()
 {
   return GetCurrentThreadId();
-}
-
-
-void PProcess::GetMemoryUsage(MemoryUsage & usage)
-{
-}
-
-
-bool PThread::GetTimes(Times & times)
-{
-  return false;
-}
-
-
-bool PProcess::GetProcessTimes(Times & times) const
-{
-  return false;
-}
-
-
-bool PProcess::GetSystemTimes(Times & times)
-{
-  return false;
 }
 
 
