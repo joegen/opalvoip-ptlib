0425f76117c5e05c314d3cf55a8037b35e25f67c
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2015-01-29 12:01:44 +0000

    Merged revision(s) 33299-33303 from ptlib/branches/v2_14: Fixed a partially read MIME (no blank line at end) marking the iostream fail bit.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/ptlib/trunk@33304 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/ptclib/inetprot.cxx
+++ src/ptclib/inetprot.cxx
@@ -621,10 +621,13 @@ void PMIMEInfo::ReadFrom(istream &strm)
 
   PString line;
   PString lastLine;
+  bool incomplete = true;
   while (strm.good()) {
     strm >> line;
-    if (line.IsEmpty())
+    if (line.IsEmpty()) {
+      incomplete = false;
       break;
+    }
     if (line[0] == ' ' || line[0] == '\t') // RFC 2822 section 2.2.2 & 2.2.3
       lastLine += line;
     else {
@@ -633,10 +636,11 @@ void PMIMEInfo::ReadFrom(istream &strm)
     }
   }
 
-  if (!lastLine.IsEmpty()) {
+  if (!lastLine.IsEmpty())
     AddMIME(lastLine);
-    strm.setstate(ios::failbit); // Should always end with empty line
-  }
+
+  if (incomplete)
+    strm.setstate(ios::failbit);
 }
 
 
