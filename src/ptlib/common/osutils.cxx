bad46d2a56750c7dc0b3f4de6c43dfc0ff161883
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2016-02-01 17:52:50 +0000

    Fixed race condition when stopping timer, especially form destructor, where if it times out and the callback function starts the timer again.

--- src/ptlib/common/osutils.cxx
+++ src/ptlib/common/osutils.cxx
@@ -1263,18 +1263,21 @@ void PTimer::Stop(bool wait)
   if (list == NULL)
     return;
 
-  /* Take out of timer list first, so when callback is waited for it's
-     completion it cannot then be called again. */
-  list->m_timersMutex.Wait();
-  list->m_timers.erase(m_handle);
-  list->m_timersMutex.Signal();
+  do {
+    /* Take out of timer list first, so when callback is waited for it's
+       completion it cannot then be called again. */
+    list->m_timersMutex.Wait();
+    list->m_timers.erase(m_handle);
+    m_running = false;
+    list->m_timersMutex.Signal();
 
-  m_running = false;
+    if (wait) {
+      m_callbackMutex.Wait();
+      m_callbackMutex.Signal();
+    }
 
-  if (wait) {
-    m_callbackMutex.Wait();
-    m_callbackMutex.Signal();
-  }
+    // We loop in case the callback function restarted the timer.
+  } while (m_running);
 }
 
 
