05606e3b07e624c3cb95f5fffa8c7f7c313b10ae
Gavin Llewellyn <gavin.llewellyn@blackboard.com>
2017-05-26 18:00:55 +0100

    Fix UCS2->UTF8 conversion for >= U+0800 (3 byte UTF8 char)

--- src/ptlib/common/contain.cxx
+++ src/ptlib/common/contain.cxx
@@ -2179,7 +2179,7 @@ void PString::InternalFromUCS2(const wchar_t * ptr, PINDEX len)
         theArray[count++] = (char)(0x80+(v&0x3f));
       }
       else {
-        theArray[count++] = (char)(0xd0+(v>>12));
+        theArray[count++] = (char)(0xe0+(v>>12));
         theArray[count++] = (char)(0x80+((v>>6)&0x3f));
         theArray[count++] = (char)(0x80+(v&0x3f));
       }
