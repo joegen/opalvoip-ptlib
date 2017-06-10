ef88498d8fd5f9679210bfd4e842f84d7cabbbfe
Robert Jongbloed <robertj@voxlucida.com.au>
2017-06-10 11:56:18 +0100

    REmoved support for older Visual Studio versions. It's free, just upgrade!

--- include/ptlib/msos/CopyDTF_2012.bat
+++
@@ -1,9 +0,0 @@
-copy ..\..\..\lib\Win32\debug\ptlibd.def ptlibd_2012_x86.dtf
-copy ..\..\..\lib\Win32\release\ptlib.def ptlib_2012_x86.dtf
-copy "..\..\..\lib\Win32\No Trace\ptlibn.def" ptlibn_2012_x86.dtf
-
-copy ..\..\..\lib\x64\debug\ptlib64d.def ptlibd_2012_x64.dtf
-copy ..\..\..\lib\x64\release\ptlib64.def ptlib_2012_x64.dtf
-copy "..\..\..\lib\x64\No Trace\ptlib64n.def" ptlibn_2012_x64.dtf
-
-pause
