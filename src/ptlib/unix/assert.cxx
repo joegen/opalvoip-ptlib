91ba9d5abc045dc6594ac9d14b0af1560c833957
Robert Jongbloed <robert.jongbloed@blackboard.com>
2017-12-23 14:50:56 +0000

    Avoid spurious errors in PAssert() on Mac.

--- src/ptlib/unix/assert.cxx
+++ src/ptlib/unix/assert.cxx
@@ -52,6 +52,7 @@
   #endif // PTRACING
 
 
+#ifndef P_MACOSX
   static bool fgetstr(std::string & str, FILE * fp)
   {
     for (;;) {
@@ -87,6 +88,7 @@
       str += (char)c;
     }
   }
+#endif // P_MACOSX
 
 
   static void InternalWalkStack(ostream & strm, unsigned framesToSkip, void * const * addresses, unsigned addressCount)
@@ -99,7 +101,7 @@
 
     std::vector<std::string> lines(addressCount);
 
-    {
+#ifndef P_MACOSX
       FILE * pipe;
       {
         std::stringstream cmd;
@@ -128,6 +130,7 @@
         fclose(pipe);
       }
     }
+#endif // P_MACOSX
 
 
     DEBUG_CERR("InternalWalkStack: before backtrace_symbols");
